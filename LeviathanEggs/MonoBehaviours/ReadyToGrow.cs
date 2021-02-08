01562c27dd385b1faf4777dfffd90d327fab7dd8
Metious <71298690+Metious@users.noreply.github.com>
2021-02-08 12:19:55 +0330

    Added Staged Growing System

---
+++ LeviathanEggs/MonoBehaviours/ReadyToGrow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+namespace LeviathanEggs.MonoBehaviours
+{
+    public class ReadyToGrow : MonoBehaviour
+    {
+        StagedGrowing _stagedGrowing;
+        void Awake() => _stagedGrowing = GetComponent<StagedGrowing>();
+        void Start()
+        {
+            _stagedGrowing ??= gameObject.EnsureComponent<StagedGrowing>();
+            transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        }
+    }
+}
---
+++ LeviathanEggs/MonoBehaviours/StagedGrowing.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+using UWE;
+namespace LeviathanEggs.MonoBehaviours
+{
+    public class StagedGrowing : MonoBehaviour
+    {
+        public TechType nextStageTechType = TechType.None;
+        public int daysToNextStage = 5;
+
+        double timePassed;
+        double days = -1;
+        bool coroutineRunning = false;
+        void Update()
+        {
+            timePassed = DayNightCycle.main.timePassed;
+
+            days = (timePassed / 1200) - 1;
+
+            if (gameObject.transform.localScale.x < 1f && gameObject.transform.localScale.y < 1f && gameObject.transform.localScale.z < 1f)
+                if (!coroutineRunning)
+                    CoroutineHost.StartCoroutine(StartGrowing());
+
+            if (gameObject.transform.localScale.x >= 1f && gameObject.transform.localScale.y >= 1f && gameObject.transform.localScale.z >= 1f)
+            {
+                if (nextStageTechType != TechType.None && days >= daysToNextStage)
+                {
+                    foreach (var collider in gameObject.GetAllComponentsInChildren<Collider>())
+                        collider.enabled = false;
+                    foreach (var rend in gameObject.GetAllComponentsInChildren<Renderer>())
+                        rend.enabled = false;
+
+                    GameObject prefab = CraftData.GetPrefabForTechType(nextStageTechType);
+                    prefab.SetActive(false);
+
+                    GameObject nextStageObject = GameObject.Instantiate(prefab, transform.position, transform.rotation);
+                    nextStageObject.EnsureComponent<ReadyToGrow>();
+                    nextStageObject.SetActive(true);
+
+                    DestroyImmediate(gameObject);
+                }
+            }
+        }
+        IEnumerator StartGrowing()
+        {
+            coroutineRunning = true;
+            while (Time.timeScale > 0f && gameObject.transform.localScale.x <= 1f && gameObject.transform.localScale.y <= 1f && gameObject.transform.localScale.z <= 1f)
+            {
+                gameObject.transform.localScale += 0.001f * Vector3.one;
+                yield return null;
+            }
+
+            coroutineRunning = false;
+
+            yield break;
+        }
+    }
+}
