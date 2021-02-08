cceabb12aff73b7bdf5bafc9cf61fa282a6b0d6e
Metious <71298690+Metious@users.noreply.github.com>
2021-02-08 12:21:26 +0330

    fixed some bugs that makes the SeaEmperorBaby disappear on a certain time + fixed no animations hatched SeaEmperorBaby

---
+++ LeviathanEggs/Patches/SeaEmperorBaby_Patches.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using LeviathanEggs.MonoBehaviours;
+namespace LeviathanEggs.Patches
+{
+    [HarmonyPatch(typeof(SeaEmperorBaby), nameof(SeaEmperorBaby.Teleport))]
+    class SeaEmperorBaby_Teleport_Patch
+    {
+        [HarmonyPrefix]
+        static bool Prefix(SeaEmperorBaby __instance)
+        {
+            if (__instance.gameObject.GetComponent<StagedGrowing>() != null)
+                return false;
+
+            return true;
+        }
+    }
+    [HarmonyPatch(typeof(SeaEmperorBaby), nameof(SeaEmperorBaby.SetTeleporterTarget))]
+    class SeaEmperorBaby_SetTeleporterTarget_Patch
+    {
+        [HarmonyPrefix]
+        static bool Prefix(SeaEmperorBaby __instance)
+        {
+            if (__instance.gameObject.GetComponent<StagedGrowing>() != null)
+                return false;
+
+            return true;
+        }
+    }
+    [HarmonyPatch(typeof(SeaEmperorBaby), nameof(SeaEmperorBaby.SwimToTeleporter))]
+    class SeaEmperorBaby_SwimToTeleporter_Patch
+    {
+        [HarmonyPrefix]
+        static bool Prefix(SeaEmperorBaby __instance)
+        {
+            if (__instance.gameObject.GetComponent<StagedGrowing>() != null)
+                return false;
+
+            return true;
+        }
+    }
+    [HarmonyPatch(typeof(SeaEmperorBaby), nameof(SeaEmperorBaby.SwimToMother))]
+    class SeaEmperorBaby_SwimToMother_Patch
+    {
+        [HarmonyPrefix]
+        static bool Prefix(SeaEmperorBaby __instance)
+        {
+            if (__instance.gameObject.GetComponent<StagedGrowing>() != null)
+                return false;
+
+            return true;
+        }
+    }
+}
