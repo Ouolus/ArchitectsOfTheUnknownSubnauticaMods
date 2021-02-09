2e3a7904a6b2003fd10241c8f8ec09eb34591561
Metious <71298690+Metious@users.noreply.github.com>
2021-02-09 07:58:40 +0330

    removed some unnecessary code

--- LeviathanEggs/MonoBehaviours/ReadyToGrow.cs
+++ LeviathanEggs/MonoBehaviours/ReadyToGrow.cs
@@ -3,11 +3,9 @@ namespace LeviathanEggs.MonoBehaviours
 {
     public class ReadyToGrow : MonoBehaviour
     {
-        StagedGrowing _stagedGrowing;
-        void Awake() => _stagedGrowing = GetComponent<StagedGrowing>();
         void Start()
         {
-            _stagedGrowing ??= gameObject.EnsureComponent<StagedGrowing>();
+            gameObject.EnsureComponent<StagedGrowing>();
             transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         }
     }
