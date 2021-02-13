37152feabbd27478580cbd4cdcf2e2637012f00e
Metious <71298690+Metious@users.noreply.github.com>
2021-02-13 19:03:11 +0330

    moved WaterParkCreature stuff to it's .cs file

--- LeviathanEggs/Patches/WaterParkCreature_Patch.cs
+++ LeviathanEggs/Patches/WaterParkCreature_Patch.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using UnityEngine;
+using LeviathanEggs.MonoBehaviours;
 namespace LeviathanEggs.Patches
 {
     [HarmonyPatch(typeof(WaterParkCreature))]
@@ -11,37 +12,127 @@ namespace LeviathanEggs.Patches
         {
             TechType techType = CraftData.GetTechType(__instance.gameObject);
 
-            if (techType == TechType.SeaEmperorBaby)
+            switch (techType)
             {
-                SeaEmperorBaby seb = __instance.gameObject.GetComponent<SeaEmperorBaby>();
-                if (seb != null)
-                {
-                    SafeAnimator.SetBool(seb.GetAnimator(), "hatched", true);
-                    seb.hatched = true;
-                }
-            }
-        }
-        [HarmonyPostfix]
-        [HarmonyPatch(nameof(WaterParkCreature.Update))]
-        static void Update_Patch(WaterParkCreature __instance)
-        {
-            TechType techType = CraftData.GetTechType(__instance.gameObject);
-            foreach (TechType tt in Main.TechTypesToSkyApply)
-            {
-                if (techType == tt)
-                {
-                    SkyApplier skyApplier = __instance.gameObject.EnsureComponent<SkyApplier>();
+                case TechType.SeaEmperorBaby:
+                    {
+                        SeaEmperorBaby seb = __instance.gameObject.GetComponent<SeaEmperorBaby>();
+                        if (seb != null)
+                        {
+                            SafeAnimator.SetBool(seb.GetAnimator(), "hatched", true);
+                            seb.hatched = true;
+                        }
+                        StagedGrowing stagedGrowing = __instance.gameObject.EnsureComponent<StagedGrowing>();
+                        stagedGrowing.daysToNextStage = 5;
+                        stagedGrowing.nextStageTechType = TechType.SeaEmperorJuvenile;
+                        __instance.canBreed = false;
+                        Pickupable pickupable = __instance.gameObject.EnsureComponent<Pickupable>();
+                        pickupable.isPickupable = true;
+                        pickupable.timeDropped = Time.time;
+                        break;
+                    }
+                case TechType.SeaEmperorJuvenile:
+                    {
+                        StagedGrowing stagedGrowing = __instance.gameObject.EnsureComponent<StagedGrowing>();
+                        stagedGrowing.daysToNextStage = 5;
+                        stagedGrowing.nextStageTechType = TechType.SeaEmperor;
+                        __instance.canBreed = false;
+                        Pickupable pickupable = __instance.gameObject.EnsureComponent<Pickupable>();
+                        pickupable.isPickupable = true;
+                        pickupable.timeDropped = Time.time;
+                        break;
+                    }
 
-                    if (__instance.gameObject.TryGetComponent(out __instance) && __instance.IsInsideWaterPark())
-                        skyApplier.anchorSky = Skies.BaseInterior;
-                    else
-                        skyApplier.anchorSky = Skies.Auto;
-                    skyApplier.renderers = __instance.gameObject.GetAllComponentsInChildren<Renderer>();
-                    skyApplier.dynamic = true;
-                    skyApplier.emissiveFromPower = false;
-                    skyApplier.hideFlags = HideFlags.None;
-                    skyApplier.enabled = true;
+                case TechType.GhostLeviathanJuvenile:
+                    {
+                        StagedGrowing stagedGrowing = __instance.gameObject.EnsureComponent<StagedGrowing>();
+                        stagedGrowing.daysToNextStage = 5;
+                        stagedGrowing.nextStageTechType = TechType.GhostLeviathan;
+                        __instance.canBreed = false;
+                        Pickupable pickupable = __instance.gameObject.EnsureComponent<Pickupable>();
+                        pickupable.isPickupable = true;
+                        pickupable.timeDropped = Time.time;
+                        break;
+                    }
+                case TechType.GhostLeviathan:
+                    {
+                        Pickupable pickupable = __instance.gameObject.EnsureComponent<Pickupable>();
+                        pickupable.isPickupable = true;
+                        pickupable.timeDropped = Time.time;
+                        break;
+                    }
+                case TechType.SeaEmperor:
+                    {
+                        Pickupable pickupable = __instance.gameObject.EnsureComponent<Pickupable>();
+                        pickupable.isPickupable = true;
+                        pickupable.timeDropped = Time.time;
+                        break;
+                    }
+                case TechType.SeaDragon:
+                    {
+                        Pickupable pickupable = __instance.gameObject.EnsureComponent<Pickupable>();
+                        pickupable.isPickupable = true;
+                        pickupable.timeDropped = Time.time;
+                        break;
+                    }
+                case TechType.PrecursorDroid:
+                    {
+                        Pickupable pickupable = __instance.gameObject.EnsureComponent<Pickupable>();
+                        pickupable.isPickupable = true;
+                        pickupable.timeDropped = Time.time;
+                        break;
+                    }
+
+            }
+
+
+            if (Main.TechTypesToSkyApply.Contains(techType))
+            {
+                SkyApplier skyApplier = __instance.gameObject.EnsureComponent<SkyApplier>();
+
+                skyApplier.anchorSky = Skies.Auto;
+                skyApplier.renderers = __instance.gameObject.GetAllComponentsInChildren<Renderer>();
+                skyApplier.dynamic = true;
+                skyApplier.emissiveFromPower = false;
+                skyApplier.hideFlags = HideFlags.None;
+                skyApplier.enabled = true;
+            }
+
+            if (Main.TechTypesToMakePickupable.Contains(techType))
+            {
+                Pickupable pickupable = __instance.gameObject.EnsureComponent<Pickupable>();
+                pickupable.isPickupable = false;
+            }
+
+            if (Main.TechTypesToTweak.Contains(techType))
+            {
+                Pickupable pickupable = __instance.gameObject.EnsureComponent<Pickupable>();
+                pickupable.isPickupable = true;
+
+                AquariumFish aquariumFish = __instance.gameObject.EnsureComponent<AquariumFish>();
+                aquariumFish.model = __instance.gameObject;
+
+                Eatable eatable = __instance.gameObject.EnsureComponent<Eatable>();
+                switch (techType)
+                {
+                    case TechType.Bleeder:
+                        eatable.foodValue = -1f;
+                        eatable.waterValue = -5f;
+                        break;
+                    case TechType.Biter:
+                        eatable.foodValue = 12f;
+                        eatable.waterValue = 1f;
+                        break;
+                    case TechType.Blighter:
+                        eatable.foodValue = 13f;
+                        eatable.waterValue = 2f;
+                        break;
+                    default:
+                        eatable.foodValue = 10f;
+                        eatable.waterValue = 4f;
+                        break;
                 }
+
             }
         }
     }
