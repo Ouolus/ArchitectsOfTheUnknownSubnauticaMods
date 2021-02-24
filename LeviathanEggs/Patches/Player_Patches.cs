762dcf39a412e9268428820698e057aa16e990c3
Metious <71298690+Metious@users.noreply.github.com>
2021-02-24 09:55:55 +0330

    fixed a bug where using the `item` command wont work for tweaked creatures

---
+++ LeviathanEggs/Patches/Player_Patches.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using HarmonyLib;
+using UWE;
+using UnityEngine;
+namespace LeviathanEggs.Patches
+{
+    [HarmonyPatch(typeof(Player))]
+    class Player_Patches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Player.Awake))]
+        static void Postfix()
+        {
+            foreach (var techType in Main.TechTypesToTweak)
+                CoroutineHost.StartCoroutine(TweakCreatures(techType));
+        }
+        static IEnumerator TweakCreatures(TechType techType)
+        {
+            yield return new WaitForSeconds(10f);
+
+            CoroutineTask<GameObject> task = CraftData.GetPrefabForTechTypeAsync(techType, false);
+            yield return task;
+
+            GameObject prefab = task.GetResult();
+            if (prefab != null)
+            {
+                Pickupable pickupable = prefab.EnsureComponent<Pickupable>();
+                pickupable.isPickupable = true;
+            }
+
+            yield break;
+        }
+    }
+}
