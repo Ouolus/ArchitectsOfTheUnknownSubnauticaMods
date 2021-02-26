46ff190d08b278af295e36ca12e532e36197f91d
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-02-26 15:42:28 -0500

    prototype for ancients mod

---
+++ ProjectAncients/ProjectAncients/Mod.cs
@@ -0,0 +1,31 @@
+﻿using QModManager.API.ModLoading;
+using ECCLibrary;
+using UnityEngine;
+using System.Reflection;
+using ProjectAncients.Prefabs;
+using SMLHelper.V2.Handlers;
+
+namespace ProjectAncients
+{
+    [QModCore]
+    public class Mod
+    {
+        public static AssetBundle assetBundle;
+
+        public static GargantuanPrefab gargantuanPrefab;
+
+        private const string assetBundleName = "projectancientsassets";
+
+        [QModPatch]
+        public static void Patch()
+        {
+            assetBundle = ECCHelpers.LoadAssetBundleFromAssetsFolder(Assembly.GetExecutingAssembly(), assetBundleName);
+            ECCAudio.RegisterClips(assetBundle);
+
+            LanguageHandler.SetLanguageLine("EncyPath_Lifeforms/Fauna/Titans", "Titans");
+
+            gargantuanPrefab = new GargantuanPrefab("gargantuanleviathan", "Gargantuan leviathan", "A titan-class lifeform. How did it get in your inventory?", assetBundle.LoadAsset<GameObject>("Garg_Prefab"), null);
+            gargantuanPrefab.Patch();
+        }
+    }
+}
