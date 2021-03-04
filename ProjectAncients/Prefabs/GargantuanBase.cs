9d5fa7a454029868fc0da28cb9f6cac6c456d6fc
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-04 15:01:56 -0500

    more aggression

--- ProjectAncients/Prefabs/GargantuanBase.cs
+++ ProjectAncients/Prefabs/GargantuanBase.cs
@@ -37,7 +37,7 @@ namespace ProjectAncients.Prefabs
 
         public override bool EnableAggression => true;
 
-        public override AttackLastTargetSettings AttackSettings => new AttackLastTargetSettings(0.4f, 15f, 16f, 20f, 60f, 30f);
+        public override AttackLastTargetSettings AttackSettings => new AttackLastTargetSettings(0.4f, 20f, 25f, 30f, 60f, 30f);
 
         public override float Mass => 10000f;
 
@@ -90,9 +90,9 @@ namespace ProjectAncients.Prefabs
             CreateTrail(prefab.SearchChild("LJT"), components, jawTentacleSnapSpeed);
             CreateTrail(prefab.SearchChild("RJT"), components, jawTentacleSnapSpeed);
 
-            MakeAggressiveTo(45f, 2, EcoTargetType.Shark, 0.2f, 2.3f);
-            MakeAggressiveTo(35f, 2, EcoTargetType.Whale, 0.2f, 2f);
-            MakeAggressiveTo(100f, 3, EcoTargetType.Leviathan, 0.3f, 5f);
+            MakeAggressiveTo(50f, 2, EcoTargetType.Shark, 0.2f, 2f);
+            MakeAggressiveTo(50f, 2, EcoTargetType.Whale, 0.23f, 2.3f);
+            MakeAggressiveTo(250f, 7, EcoTargetType.Leviathan, 0.3f, 5f);
 
             GargantuanBehaviour gargBehaviour = prefab.AddComponent<GargantuanBehaviour>();
             gargBehaviour.creature = components.creature;
