b04a44fe7b7ce901d80b2459ad557e283961d111
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-04 14:30:24 -0500

    void ambience

---
+++ ProjectAncients/Patches/WaterAmbience_Patch.cs
@@ -0,0 +1,20 @@
+﻿using ECCLibrary;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ProjectAncients.Patches
+{
+    [HarmonyPatch]
+    class WaterAmbience_Patches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(WaterAmbience), nameof(WaterAmbience.Start))]
+        public static void Start_Postfix(WaterAmbience __instance)
+        {
+            GameObject ambienceParent = __instance.gameObject.SearchChild("background");
+            GameObject voidAmbience = GameObject.Instantiate(ambienceParent.SearchChild("arcticAmbience"), ambienceParent.transform);
+            voidAmbience.name = "voidAmbience";
+            voidAmbience.GetComponent<FMODGameParams>().onlyInBiome = "void";
+        }
+    }
+}
