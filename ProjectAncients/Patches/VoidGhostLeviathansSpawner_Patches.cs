57a233bd60a0e7e57a1fd2cf6f9bddabea7d3f75
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-03 21:44:18 -0500

    Ghost leviathans can now be targeted by adult gargs

---
+++ ProjectAncients/Patches/VoidGhostLeviathansSpawner_Patches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ProjectAncients.Patches
+{
+    [HarmonyPatch]
+    class VoidGhostLeviathansSpawner_Patches
+    {
+        [HarmonyPrefix] //Prefix because ghost leviathans can technically spawn during the Start method.
+        [HarmonyPatch(typeof(VoidGhostLeviathansSpawner), nameof(VoidGhostLeviathansSpawner.Start))]
+        public static void Start_Prefix(VoidGhostLeviathansSpawner __instance) //This method modifies the void ghost leviathans, so the Gargantuan can target them.
+        {
+            var ecoTarget = __instance.ghostLeviathanPrefab.EnsureComponent<EcoTarget>();
+            ecoTarget.type = EcoTargetType.Leviathan;
+        }
+    }
+}
