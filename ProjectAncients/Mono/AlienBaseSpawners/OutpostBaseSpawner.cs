b7f1570276fc489d15038b2e0e34ac3c12c09535
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-06 23:57:18 -0500

    final design for outpost base??

--- ProjectAncients/Mono/AlienBaseSpawners/OutpostBaseSpawner.cs
+++ ProjectAncients/Mono/AlienBaseSpawners/OutpostBaseSpawner.cs
@@ -6,11 +6,8 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
     {
         public override void ConstructBase()
         {
-            SpawnPrefabsArray(box2x2x2, 2f, new Vector3(7, 1, 7), Vector3.one * 3f, new Vector3(0f, 0f, 0f));
-            SpawnPrefab(light_small, new Vector3(-21f, 3f, -21f));
-            SpawnPrefab(light_small, new Vector3(-21f, 3f, 21f));
-            SpawnPrefab(light_small, new Vector3(21f, 3f, -21f));
-            SpawnPrefab(light_small, new Vector3(21f, 3f, 21f));
+            SpawnPrefab(outpost1, Vector3.zero);
+            SpawnPrefab(Mod.outpostABTerminal.ClassID, Vector3.up * 10.5f);
         }
     }
 }
