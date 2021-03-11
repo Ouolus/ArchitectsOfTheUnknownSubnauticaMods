1391298b4ff181d5ecbdf3b7daab0c05ee094734
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-11 12:13:25 -0500

    fixed incorrect negative

--- ProjectAncients/Mono/AlienBaseSpawners/TowerOutpostSpawner.cs
+++ ProjectAncients/Mono/AlienBaseSpawners/TowerOutpostSpawner.cs
@@ -10,7 +10,7 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
             SpawnColumns(-24f);
             SpawnColumns(-32f);
             SpawnColumns(-40f);
-            GenerateCable(new Vector3(-527, -49, 28), Vector3.forward, new Vector3(-546.6f, -61, -50.6f), Vector3.up, Vector3.down, 15f);
+            GenerateCable(new Vector3(-527, -49, 28), Vector3.forward, new Vector3(-546.6f, -61, 50.6f), Vector3.down, Vector3.down, 15f);
             GenerateCable(new Vector3(-526, -49, 16), Vector3.back, new Vector3(-516, -79, -30), Vector3.down, Vector3.down, 15f);
         }
     }
