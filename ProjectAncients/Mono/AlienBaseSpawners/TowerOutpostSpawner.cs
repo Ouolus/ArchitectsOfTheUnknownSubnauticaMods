7b4044234ccfb417270b52269323eed39ddf2fdd
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-11 12:11:26 -0500

    added a couple cables to the grassy plateaus tower outpost

--- ProjectAncients/Mono/AlienBaseSpawners/TowerOutpostSpawner.cs
+++ ProjectAncients/Mono/AlienBaseSpawners/TowerOutpostSpawner.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEngine;
 
 namespace ProjectAncients.Mono.AlienBaseSpawners
 {
@@ -14,6 +10,8 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
             SpawnColumns(-24f);
             SpawnColumns(-32f);
             SpawnColumns(-40f);
+            GenerateCable(new Vector3(-527, -49, 28), Vector3.forward, new Vector3(-546.6f, -61, -50.6f), Vector3.up, Vector3.down, 15f);
+            GenerateCable(new Vector3(-526, -49, 16), Vector3.back, new Vector3(-516, -79, -30), Vector3.down, Vector3.down, 15f);
         }
     }
 }
