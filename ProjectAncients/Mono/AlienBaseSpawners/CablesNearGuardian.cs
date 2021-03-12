012f47df941fa211cc64252f72a9a4ae72eadd64
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-12 16:11:14 -0500

    ambient lights around the guardian

--- ProjectAncients/Mono/AlienBaseSpawners/CablesNearGuardian.cs
+++ ProjectAncients/Mono/AlienBaseSpawners/CablesNearGuardian.cs
@@ -8,6 +8,11 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
         {
             GenerateCable(new Vector3(354, -384, -1768), Vector3.up, new Vector3(356, -348, -1756), new Vector3(-0.2f, 0, 1), Vector3.back, 10f); //when you're facing the guardian from the void, this is the one on the left
             GenerateCable(new Vector3(382, -386, -1759), Vector3.up, new Vector3(376, -351, -1748), new Vector3(-0.5f, 0.1f, 0.9f), Vector3.back, 10f); //one on the right
+            SpawnPrefabGlobally(ambience_greenLight, new Vector3(356, -336, -1747));
+            SpawnPrefabGlobally(ambience_greenLight, new Vector3(362, -335, -1740));
+            SpawnPrefabGlobally(ambience_greenLight, new Vector3(371, -338, -1747));
+            SpawnPrefabGlobally(ambience_greenLight, new Vector3(371, -341, -1753));
+            SpawnPrefabGlobally(ambience_greenLight, new Vector3(366, -344, -1753));
         }
     }
 }
