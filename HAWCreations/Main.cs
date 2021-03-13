bdd4efffc92d7ad32729c4245458e4fc552e24d7
Metious <71298690+Metious@users.noreply.github.com>
2021-03-13 19:52:51 +0330

    basic setup

---
+++ HAWCreations/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Reflection;
+using HarmonyLib;
+using QModManager.API.ModLoading;
+
+namespace HAWCreations
+{
+    [QModCore]
+    public static class Main
+    {
+        internal static Assembly myAssembly = Assembly.GetExecutingAssembly();
+        internal static string modPath = myAssembly.Location;
+        internal static string AssetsFolder = Path.Combine(modPath, "Assets");
+        [QModPatch]
+        public static void Load()
+        {
+            Harmony.CreateAndPatchAll(myAssembly, $"HAW_{myAssembly.GetName().Name}");
+        }
+    }
+}
