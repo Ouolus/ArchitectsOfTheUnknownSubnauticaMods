ac8530c51b6071217e38b5707b7456e6913f0ebf
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-14 16:59:23 -0400

    wip code for infection tester

--- ProjectAncients/Prefabs/AlienBase/InfectionTesterTerminal.cs
+++ ProjectAncients/Prefabs/AlienBase/InfectionTesterTerminal.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SMLHelper.V2.Assets;
+using UnityEngine;
+using UWE;
 
 namespace ProjectAncients.Prefabs.AlienBase
 {
-    class InfectionTesterTerminal
+    public class InfectionTesterTerminal : Spawnable
     {
+        const string baseClassId = "b1f54987-4652-4f62-a983-4bf3029f8c5b";
+
+        public InfectionTesterTerminal(string classId)
+            : base(classId, "Forcefield Control", ".")
+        {
+        }
+
+        public override WorldEntityInfo EntityInfo => new WorldEntityInfo()
+        {
+            classId = ClassID,
+            cellLevel = LargeWorldEntity.CellLevel.Medium,
+            localScale = Vector3.one,
+            slotType = EntitySlot.Type.Large,
+            techType = this.TechType
+        };
+
+        public override GameObject GetGameObject()
+        {
+            PrefabDatabase.TryGetPrefab(baseClassId, out GameObject prefab);
+            GameObject obj = GameObject.Instantiate(prefab);
+            PrecursorDisableGunTerminal disableGun = obj.GetComponentInChildren<PrecursorDisableGunTerminal>();
+            obj.SetActive(false);
+            return obj;
+        }
     }
 }
