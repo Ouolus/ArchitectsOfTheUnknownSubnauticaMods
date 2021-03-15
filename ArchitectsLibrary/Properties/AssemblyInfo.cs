e839be87cd49b4242f839b9411aeb052b692af08
Metious <71298690+Metious@users.noreply.github.com>
2021-03-15 15:47:47 +0330

    Renamed the proejct

--- ArchitectsLibrary/Properties/AssemblyInfo.cs
+++ ArchitectsLibrary/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HAWCreations")]
+[assembly: AssemblyTitle("ArchitectsLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HAWCreations")]
+[assembly: AssemblyProduct("ArchitectsLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
