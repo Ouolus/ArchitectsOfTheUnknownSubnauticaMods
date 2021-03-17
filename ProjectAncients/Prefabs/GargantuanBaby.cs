26c540369860444a1b127d3a62fa0dcd3f398934
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-17 11:25:21 -0400

    garg baby ai improvements

--- ProjectAncients/Prefabs/GargantuanBaby.cs
+++ ProjectAncients/Prefabs/GargantuanBaby.cs
@@ -11,7 +11,8 @@ namespace ProjectAncients.Prefabs
     public class GargantuanBaby : GargantuanBase
     {
         public override SwimRandomData SwimRandomSettings => new SwimRandomData(true, new Vector3(10f, 3f, 10f), 5f, 1f, 0.1f);
-        public override StayAtLeashData StayAtLeashSettings => new StayAtLeashData(0.2f, 10f);
+        public override StayAtLeashData StayAtLeashSettings => new StayAtLeashData(0.39f, 9f);
+        public override AvoidObstaclesData AvoidObstaclesSettings => new AvoidObstaclesData(0.38f, true, 5f);
         public override VFXSurfaceTypes SurfaceType => VFXSurfaceTypes.organic;
         public override AttackLastTargetSettings AttackSettings => new AttackLastTargetSettings(0.4f, 15f, 6f, 7f, 2f, 15f);
         public override WaterParkCreatureParameters WaterParkParameters => new WaterParkCreatureParameters(0.1f, 0.5f, 1f, 2f, false);
@@ -29,11 +30,11 @@ namespace ProjectAncients.Prefabs
         {
             base.AddCustomBehaviour(components);
             CreatureFollowPlayer followPlayer = prefab.AddComponent<CreatureFollowPlayer>();
-            followPlayer.distanceToPlayer = 8f;
+            followPlayer.distanceToPlayer = 7f;
             followPlayer.creature = components.creature;
             followPlayer.maxYPos = -8f;
             prefab.AddComponent<GargantuanBabyTeleport>();
-            components.locomotion.driftFactor = 0.9f;
+            components.locomotion.driftFactor = 1f;
             components.locomotion.forwardRotationSpeed = 0.4f;
             components.locomotion.upRotationSpeed = 3f;
             components.locomotion.maxAcceleration = 15f;
