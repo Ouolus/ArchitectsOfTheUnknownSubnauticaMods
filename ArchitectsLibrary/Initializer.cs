3ba33b0d87e5a62391a1601538fd9480bdfb9a6a
Metious <71298690+Metious@users.noreply.github.com>
2021-03-17 17:46:37 +0330

    added SetCreatureEgg & ACU parameters handlers

---
+++ ArchitectsLibrary/Initializer.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using ArchitectsLibrary.Handlers;
+
+namespace ArchitectsLibrary
+{
+    internal class Initializer
+    {
+        public static void PatchAllDictionaries()
+        {
+            PatchDictionary(WaterParkCreature.creatureEggs, AUHandler.customCreatureEggDictionary);
+            PatchDictionary(WaterParkCreature.waterParkCreatureParameters, AUHandler.customWaterParkCreatureParameters);
+        }
+        
+        private static void PatchDictionary<V>(IDictionary<TechType, V> origDictionary, IDictionary<TechType, V> cusDictionary)
+        {
+            foreach (KeyValuePair<TechType,V> kvp in cusDictionary)
+            {
+                origDictionary[kvp.Key] = kvp.Value;
+            }
+        }
+    }
+}
