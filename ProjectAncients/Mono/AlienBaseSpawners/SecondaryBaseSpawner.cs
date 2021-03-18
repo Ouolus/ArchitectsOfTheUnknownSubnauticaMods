22bace932a2efbb02588a5019f62ba02a30acbd0
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-18 11:13:50 -0400

    moving tablet pedestal

--- ProjectAncients/Mono/AlienBaseSpawners/SecondaryBaseSpawner.cs
+++ ProjectAncients/Mono/AlienBaseSpawners/SecondaryBaseSpawner.cs
@@ -37,8 +37,8 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
             SpawnPrefab(natural_lr_hangingplant1_3, new Vector3(-4f, ceilingLocalY, -1.5f));
             SpawnPrefab(natural_lr_hangingplant1_2, new Vector3(-5f, ceilingLocalY, -5f));
 
-            SpawnPrefab(prop_tabletPedestal, new Vector3(centerLocalX, -3.17f, 19f), Vector3.zero, Vector3.one * 0.5f);
-            SpawnPrefab(TabletClassId, new Vector3(centerLocalX, floorLocalY + 2.35f, 19f), new Vector3(22.5f, 0f, 0f));
+            SpawnPrefab(prop_tabletPedestal, new Vector3(centerLocalX, -3.17f, 17f), Vector3.zero, Vector3.one * 0.5f);
+            SpawnPrefab(TabletClassId, new Vector3(centerLocalX, floorLocalY + 1f, 17f), new Vector3(22.5f, 0f, 0f));
         }
 
         protected virtual string TabletClassId
