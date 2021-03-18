7072126e949e7c34bd75ce0f783f2b0a65cc74f1
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-17 21:40:00 -0400

    adult  garg

---
+++ ProjectAncients/Prefabs/AdultGargantuan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ProjectAncients.Prefabs
+{
+    public class AdultGargantuan : GargantuanBase
+    {
+        public AdultGargantuan(string classId, string friendlyName, string description, GameObject model, Texture2D spriteTexture) : base(classId, friendlyName, description, model, spriteTexture)
+        {
+        }
+
+        public override float BiteDamage => 2500f;
+
+        public override string AttachBoneName => "AttachBone";
+
+        public override float VehicleDamagePerSecond => 200f;
+    }
+}
