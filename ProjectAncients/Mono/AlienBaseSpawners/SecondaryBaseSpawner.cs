95725a556113e0616c81f845f3384eb10554e4be
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-19 15:06:35 -0400

    fixed tablet clipping through pedestal and being unpickupable

--- ProjectAncients/Mono/AlienBaseSpawners/SecondaryBaseSpawner.cs
+++ ProjectAncients/Mono/AlienBaseSpawners/SecondaryBaseSpawner.cs
@@ -38,7 +38,7 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
             SpawnPrefab(natural_lr_hangingplant1_2, new Vector3(-5f, ceilingLocalY, -5f));
 
             SpawnPrefab(prop_tabletPedestal, new Vector3(centerLocalX, -3.17f, 17f), Vector3.zero, Vector3.one * 0.5f);
-            SpawnPrefab(TabletClassId, new Vector3(centerLocalX, floorLocalY + 1f, 17f), new Vector3(22.5f, 0f, 0f));
+            SpawnPrefab(TabletClassId, new Vector3(centerLocalX, floorLocalY + 1.07f, 17f), new Vector3(22.5f, 0f, 0f));
         }
 
         protected virtual string TabletClassId
