3a35c0e00328e03c6df621f34a2baf6d78facbde
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-19 11:51:58 -0400

    dont despawn adult garg if player is in precursor base or observatory

--- ProjectAncients/Mono/Singletons/VoidGargSingleton.cs
+++ ProjectAncients/Mono/Singletons/VoidGargSingleton.cs
@@ -32,7 +32,8 @@ namespace ProjectAncients.Mono
 
 		void CheckDistance()
 		{
-			if (!VoidGargSpawner.IsVoidBiome(Player.main.GetBiomeString()))
+			string playerBiome = Player.main.GetBiomeString();
+			if (!VoidGargSpawner.IsVoidBiome(playerBiome) && !playerBiome.StartsWith("precursor", System.StringComparison.OrdinalIgnoreCase) && !playerBiome.StartsWith("observatory", System.StringComparison.OrdinalIgnoreCase))
 			{
 				float distance = Vector3.Distance(MainCameraControl.main.transform.position, transform.position);
 				if (distance > 250f)
