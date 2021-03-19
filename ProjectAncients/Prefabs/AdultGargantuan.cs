71c71750b1b055960213911805c5b8b071c02d26
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-19 11:54:30 -0400

    updated shader settings for adult and made tentacles a bit more rigid

--- ProjectAncients/Prefabs/AdultGargantuan.cs
+++ ProjectAncients/Prefabs/AdultGargantuan.cs
@@ -22,12 +22,21 @@ namespace ProjectAncients.Prefabs
 
         public override bool OneShotsPlayer => true;
 
+        public override float TentacleSnapSpeed => 8f;
+
         public override void AddCustomBehaviour(CreatureComponents components)
         {
             base.AddCustomBehaviour(components);
             Renderer renderer = prefab.SearchChild("Gargantuan.001").GetComponent<SkinnedMeshRenderer>();
-            renderer.materials[1].SetFloat("_EmissionLM", 1.5f);
-            renderer.materials[1].SetFloat("_EmissionLMNight", 1.5f);
+            UpdateGargMaterial(renderer.materials[0]);
+            UpdateGargMaterial(renderer.materials[1]);
+            UpdateGargMaterial(renderer.materials[2]);
+        }
+
+        void UpdateGargMaterial(Material material)
+        {
+            material.SetFloat("_EmissionLM", 1.5f);
+            material.SetFloat("_EmissionLMNight", 1.5f);
         }
 
         public override bool CanPerformCyclopsCinematic => true;
