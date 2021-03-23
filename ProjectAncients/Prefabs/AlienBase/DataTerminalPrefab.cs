bb11e63b6874cae46f919bba314c74368d6b4626
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-23 17:54:05 -0400

    support for custom delay in data terminals

--- ProjectAncients/Prefabs/AlienBase/DataTerminalPrefab.cs
+++ ProjectAncients/Prefabs/AlienBase/DataTerminalPrefab.cs
@@ -13,13 +13,13 @@ namespace ProjectAncients.Prefabs.AlienBase
         private string[] pingClassId;
         private string audioClipPrefix;
         private Vector3 pingPosition;
-        private float delay = 7f;
+        private float delay = 5f;
         private TechType techToUnlock;
         public const string greenTerminalCID = "625d01c2-40b7-4c87-a1cc-493ad6101c34";
         public const string orangeTerminalCID = "dd3bf908-badb-4c8c-a195-eb50be09df63";
         public const string blueTerminalCID = "b629c806-d3cd-4ee4-ae99-7b1359b60049";
 
-        public DataTerminalPrefab(string classId, string encyKey, string[] pingClassId = default, string audioClipPrefix = "DataTerminal1", string terminalClassId = blueTerminalCID, TechType techToUnlock = TechType.None)
+        public DataTerminalPrefab(string classId, string encyKey, string[] pingClassId = default, string audioClipPrefix = "DataTerminal1", string terminalClassId = blueTerminalCID, TechType techToUnlock = TechType.None, float delay = 5f)
             : base(classId, "Data terminal", ".")
         {
             this.encyKey = encyKey;
@@ -27,6 +27,7 @@ namespace ProjectAncients.Prefabs.AlienBase
             this.pingClassId = pingClassId;
             this.audioClipPrefix = audioClipPrefix;
             this.techToUnlock = techToUnlock;
+            this.delay = delay;
         }
 
         public override WorldEntityInfo EntityInfo => new WorldEntityInfo()
@@ -43,7 +44,14 @@ namespace ProjectAncients.Prefabs.AlienBase
             PrefabDatabase.TryGetPrefab(terminalClassId, out GameObject prefab);
             GameObject obj = GameObject.Instantiate(prefab);
             StoryHandTarget storyHandTarget = obj.GetComponent<StoryHandTarget>();
-            storyHandTarget.goal = new Story.StoryGoal(encyKey, Story.GoalType.Encyclopedia, delay);
+            if (!string.IsNullOrEmpty(encyKey))
+            {
+                storyHandTarget.goal = new Story.StoryGoal(encyKey, Story.GoalType.Encyclopedia, delay);
+            }
+            else
+            {
+                storyHandTarget.goal = null;
+            }
             obj.SetActive(false);
             if (pingClassId != null && pingClassId.Length > 0)
             {
