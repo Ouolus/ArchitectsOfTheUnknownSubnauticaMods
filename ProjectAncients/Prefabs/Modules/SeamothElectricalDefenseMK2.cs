d748408767a5b6860bb5cd2ad59c52150aad79ed
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-23 11:43:04 -0400

    changed how perimeter defense mk2 is unlocked

--- ProjectAncients/Prefabs/Modules/SeamothElectricalDefenseMK2.cs
+++ ProjectAncients/Prefabs/Modules/SeamothElectricalDefenseMK2.cs
@@ -19,6 +19,7 @@ namespace ProjectAncients.Prefabs.Modules
         public override CraftTree.Type FabricatorType => CraftTree.Type.Workbench;
         public override string[] StepsToFabricatorTab => new string[] { "SeamothMenu" };
         public override TechCategory CategoryForPDA => TechCategory.VehicleUpgrades;
+        public override TechType RequiredForUnlock => Mod.architectElectricityMasterTech;
 
         #region Interface Implementation
 
