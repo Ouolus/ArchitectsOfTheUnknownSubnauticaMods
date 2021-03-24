1224803cf2dda0d327c2517195ab76b2b8f4af50
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-24 11:15:29 -0400

    egg base basic prefab

---
+++ ProjectAncients/Mono/AlienBaseSpawners/VoidBaseSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace ProjectAncients.Mono.AlienBaseSpawners
+{
+    public class VoidBaseSpawner : AlienBaseSpawner
+    {
+        public override void ConstructBase()
+        {
+            GameObject baseModel = SpawnPrefab(Mod.voidBaseModel.ClassID, Vector3.zero);
+            GenerateAtmospheres(baseModel, "AtmosphereRoot", atmosphereVolume_cache);
+            SpawnPrefab(airlock_1, new Vector3(0f, 0f, 19.75f), Vector3.one);
+            SpawnPrefab(vfx_entrance, new Vector3(0f, 0f, 19.75f), new Vector3(90f, 0f, 0f), Vector3.one);
+        }
+    }
+}
