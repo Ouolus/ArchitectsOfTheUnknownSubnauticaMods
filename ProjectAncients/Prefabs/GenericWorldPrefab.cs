38991e7808715bfec61efbe16e64b97ea3a40096
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-25 15:43:58 -0400

    voidbase shader settings update

--- ProjectAncients/Prefabs/GenericWorldPrefab.cs
+++ ProjectAncients/Prefabs/GenericWorldPrefab.cs
@@ -34,9 +34,23 @@ namespace ProjectAncients.Prefabs
                 ECCHelpers.ApplySNShaders(prefab, materialProperties);
                 foreach (Renderer renderer in prefab.GetComponentsInChildren<Renderer>())
                 {
-                    renderer.material.SetColor("_SpecColor", new Color(0.25f, 0.54f, 0.41f));
-                    renderer.material.SetFloat("_SpecInt", 8f);
-                    renderer.material.SetFloat("_Fresnel", 0.4f);
+                    Material material = renderer.material;
+                    material.SetColor("_SpecColor", new Color(0.25f, 0.54f, 0.41f));
+                    material.SetFloat("_SpecInt", 8f);
+                    material.SetFloat("_Fresnel", 0.4f);
+                    if(ECCHelpers.CompareStrings(material.name, "Transparent", ECCStringComparison.Contains))
+                    {
+                        material.SetFloat("_SrcBlend", 5f);
+                        material.SetFloat("_DstBlend", 10f);
+                        material.SetFloat("_SrcBlend2", 5f);
+                        material.SetFloat("_DstBlend2", 10f);
+                        material.SetFloat("_AddSrcBlend", 5f);
+                        material.SetFloat("_AddDstBlend", 1f);
+                        material.SetFloat("_AddSrcBlend2", 5f);
+                        material.SetFloat("_AddDstBlend2", 1f);
+                        material.SetInt("_ZWrite", 0);
+                        material.EnableKeyword("MARMO_SIMPLE_GLASS");
+                    }
                 }
                 foreach(Collider col in prefab.GetComponentsInChildren<Collider>())
                 {
