55fbd9f563a63902152701eae0598e2dbeef32b8
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-25 14:12:47 -0400

    custom forcefield for interior areas

---
+++ ProjectAncients/Prefabs/AlienBase/VoidInteriorForcefield.cs
@@ -0,0 +1,32 @@
+﻿using SMLHelper.V2.Assets;
+using ECCLibrary;
+using ProjectAncients.Mono;
+using UnityEngine;
+using UWE;
+using System.Collections;
+
+namespace ProjectAncients.Prefabs.AlienBase
+{
+    public class VoidInteriorForcefield : Spawnable
+    {
+        static GameObject prefab;
+        public VoidInteriorForcefield() : base("VoidInteriorForcefield", "Forcefield", ".")
+        {
+        }
+
+        public override IEnumerator GetGameObjectAsync(IOut<GameObject> gameObject)
+        {
+            if(prefab == null)
+            {
+                IPrefabRequest request = PrefabDatabase.GetPrefabAsync("4ea69565-60e4-4554-bbdb-671eaba6dffb");
+                yield return request;
+                if(request.TryGetPrefab(out GameObject requestObj))
+                {
+                    prefab = GameObject.Instantiate(requestObj);
+                    prefab.SetActive(false);
+                }
+            }
+
+        }
+    }
+}
