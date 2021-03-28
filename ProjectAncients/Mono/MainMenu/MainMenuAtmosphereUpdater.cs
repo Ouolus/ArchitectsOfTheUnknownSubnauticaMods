27d3acdc51e7a74d763f7b2d84dc174838b934fb
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-28 13:30:24 -0400

    garg roars in main menu

--- ProjectAncients/Mono/MainMenu/MainMenuAtmosphereUpdater.cs
+++ ProjectAncients/Mono/MainMenu/MainMenuAtmosphereUpdater.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using ECCLibrary;
 
 namespace ProjectAncients.Mono
 {
     public class MainMenuAtmosphereUpdater : MonoBehaviour
     {
+        ECCAudio.AudioClipPool clipPool;
+        AudioSource source;
         void Start()
         {
             uSkyManager skyManager = FindObjectOfType<uSkyManager>();
@@ -11,6 +16,29 @@ namespace ProjectAncients.Mono
             skyManager.StarIntensity = 2f;
             skyManager.NightSky = uSkyManager.NightModes.Rotation;
             skyManager.planetRadius = 500f;
+            clipPool = ECCAudio.CreateClipPool("garg_for_anth_distant");
+            source = gameObject.AddComponent<AudioSource>();
+            source.volume = ECCHelpers.GetECCVolume();
+            StartCoroutine(RoarLoop());
+        }
+
+        IEnumerator RoarLoop()
+        {
+            yield return new WaitForSeconds(6f);
+            PlayRoar();
+            for(; ; )
+            {
+                yield return new WaitForSeconds(Random.Range(22f, 34f));
+                PlayRoar();
+            }
+        }
+
+        void PlayRoar()
+        {
+            AudioClip nextClip = clipPool.GetRandomClip();
+            source.volume = ECCHelpers.GetECCVolume();
+            source.clip = nextClip;
+            source.Play();
         }
     }
 }
