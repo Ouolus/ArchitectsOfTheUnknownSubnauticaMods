bb1e38c42f09de6e1bdfe89cbe3be44d35aac99e
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-27 23:46:12 -0400

    spooooky main menu

---
+++ ProjectAncients/Patches/MainMenuMusic_Patch.cs
@@ -0,0 +1,37 @@
+﻿using ECCLibrary;
+using ECCLibrary.Internal;
+using HarmonyLib;
+using ProjectAncients.Prefabs;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ProjectAncients.Patches
+{
+    [HarmonyPatch(typeof(MainMenuMusic))]
+    public class MainMenuMusic_Patch
+    {
+        [HarmonyPatch(nameof(MainMenuMusic.Start))]
+        [HarmonyPrefix]
+        public static void Prefix(MainMenuMusic __instance)
+        {
+            FMODAsset bloodKelpMusic = ScriptableObject.CreateInstance<FMODAsset>();
+            bloodKelpMusic.path = "event:/env/music/blood_kelp_background_music";
+            //bloodKelpMusic.id = "{644c6e32-9488-46dc-b223-ae50a312432f}";
+            __instance.music = bloodKelpMusic;
+
+            Light[] lights = Object.FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.name == "Directional Light")
+                {
+                    light.transform.forward = Vector3.up;
+                }
+                else
+                {
+                    //highlight light
+                }
+            }
+        }
+    }
+}
