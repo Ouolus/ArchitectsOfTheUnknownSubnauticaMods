ec24b7f54cc2ab94ca697303426ca6313d860087
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-27 22:06:02 -0400

    more bone

--- ProjectAncients/Mono/AlienBaseSpawners/ResearchBaseSpawner.cs
+++ ProjectAncients/Mono/AlienBaseSpawners/ResearchBaseSpawner.cs
@@ -18,8 +18,10 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
             SpawnPrefabGlobally(bone_curly, new Vector3(-891.40f, -197.51f, -597.30f), Vector3.up, false);
             SpawnPrefabGlobally(bone_generic1, new Vector3(-861.62f, -193.75f, -575.27f), new Vector3(6f, 0f, 9f), Vector3.one);
             SpawnPrefabGlobally(bone_generic2, new Vector3(-836.00f, -190.14f, -569.54f), new Vector3(340.99f, 357.34f, 15.78f), Vector3.one);
-            SpawnPrefabGlobally(bone_reaperSkull, new Vector3(-815.32f, -192.80f, -598.73f), new Vector3(349.11f, 358.48f, 15.82f), Vector3.one);
+            SpawnPrefabGlobally(bone_reaperSkull, new Vector3(-815.32f, -192.80f, -598.73f), new Vector3(310.89f, 240.00f, 0f), Vector3.one);
             SpawnPrefabGlobally(bone_reaperFullRibcage_chipped, new Vector3(-805.79f, -183.85f, -576.63f), new Vector3(335.11f, 358.04f, 8.85f), Vector3.one);
+            SpawnPrefabGlobally(bone_reaperMandible, new Vector3(-815.81f, -196.04f, -610.00f), new Vector3(342.22f, 85.71f, 337.03f), Vector3.one);
+            SpawnPrefabGlobally(bone_reaperMandible, new Vector3(-809.74f, -190f, -587.91f), new Vector3(351.43f, 68.57f, 0f), Vector3.one);
         }
     }
 }
