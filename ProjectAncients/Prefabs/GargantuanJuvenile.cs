4e8784c847b02c9e446325b049d8c96da4272a89
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-27 21:57:29 -0400

    ency title for garg juvenile

--- ProjectAncients/Prefabs/GargantuanJuvenile.cs
+++ ProjectAncients/Prefabs/GargantuanJuvenile.cs
@@ -15,6 +15,8 @@ namespace ProjectAncients.Prefabs
 
         public override ScannableItemData ScannableSettings => new ScannableItemData(true, 10f, "Lifeforms/Fauna/Leviathans", Mod.assetBundle.LoadAsset<Sprite>("Juvenile_Popup"), null);
 
+        public override string GetEncyTitle => "Gargantuan Leviathan Juvenile";
+
         public GargantuanJuvenile(string classId, string friendlyName, string description, GameObject model, Texture2D spriteTexture) : base(classId, friendlyName, description, model, spriteTexture)
         {
         }
