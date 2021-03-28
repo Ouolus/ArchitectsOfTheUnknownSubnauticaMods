3c9f657082a75ed21ba77f75228a73a02128d699
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-28 14:26:52 -0400

    bones field cable enlargement

--- ProjectAncients/Mono/AlienBaseSpawners/BonesFieldsOutpostSpawner.cs
+++ ProjectAncients/Mono/AlienBaseSpawners/BonesFieldsOutpostSpawner.cs
@@ -7,7 +7,7 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
         public override void ConstructBase()
         {
             base.ConstructBase();
-            GenerateCable(new Vector3(-725.3f, -746.2f, -217.6f), Vector3.up, new Vector3(-708.3f, -699.5f, -215.5f), new Vector3(0.7f, 0.7f, 0.2f), Vector3.left, 10f);
+            GenerateCable(new Vector3(-725.3f, -746.2f, -217.6f), Vector3.up, new Vector3(-708.3f, -699.5f, -215.5f), new Vector3(0.7f, 0.7f, 0.2f), Vector3.left, 10f, 2f);
             SpawnPrefab(supplies_orangeTablet, new Vector3(4f, -7.2f, 3f), Vector3.up * 36f);
         }
     }
