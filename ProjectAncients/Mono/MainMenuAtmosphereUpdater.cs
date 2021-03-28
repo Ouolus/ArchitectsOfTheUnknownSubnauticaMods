d0eb9886c1b592169cb24d2802982c39604fc597
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-28 01:02:49 -0400

    atmosphere updater script, not sure if it works

---
+++ ProjectAncients/Mono/MainMenuAtmosphereUpdater.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ProjectAncients.Mono
+{
+    public class MainMenuAtmosphereUpdater : MonoBehaviour
+    {
+        void Start()
+        {
+            Material material = RenderSettings.skybox;
+            material.SetVector("_SunDir", new Vector4(0f, 1f, 0f, 0f));
+        }
+    }
+}
