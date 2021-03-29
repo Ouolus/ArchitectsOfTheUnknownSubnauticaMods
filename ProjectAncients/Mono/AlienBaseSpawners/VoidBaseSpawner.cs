9f8a034600029026618485cc60bb2facb1e7459f
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-03-29 17:15:31 -0400

    organization

--- ProjectAncients/Mono/AlienBaseSpawners/VoidBaseSpawner.cs
+++ ProjectAncients/Mono/AlienBaseSpawners/VoidBaseSpawner.cs
@@ -8,36 +8,37 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
         {
             GameObject baseModel = SpawnPrefab(Mod.voidBaseModel.ClassID, Vector3.zero);
             GenerateAtmospheres(baseModel, "AtmosphereRoot", atmosphereVolume_cache);
-            SpawnPrefab(light_big_animated, new Vector3(0f, -0.5f, 36f), new Vector3(0f, 0f, 0f));
-            SpawnPrefabsArray(light_big_animated, 2f, new Vector3(1, 1, 7), Vector3.one, new Vector3(-8.7f + 0.5f, 0.5f, 35));
-            SpawnPrefabsArray(light_big_animated, 2f, new Vector3(1, 1, 7), Vector3.one, new Vector3(8.7f + 1.5f, 0.5f, 35));
 
-            SpawnPrefabsArray(light_big_animated, 2f, new Vector3(1, 1, 5), Vector3.one, new Vector3(-3f + 0.5f, 0.5f, 18f));
-            SpawnPrefabsArray(light_big_animated, 2f, new Vector3(1, 1, 5), Vector3.one, new Vector3(3f + 1.5f, 0.5f, 18f));
-
-            SpawnPrefabsArray(light_big_animated, 2f, new Vector3(1, 1, 8), Vector3.one, new Vector3(-3f + 0.5f, 11.5f, 14f), Vector3.left * 180f);
-            SpawnPrefabsArray(light_big_animated, 2f, new Vector3(1, 1, 8), Vector3.one, new Vector3(3f + 1.5f, 11.5f, 14f), Vector3.left * 180f);
-
-            const float cableDrop = 12f;
-            GenerateCable(new Vector3(388, -393, -1865), new Vector3(0.7f, 0f, 0.7f), new Vector3(396, -395, -1762), new Vector3(-0.3f, -0.1f, 0.9f), Vector3.right, cableDrop, 2f);
-            GenerateCable(new Vector3(383, -412, -1870), new Vector3(0.7f, 0f, 0.7f), new Vector3(383, -416, -1763), new Vector3(0, 0, 1), Vector3.down, cableDrop, 2f);
-            GenerateCable(new Vector3(381, -424, -1872), new Vector3(0.7f, 0f, 0.7f), new Vector3(370.5f, -426, -1769.7f), new Vector3(0.4f, -0.1f, 0.9f), Vector3.down, cableDrop, 2f);
-            GenerateCable(new Vector3(379, -436, -1874), new Vector3(0.7f, 0f, 0.7f), new Vector3(398, -451, -1769.9f), new Vector3(-0.1f, 0, 1), Vector3.down, cableDrop, 2f);
-
-            GenerateCable(new Vector3(358, -393, -1865), new Vector3(-0.7f, 0f, 0.7f), new Vector3(346.5f, -393, -1765), new Vector3(0f, 0f, 1f), Vector3.right, cableDrop, 2f);
-            GenerateCable(new Vector3(363, -412, -1870), new Vector3(-0.7f, 0f, 0.7f), new Vector3(344f, -418, -1763), new Vector3(0, 0, 1), Vector3.down, cableDrop, 2f);
-            GenerateCable(new Vector3(365, -424, -1872), new Vector3(-0.7f, 0f, 0.7f), new Vector3(338, -427, -1770), new Vector3(0f, 0f, 1f), Vector3.down, cableDrop, 2f);
-            GenerateCable(new Vector3(367, -436, -1874), new Vector3(-0.7f, 0f, 0.7f), new Vector3(340f, -450f, -1771f), new Vector3(0.2f, -0.2f, 0.9f), Vector3.down, cableDrop, 2f);
-
-
+            //Exterior platform
             SpawnPrefab(Mod.voidDoor_red.ClassID, new Vector3(0f, 0f, 24f));
             SpawnPrefab(Mod.voidDoor_orange.ClassID, new Vector3(0f, 0f, 24f));
             SpawnPrefab(Mod.voidDoor_white.ClassID, new Vector3(0f, 0f, 24f));
             SpawnPrefab(Mod.voidDoor_blue.ClassID, new Vector3(0f, 0f, 24f));
             SpawnPrefab(Mod.voidDoor_purple.ClassID, new Vector3(0f, 0f, 24f));
-            
-            SpawnPrefab(Mod.voidDoor_interior.ClassID, new Vector3(0f, 0f, -8f));
+            SpawnPrefab(light_big_animated, new Vector3(0f, -0.5f, 36f), new Vector3(0f, 0f, 0f));
             SpawnPrefab(vfx_entrance, new Vector3(-0.5f, 3f, 24f), new Vector3(90, 0, 0), new Vector3(1.30f, 1.5f, 1f));
+            SpawnPrefabsArray(light_big_animated, 2f, new Vector3(1, 1, 7), Vector3.one, new Vector3(-8.7f + 0.5f, 0.5f, 35));
+            SpawnPrefabsArray(light_big_animated, 2f, new Vector3(1, 1, 7), Vector3.one, new Vector3(8.7f + 1.5f, 0.5f, 35));
+
+            //Entrance hallway
+            SpawnPrefabsArray(light_big_animated, 2f, new Vector3(1, 1, 5), Vector3.one, new Vector3(-3f + 0.5f, 0.5f, 18f));
+            SpawnPrefabsArray(light_big_animated, 2f, new Vector3(1, 1, 5), Vector3.one, new Vector3(3f + 1.5f, 0.5f, 18f));
+            //Entrance hallway ceiling
+            SpawnPrefabsArray(light_big_animated, 2f, new Vector3(1, 1, 8), Vector3.one, new Vector3(-3f + 0.5f, 11.5f, 14f), Vector3.left * 180f);
+            SpawnPrefabsArray(light_big_animated, 2f, new Vector3(1, 1, 8), Vector3.one, new Vector3(3f + 1.5f, 11.5f, 14f), Vector3.left * 180f);
+
+            //Exterior cables
+            const float cableDrop = 12f;
+            //Left side
+            GenerateCable(new Vector3(388, -393, -1865), new Vector3(0.7f, 0f, 0.7f), new Vector3(396, -395, -1762), new Vector3(-0.3f, -0.1f, 0.9f), Vector3.right, cableDrop, 2f);
+            GenerateCable(new Vector3(383, -412, -1870), new Vector3(0.7f, 0f, 0.7f), new Vector3(383, -416, -1763), new Vector3(0, 0, 1), Vector3.down, cableDrop, 2f);
+            GenerateCable(new Vector3(381, -424, -1872), new Vector3(0.7f, 0f, 0.7f), new Vector3(370.5f, -426, -1769.7f), new Vector3(0.4f, -0.1f, 0.9f), Vector3.down, cableDrop, 2f);
+            GenerateCable(new Vector3(379, -436, -1874), new Vector3(0.7f, 0f, 0.7f), new Vector3(398, -451, -1769.9f), new Vector3(-0.1f, 0, 1), Vector3.down, cableDrop, 2f);
+            //Right side
+            GenerateCable(new Vector3(358, -393, -1865), new Vector3(-0.7f, 0f, 0.7f), new Vector3(346.5f, -393, -1765), new Vector3(0f, 0f, 1f), Vector3.right, cableDrop, 2f);
+            GenerateCable(new Vector3(363, -412, -1870), new Vector3(-0.7f, 0f, 0.7f), new Vector3(344f, -418, -1763), new Vector3(0, 0, 1), Vector3.down, cableDrop, 2f);
+            GenerateCable(new Vector3(365, -424, -1872), new Vector3(-0.7f, 0f, 0.7f), new Vector3(338, -427, -1770), new Vector3(0f, 0f, 1f), Vector3.down, cableDrop, 2f);
+            GenerateCable(new Vector3(367, -436, -1874), new Vector3(-0.7f, 0f, 0.7f), new Vector3(340f, -450f, -1771f), new Vector3(0.2f, -0.2f, 0.9f), Vector3.down, cableDrop, 2f);
 
             //Aquarium
             SpawnPrefab(vfx_entrance, new Vector3(0f, -0.345f, 0f), new Vector3(0f, 0f, 0f), new Vector3(2f, 1f, 2f));
@@ -50,11 +51,11 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
             SpawnPrefab(natural_rockBlade1, new Vector3(3f, -10f, 6f), Vector3.up * 135f, Vector3.one * 0.21f);
             SpawnPrefab(natural_rockBlade2, new Vector3(2f, -10f, -5f), Vector3.up * -60f, Vector3.one * 0.22f);
 
-
             //Supplies
             SpawnPrefab(supplies_purpleTablet, new Vector3(13f, -7.3f, -7.5f), Vector3.up * 64f);
 
-            //Egg stuff
+            //Egg room
+            SpawnPrefab(Mod.voidDoor_interior.ClassID, new Vector3(0f, 0f, -8f));
             SpawnPrefab(structure_specialPlatform, new Vector3(0f, 0f, -16f));
             SpawnPrefab(Mod.gargEgg.ClassID, new Vector3(0f, 1f, -16f));
             SpawnPrefab(light_small, new Vector3(2.5f, 0f, -13.5f));
@@ -68,10 +69,10 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
             //Left room
             SpawnPrefab(supplies_purpleTablet, new Vector3(15.45f, 0f, -2.19f), Vector3.up * -68f);
 
-            GenerateCable(new Vector3(373.11f + 6f, -389.00f, -1896.07f + 6f), Vector3.down, new Vector3(373.11f + 6f, -399.00f - 1f, -1896.97f + 6f), Vector3.down, Vector3.zero, 0f, 0.5f);
-            GenerateCable(new Vector3(373.11f - 6f, -389.00f, -1896.07f + 6f), Vector3.down, new Vector3(373.11f - 6f, -399.00f - 1f, -1896.97f + 6f), Vector3.down, Vector3.zero, 0f, 0.5f);
-            GenerateCable(new Vector3(373.11f + 6f, -389.00f, -1896.07f - 6f), Vector3.down, new Vector3(373.11f + 6f, -399.00f - 1f, -1896.97f - 6f), Vector3.down, Vector3.zero, 0f, 0.5f);
-            GenerateCable(new Vector3(373.11f - 6f, -389.00f, -1896.07f - 6f), Vector3.down, new Vector3(373.11f - 6f, -399.00f - 1f, -1896.97f - 6f), Vector3.down, Vector3.zero, 0f, 0.5f);
+            GenerateCable(new Vector3(373.11f + 6f, -389.00f, -1896.07f + 5f), Vector3.down, new Vector3(373.11f + 6f, -399.00f - 1f, -1896.97f + 5f), Vector3.down, Vector3.zero, 0f, 0.5f);
+            GenerateCable(new Vector3(373.11f - 6f, -389.00f, -1896.07f + 5f), Vector3.down, new Vector3(373.11f - 6f, -399.00f - 1f, -1896.97f + 5f), Vector3.down, Vector3.zero, 0f, 0.5f);
+            GenerateCable(new Vector3(373.11f + 6f, -389.00f, -1896.07f - 5f), Vector3.down, new Vector3(373.11f + 6f, -399.00f - 1f, -1896.97f - 5f), Vector3.down, Vector3.zero, 0f, 0.5f);
+            GenerateCable(new Vector3(373.11f - 6f, -389.00f, -1896.07f - 5f), Vector3.down, new Vector3(373.11f - 6f, -399.00f - 1f, -1896.97f - 5f), Vector3.down, Vector3.zero, 0f, 0.5f);
         }
     }
 }
