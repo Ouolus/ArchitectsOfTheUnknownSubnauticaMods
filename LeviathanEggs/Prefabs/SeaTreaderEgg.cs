7b5da4d4200164fa082c32175e8f30692a13964d
Metious <71298690+Metious@users.noreply.github.com>
2021-04-02 01:32:33 +0430

    added the new SeaTreaderEgg model

--- LeviathanEggs/Prefabs/SeaTreaderEgg.cs
+++ LeviathanEggs/Prefabs/SeaTreaderEgg.cs
@@ -13,33 +13,11 @@ namespace LeviathanEggs.Prefabs
             LateEnhancements += InitializeObject;
         }
 
-        public override GameObject Model => LoadGameObject("RobotEgg");
+        public override GameObject Model => LoadGameObject("SeaTreaderEgg.prefab");
         public override TechType HatchingCreature => TechType.SeaTreader;
         public override float HatchingTime => 5f;
         public override Sprite ItemSprite => LoadSprite("RobotEgg");
 
-        public void InitializeObject(GameObject prefab)
-        {
-            Material material = new Material(Shader.Find("MarmosetUBER"))
-            {
-                mainTexture = LoadTexture2D("RobotEggDiffuse"),
-            };
-            material.EnableKeyword("MARMO_NORMALMAP");
-            material.EnableKeyword("MARMO_SPECMAP");
-            material.EnableKeyword("MARMO_EMISSION");
-
-            material.SetTexture(ShaderPropertyID._Illum, LoadTexture2D("RobotEggIllum"));
-            material.SetTexture(ShaderPropertyID._SpecTex, LoadTexture2D("RobotEggDiffuse"));
-            material.SetTexture(ShaderPropertyID._BumpMap, LoadTexture2D("RobotEggNormal"));
-
-            Renderer[] renderers = prefab.GetAllComponentsInChildren<Renderer>();
-            foreach (var rend in renderers)
-            {
-                rend.material = material;
-                rend.sharedMaterial = material;
-            }
-
-            prefab.AddComponent<SpawnLocations>();
-        }
+        public void InitializeObject(GameObject prefab) => prefab.AddComponent<SpawnLocations>();
     }
 }
