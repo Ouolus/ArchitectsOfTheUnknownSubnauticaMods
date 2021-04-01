9e063f8d8b4daeae1d815a0b1027154c3951d557
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-01 09:57:45 -0400

    garg secret command and ency improvements

---
+++ ProjectAncients/Mono/Commands/SecretCommand.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ProjectAncients.Mono.Commands
+{
+    public class SecretCommand : MonoBehaviour
+    {
+        void Start()
+        {
+            DevConsole.RegisterConsoleCommand(this, "garg", false, true);
+        }
+
+        private void OnConsoleCommand_garg(NotificationCenter.Notification n)
+        {
+            GameObject garg = GameObject.Find("GargantuanVoid(Clone)");
+            garg.FindChild("AdultGargModel").SetActive(false);
+            GameObject trollFace = garg.FindChild("TrollFace");
+            trollFace.SetActive(true);
+            trollFace.AddComponent<GargEyeTracker>();
+        }
+    }
+}
