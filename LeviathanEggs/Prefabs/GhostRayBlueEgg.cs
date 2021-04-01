61cb59abd30a0a6ef06118bef3aeb4549f556acc
Metious <71298690+Metious@users.noreply.github.com>
2021-04-02 00:52:55 +0430

    moved GhostrayEgg to the new API

--- LeviathanEggs/Prefabs/GhostRayBlueEgg.cs
+++ LeviathanEggs/Prefabs/GhostRayBlueEgg.cs
@@ -9,16 +9,16 @@ namespace LeviathanEggs.Prefabs
     {
         public GhostRayBlueEgg()
             : base("GhostRayBlueEgg", "Ghost Ray Egg", "Ghost Rays hatch from these.")
-        {}
+        {
+            LateEnhancements += InitializeObject;
+        }
         public override GameObject Model => LoadGameObject("RobotEgg");
         public override TechType HatchingCreature => TechType.GhostRayBlue;
         public override float HatchingTime => 5f;
         public override Sprite ItemSprite => LoadSprite("RobotEgg");
 
-        public override GameObject GetGameObject()
+        public void InitializeObject(GameObject prefab)
         {
-            var prefab = base.GetGameObject();
-            
             Material material = new Material(Shader.Find("MarmosetUBER"))
             {
                 mainTexture = LoadTexture2D("RobotEggDiffuse"),
@@ -39,8 +39,6 @@ namespace LeviathanEggs.Prefabs
             }
 
             prefab.AddComponent<SpawnLocations>();
-
-            return prefab;
         }
     }
 }
