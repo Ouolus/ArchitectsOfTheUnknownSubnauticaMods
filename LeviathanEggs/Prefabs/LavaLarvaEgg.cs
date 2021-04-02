bd5a5e91aef3b05f131fa9516a68f1a3a4528cc3
Metious <71298690+Metious@users.noreply.github.com>
2021-04-03 02:24:59 +0430

    resized LavaLarvaEgg in inventory

--- LeviathanEggs/Prefabs/LavaLarvaEgg.cs
+++ LeviathanEggs/Prefabs/LavaLarvaEgg.cs
@@ -16,6 +16,7 @@ namespace LeviathanEggs.Prefabs
         public override TechType HatchingCreature => TechType.LavaLarva;
         public override float HatchingTime => 5f;
         public override Sprite ItemSprite => LoadSprite("LavaLarvaEgg");
+        public override Vector2int SizeInInventory { get; } = new(1, 1);
 
         public void InitializeObject(GameObject prefab) => prefab.AddComponent<SpawnLocations>();
     }
