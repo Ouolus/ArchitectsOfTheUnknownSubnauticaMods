c43cfc01396d8a5c038e519d5456a090ac608c08
Metious <71298690+Metious@users.noreply.github.com>
2021-04-03 02:25:10 +0430

    resized ReaperEgg in inventory

--- LeviathanEggs/Prefabs/ReaperEgg.cs
+++ LeviathanEggs/Prefabs/ReaperEgg.cs
@@ -18,6 +18,7 @@ namespace LeviathanEggs.Prefabs
         public override TechType HatchingCreature => TechType.ReaperLeviathan;
         public override float HatchingTime => 5f;
         public override Sprite ItemSprite => LoadSprite("ReaperEgg");
+        public override Vector2int SizeInInventory { get; } = new(3, 3);
 
         public void InitializeObject(GameObject prefab) => prefab.AddComponent<SpawnLocations>();
     }
