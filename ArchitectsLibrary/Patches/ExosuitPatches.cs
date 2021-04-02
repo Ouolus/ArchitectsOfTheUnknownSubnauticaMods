4289e3262a678d3279fceb67fa7defa7926a2f86
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-02 14:24:09 -0400

    exosuit interfaces & patches

--- ArchitectsLibrary/Patches/ExosuitPatches.cs
+++ ArchitectsLibrary/Patches/ExosuitPatches.cs
@@ -8,13 +8,31 @@ namespace ArchitectsLibrary.Patches
     [HarmonyPatch(typeof(Exosuit))]
     public class ExosuitPatches
     {
-        internal static Dictionary<TechType, IExosuitOnEquip> ExosuitOnEquips = new();
+        internal static Dictionary<TechType, IExosuitOnEquip> exosuitOnEquips = new();
+        internal static Dictionary<TechType, IExosuitOnToggle> exosuitOnToggles = new();
+
+        [HarmonyPatch(nameof(SeaMoth.OnUpgradeModuleToggle))]
+        [HarmonyPostfix]
+        static void OnUpgradeModuleToggle_Postfix(Exosuit __instance, bool active, int slotID)
+        {
+            InventoryItem item = __instance.GetSlotItem(slotID);
+            if(item is null)
+            {
+                return;
+            }
+            if (exosuitOnToggles.TryGetValue(item.item.GetTechType(), out IExosuitOnToggle exosuitOnToggle))
+            {
+                exosuitOnToggle.OnToggle(slotID, active, __instance);
+
+                __instance.quickSlotTimeUsed[slotID] = Time.time;
+            }
+        }
 
         [HarmonyPatch(nameof(Exosuit.OnUpgradeModuleChange))]
         [HarmonyPostfix]
         static void OnUpgradeModuleChange(Exosuit __instance, TechType techType, int slotID)
         {
-            if (ExosuitOnEquips.TryGetValue(techType, out IExosuitOnEquip exosuitOnEquip))
+            if (exosuitOnEquips.TryGetValue(techType, out IExosuitOnEquip exosuitOnEquip))
             {
                 exosuitOnEquip.OnEquip(slotID, __instance);
             }
