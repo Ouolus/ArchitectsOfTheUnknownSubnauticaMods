0ba5d9b47db66ebc0ec657d5d2bca28eee814604
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-02 12:56:03 -0400

    pt1 of exosuit upgrades

---
+++ ArchitectsLibrary/Patches/ExosuitPatches.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using ArchitectsLibrary.Interfaces;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ArchitectsLibrary.Patches
+{
+    [HarmonyPatch(typeof(Exosuit))]
+    public class ExosuitPatches
+    {
+        internal static Dictionary<TechType, IExosuitOnEquip> ExosuitOnEquips = new();
+
+        [HarmonyPatch(nameof(Exosuit.OnUpgradeModuleChange))]
+        [HarmonyPostfix]
+        static void OnUpgradeModuleChange(Exosuit __instance, TechType techType, int slotID)
+        {
+            if (ExosuitOnEquips.TryGetValue(techType, out IExosuitOnEquip exosuitOnEquip))
+            {
+                exosuitOnEquip.OnEquip(slotID, __instance);
+            }
+        }
+    }
+}
