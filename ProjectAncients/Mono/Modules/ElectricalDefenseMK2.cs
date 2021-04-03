fed7ab1eb8d6a35a50f2b326fb4ddbcc04abf14b
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-03 17:28:41 -0400

    support for different attack types with electrical defense mk2

--- ProjectAncients/Mono/Modules/ElectricalDefenseMK2.cs
+++ ProjectAncients/Mono/Modules/ElectricalDefenseMK2.cs
@@ -9,29 +9,54 @@ namespace ProjectAncients.Mono.Modules
         public GameObject[] fxElectSpheres;
         public float charge;
         public float chargeScalar;
+        public AttackType attackType;
 
-        float _damage = 7f;
-        float _radius = 18f;
-        float _chargeRadius = 1.5f;
-        float _chargeDamage = 3.5f;
+        float _damageElec = 1f;
+        float _radiusElec = 10f;
+        float _chargeRadiusElec = 1f;
+        float _chargeDamageElec = 2f;
+
+        float _damageArchElec = 7f;
+        float _radiusArchElec = 18f;
+        float _chargeRadiusArchElec = 1.5f;
+        float _chargeDamageArchElec = 3.5f;
 
         IEnumerator Start()
         {
             yield return new WaitUntil(() => fxElectSpheres is not null);
 
-            var fxElects = (GameObject[]) fxElectSpheres.Clone();
-            foreach (var electSphere in fxElects)
+            var fxElects = (GameObject[])fxElectSpheres.Clone();
+            if (attackType != AttackType.SmallElectricity)
             {
-                var renderers = electSphere.GetComponentsInChildren<Renderer>();
-                foreach (var renderer in renderers)
+                foreach (var electSphere in fxElects)
                 {
-                    renderer.material.SetColor("_Color", Color.green);
+                    var renderers = electSphere.GetComponentsInChildren<Renderer>();
+                    foreach (var renderer in renderers)
+                    {
+                        renderer.material.SetColor("_Color", Color.green);
+                    }
                 }
             }
-            
-            float radius = _radius + charge * _chargeRadius;
-            float originalDamage = _damage + charge * _chargeDamage;
-            
+
+            float radius;
+            if(attackType == AttackType.ArchitectElectricity || attackType == AttackType.Both)
+            {
+                radius = _radiusArchElec + charge * _chargeRadiusArchElec;
+            }
+            else
+            {
+                radius = _radiusElec + charge * _chargeRadiusElec;
+            }
+            float originalDamage;
+            if(attackType == AttackType.ArchitectElectricity || attackType == AttackType.Both)
+            {
+                originalDamage = _damageArchElec + charge * _chargeDamageArchElec;
+            }
+            else
+            {
+                originalDamage = _damageElec + charge * _chargeDamageElec;
+            }
+
             Utils.PlayFMODAsset(defenseSound, transform);
 
             int getObjIndex = Mathf.Clamp((int)(chargeScalar * fxElects.Length), 0,
@@ -53,11 +78,29 @@ namespace ProjectAncients.Mono.Modules
 
                 if (creature is not null && liveMixin is not null)
                 {
-                    liveMixin.TakeDamage(originalDamage / 2f, transform.position, Mod.architectElect, gameObject);
-                    liveMixin.TakeDamage(originalDamage / 2f, transform.position, DamageType.Electrical, gameObject);
+                    if(attackType == AttackType.ArchitectElectricity)
+                    {
+                        liveMixin.TakeDamage(originalDamage, transform.position, Mod.architectElect, gameObject);
+                    }
+                    else if(attackType == AttackType.SmallElectricity)
+                    {
+                        liveMixin.TakeDamage(originalDamage, transform.position, DamageType.Electrical, gameObject);
+                    }
+                    else if(attackType == AttackType.Both)
+                    {
+                        liveMixin.TakeDamage(originalDamage / 2f, transform.position, Mod.architectElect, gameObject);
+                        liveMixin.TakeDamage(originalDamage / 2f, transform.position, DamageType.Electrical, gameObject);
+                    }
                 }
             }
             Destroy(gameObject, 5f);
+        }
+
+        public enum AttackType
+        {
+            SmallElectricity,
+            ArchitectElectricity,
+            Both
         }
     }
 }
