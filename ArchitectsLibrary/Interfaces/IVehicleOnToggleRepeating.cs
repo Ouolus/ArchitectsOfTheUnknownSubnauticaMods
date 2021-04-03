7a957b71f1c6acf4d133937b75ef1ba27c8fab9c
Metious <71298690+Metious@users.noreply.github.com>
2021-04-04 01:16:54 +0430

    slight mistake in the parameter naming

--- ArchitectsLibrary/Interfaces/IVehicleOnToggleRepeating.cs
+++ ArchitectsLibrary/Interfaces/IVehicleOnToggleRepeating.cs
@@ -14,8 +14,8 @@ namespace ArchitectsLibrary.Interfaces
         /// your logic for your module when its Toggled on by a Seamoth or Exosuit, called on an interval of <see cref="RepeatingActionCooldown"/>
         /// </summary>
         /// <param name="slotID">which slot your upgrade is on</param>
-        /// <param name="seaMoth">an instance of the <see cref="Vehicle"/> so you can use the class' stuff to benefit from a more
+        /// <param name="vehicle">an instance of the <see cref="Vehicle"/> so you can use the class' stuff to benefit from a more
         /// complex logic for your upgrade.</param>
-        void DoRepeatingAction(int slotID, Vehicle seaMoth);
+        void DoRepeatingAction(int slotID, Vehicle vehicle);
     }
 }
