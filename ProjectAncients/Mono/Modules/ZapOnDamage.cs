dd6fe6bba692760c5d770944193a0f3d7e57fc12
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-03 00:50:46 -0400

    Update ZapOnDamage.cs

--- ProjectAncients/Mono/Modules/ZapOnDamage.cs
+++ ProjectAncients/Mono/Modules/ZapOnDamage.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEngine;
 
 namespace ProjectAncients.Mono.Modules
 {
-    class ZapOnDamage
+    public class ZapOnDamage : MonoBehaviour
     {
+        public GameObject zapPrefab;
     }
 }
