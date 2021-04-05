04d40f9074709771d050b923f100148b0862e3af
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-05 19:52:36 -0400

    getting ion cube material at game start

--- ArchitectsLibrary/Main.cs
+++ ArchitectsLibrary/Main.cs
@@ -3,6 +3,8 @@ using ArchitectsLibrary.Patches;
 using HarmonyLib;
 using QModManager.API.ModLoading;
 using QModManager.Utility;
+using UnityEngine;
+using System.Collections;
 
 namespace ArchitectsLibrary
 {
@@ -10,20 +12,31 @@ namespace ArchitectsLibrary
     public static class Main
     {
         private static Assembly myAssembly = Assembly.GetExecutingAssembly();
+        public static Material ionCubeMaterial;
 
         [QModPatch]
         public static void Load()
         {
-            Logger.Log(Logger.Level.Info, "ArchitectsLibrary started Patching.");
+            QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Info, "ArchitectsLibrary started Patching.");
             
             Initializer.PatchAllDictionaries();
             
             Harmony harmony = new Harmony($"ArchitectsOfTheUnknown_{myAssembly.GetName().Name}");
             
             VehiclePatches.Patch(harmony);
-            
-            Logger.Log(Logger.Level.Info, "ArchitectsLibrary successfully finished Patching!");
+
+            QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Info, "ArchitectsLibrary successfully finished Patching!");
+
+            UWE.CoroutineHost.StartCoroutine(LoadIonCubeMaterial());
         }
         
+        private static IEnumerator LoadIonCubeMaterial()
+        {
+            CoroutineTask<GameObject> task = CraftData.GetPrefabForTechTypeAsync(TechType.PrecursorIonCrystal);
+            yield return task;
+
+            GameObject ionCube = task.GetResult();
+            ionCubeMaterial = ionCube.GetComponentInChildren<MeshRenderer>().material;
+        }
     }
 }
