5606950aa8037aec77f88c6382f3081f03fa9e0b
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-10 18:33:01 -0400

    simplifying teleporternetwork, hopefully my unity math works

--- ProjectAncients/Prefabs/AlienBase/Teleporter/TeleporterNetwork.cs
+++ ProjectAncients/Prefabs/AlienBase/Teleporter/TeleporterNetwork.cs
@@ -8,25 +8,32 @@ namespace ProjectAncients.Prefabs.AlienBase
         public string classIdRoot;
         public TeleporterPrimaryPrefab primaryTeleporter;
         public TeleporterFramePrefab auxiliaryTeleporter;
-        public Vector3 masterCoords, masterEuler, auxiliaryCoords, auxiliaryEuler;
+        public Vector3 masterCoords, auxiliaryCoords;
+        public float masterAngle, auxiliaryAngle;
 
-        public TeleporterNetwork(string classIdRoot, string teleporterId, Vector3 masterCoords, Vector3 masterEuler, Vector3 masterPlayerPos, float masterPlayerAngle, Vector3 auxiliaryCoords, Vector3 auxiliaryEuler, Vector3 auxiliaryPlayerPos, float auxiliaryPlayerAngle)
+        public TeleporterNetwork(string classIdRoot, string teleporterId, Vector3 masterCoords, float masterAngle, Vector3 auxiliaryCoords, float auxiliaryAngle)
         {
             this.classIdRoot = classIdRoot;
             this.masterCoords = masterCoords;
-            this.masterEuler = masterEuler;
             this.auxiliaryCoords = auxiliaryCoords;
-            this.auxiliaryEuler = auxiliaryEuler;
-            primaryTeleporter = new TeleporterPrimaryPrefab(string.Format("{0}Primary", classIdRoot), teleporterId, auxiliaryPlayerPos, masterPlayerAngle);
-            auxiliaryTeleporter = new TeleporterFramePrefab(string.Format("{0}Auxiliary", classIdRoot), teleporterId, masterPlayerPos, auxiliaryPlayerAngle);
+            this.masterAngle = masterAngle;
+            this.auxiliaryAngle = auxiliaryAngle;
+            primaryTeleporter = new TeleporterPrimaryPrefab(string.Format("{0}Primary", classIdRoot), teleporterId, GetPlayerSpawnPosition(auxiliaryCoords, auxiliaryAngle), auxiliaryAngle);
+            auxiliaryTeleporter = new TeleporterFramePrefab(string.Format("{0}Auxiliary", classIdRoot), teleporterId, GetPlayerSpawnPosition(masterCoords, masterAngle), masterAngle);
         }
         
         public void Patch()
         {
             primaryTeleporter.Patch();
             auxiliaryTeleporter.Patch();
-            StaticCreatureSpawns.RegisterStaticSpawn(new StaticSpawn(primaryTeleporter.ClassID, masterCoords, string.Format("{0}Primary", classIdRoot), 50f, masterEuler));
-            StaticCreatureSpawns.RegisterStaticSpawn(new StaticSpawn(auxiliaryTeleporter.ClassID, auxiliaryCoords, string.Format("{0}Auxiliary", classIdRoot), 50f, auxiliaryEuler));
+            StaticCreatureSpawns.RegisterStaticSpawn(new StaticSpawn(primaryTeleporter.ClassID, masterCoords, string.Format("{0}Primary", classIdRoot), 50f, Vector3.up * masterAngle));
+            StaticCreatureSpawns.RegisterStaticSpawn(new StaticSpawn(auxiliaryTeleporter.ClassID, auxiliaryCoords, string.Format("{0}Auxiliary", classIdRoot), 50f, Vector3.up * auxiliaryAngle));
+        }
+
+        Vector3 GetPlayerSpawnPosition(Vector3 coords, float angle)
+        {
+            Vector3 direction = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.up;
+            return coords + (direction * 3f);
         }
     }
 }
