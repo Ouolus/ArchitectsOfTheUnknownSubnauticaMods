3a84216a4fa6ef4457171995b9340c999131789a
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-10 17:42:00 -0400

    forgot to assign values in constructor

--- ProjectAncients/Prefabs/AlienBase/Teleporter/TeleporterNetwork.cs
+++ ProjectAncients/Prefabs/AlienBase/Teleporter/TeleporterNetwork.cs
@@ -13,6 +13,10 @@ namespace ProjectAncients.Prefabs.AlienBase
         public TeleporterNetwork(string classIdRoot, string teleporterId, Vector3 masterCoords, Vector3 masterEuler, Vector3 masterPlayerPos, float masterPlayerAngle, Vector3 auxiliaryCoords, Vector3 auxiliaryEuler, Vector3 auxiliaryPlayerPos, float auxiliaryPlayerAngle)
         {
             this.classIdRoot = classIdRoot;
+            this.masterCoords = masterCoords;
+            this.masterEuler = masterEuler;
+            this.auxiliaryCoords = auxiliaryCoords;
+            this.auxiliaryEuler = auxiliaryEuler;
             primaryTeleporter = new TeleporterPrimaryPrefab(string.Format("{0}Primary", classIdRoot), teleporterId, masterPlayerPos, masterPlayerAngle);
             auxiliaryTeleporter = new TeleporterAuxiliaryPrefab(string.Format("{0}Auxiliary", classIdRoot), teleporterId, auxiliaryPlayerPos, auxiliaryPlayerAngle);
         }
