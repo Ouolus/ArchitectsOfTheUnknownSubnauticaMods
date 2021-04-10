c6cad2329dab6271a3a0aaa248ecea76484176ab
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-10 16:57:34 -0400

    teleporter teleporter prefab

---
+++ ProjectAncients/Prefabs/AlienBase/Teleporter/TeleporterPrefab.cs
@@ -0,0 +1,38 @@
+﻿using SMLHelper.V2.Assets;
+using UnityEngine;
+using UWE;
+using System.Collections;
+using ECCLibrary;
+
+namespace ProjectAncients.Prefabs.AlienBase.Teleporter
+{
+    public class TeleporterPrefab : Spawnable
+    {
+        const string referenceClassId = "f0429c44-a387-42e6-b621-74ba4dd8c2da";
+        private string teleporterId;
+        private Vector3 teleportPosition;
+        private float teleportAngle;
+
+        public TeleporterPrefab(string classId, string teleporterId, Vector3 teleportPosition, float teleportAngle) : base(classId, "Alien Arch", "An alien teleporter device.")
+        {
+            this.teleporterId = teleporterId;
+            this.teleportPosition = teleportPosition;
+            this.teleportAngle = teleportAngle;
+        }
+
+        public override IEnumerator GetGameObjectAsync(IOut<GameObject> gameObject)
+        {
+            IPrefabRequest request = PrefabDatabase.GetPrefabAsync(referenceClassId);
+            yield return request;
+            request.TryGetPrefab(out GameObject prefab);
+            GameObject obj = GameObject.Instantiate(prefab);
+            obj.SetActive(false);
+            var teleporter = obj.GetComponent<PrecursorTeleporter>();
+            teleporter.teleporterIdentifier = teleporterId;
+            teleporter.warpToPos = teleportPosition;
+            teleporter.warpToAngle = teleportAngle;
+
+            gameObject.Set(obj);
+        }
+    }
+}
