a3785460d2688ac2976a5f669671032d6df4989b
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-18 21:50:53 -0400

    xml

--- ArchitectsLibrary/CreatorKit/SNCreatorKit.cs
+++ ArchitectsLibrary/CreatorKit/SNCreatorKit.cs
@@ -7,6 +7,9 @@ namespace ArchitectsLibrary.CreatorKit
     /// </summary>
     public static class SNCreatorKit
     {
+        /// <summary>
+        /// The first method that runs in the Creator Kit that sets up everything.
+        /// </summary>
         public static void Entry()
         {
             PackGenerator templatePack = new PackGenerator(new PackData(new PackJson("Template Pack", "An example of a pack created with the Subnautica Creator Kit.", "An example pack.", "1.0", "Architects of the Unknown"), "TemplatePack"));
