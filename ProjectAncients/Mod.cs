19bed12187e5b12f4a1b70adb7942c9f1d765bfc
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-20 10:54:55 -0400

    move PCF teleporter exit

--- ProjectAncients/Mod.cs
+++ ProjectAncients/Mod.cs
@@ -390,7 +390,7 @@ namespace ProjectAncients
             #endregion
 
             #region Teleporters
-            TeleporterNetwork voidPcfNetwork = new TeleporterNetwork("VoidBasePCF", Vector3.zero, 0f, new Vector3(267.30f, -1438.50f, -350.44f), 150f, true);
+            TeleporterNetwork voidPcfNetwork = new TeleporterNetwork("VoidBasePCF", Vector3.zero, 0f, new Vector3(267.30f, -1439f, -350.44f), 150f, true, true);
             voidPcfNetwork.Patch();
 
             TeleporterNetwork voidWeaponsNetwork = new TeleporterNetwork("VoidBaseWeaponsBase", new Vector3(12f, 0f, 0f), 0f, new Vector3(-857.80f, -189.89f, -641.00f), 0f);
