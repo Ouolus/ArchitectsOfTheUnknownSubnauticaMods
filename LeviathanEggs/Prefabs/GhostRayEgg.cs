410e69489b9bb786af69e043e624298f788eaa67
Metious <71298690+Metious@users.noreply.github.com>
2021-04-22 03:09:22 +0430

    GhostRayEgg's new model

--- LeviathanEggs/Prefabs/GhostRayEgg.cs
+++ LeviathanEggs/Prefabs/GhostRayEgg.cs
@@ -9,37 +9,11 @@ namespace LeviathanEggs.Prefabs
     {
         public GhostRayEgg()
             : base("GhostRayEgg", "Ghost Ray Egg", "Ghost Rays hatch from these.")
-        {
-            LateEnhancements += InitializeObject;
-        }
-        public override GameObject Model => LoadGameObject("RobotEgg.prefab");
+        {}
+        public override GameObject Model => LoadGameObject("GhostRayEgg.prefab");
         public override TechType HatchingCreature => TechType.GhostRayBlue;
         public override float HatchingTime => 5f;
-        public override Sprite ItemSprite => LoadSprite("RobotEgg");
+        public override Sprite ItemSprite => LoadSprite("GhostRayEgg");
         public override Vector2int SizeInInventory { get; } = new(2, 2);
-
-        public void InitializeObject(GameObject prefab)
-        {
-            Material material = new Material(Shader.Find("MarmosetUBER"))
-            {
-                mainTexture = LoadTexture2D("RobotEggDiffuse"),
-            };
-            material.EnableKeyword("MARMO_NORMALMAP");
-            material.EnableKeyword("MARMO_SPECMAP");
-            material.EnableKeyword("MARMO_EMISSION");
-
-            material.SetTexture(ShaderPropertyID._Illum, LoadTexture2D("RobotEggIllum"));
-            material.SetTexture(ShaderPropertyID._SpecTex, LoadTexture2D("RobotEggDiffuse"));
-            material.SetTexture(ShaderPropertyID._BumpMap, LoadTexture2D("RobotEggNormal"));
-
-            Renderer[] renderers = prefab.GetAllComponentsInChildren<Renderer>();
-            foreach (var rend in renderers)
-            {
-                rend.material = material;
-                rend.sharedMaterial = material;
-            }
-
-            prefab.AddComponent<SpawnLocations>();
-        }
     }
 }
