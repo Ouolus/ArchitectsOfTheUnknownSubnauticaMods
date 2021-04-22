90f2b0990c5d0dfe1ace5aa16675d530125399e8
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-22 13:39:51 -0400

    spinefish school in aquarium

--- ProjectAncients/Mono/AlienBaseSpawners/VoidBaseInteriorSpawner.cs
+++ ProjectAncients/Mono/AlienBaseSpawners/VoidBaseInteriorSpawner.cs
@@ -15,6 +15,7 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
             yield return StartCoroutine(SpawnPrefabsArray(pedestal_empty2, 4f, new Vector3(1, 1, 10), Vector3.one, new Vector3(3f + 1.5f + 1.5f, 2f, 40f)));
 
             //Aquarium
+            yield return StartCoroutine(SpawnPrefab(creature_spinefishSchool, new Vector3(0f, 7f, -21f), new Vector3(0f, 0f, 0f), Vector3.one));
 
             //Aquarium room
             yield return StartCoroutine(SpawnPrefab(structure_doorwaySmall, new Vector3(25.62f, firstFloorY, 2.72f), new Vector3(0f, -135f, 0f), Vector3.one * 1.2f));
@@ -37,7 +38,6 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
             yield return StartCoroutine(SpawnPrefab(creature_alienRobot, new Vector3(3f, secondFloorY + 1f, 32f)));
             yield return StartCoroutine(SpawnPrefab(structure_column, new Vector3(0f, secondFloorY + 6f, 32f), Vector3.zero, new Vector3(1f, 0.87f, 1f)));
             yield return StartCoroutine(SpawnPrefab(pedestal_empty2, new Vector3(0f, secondFloorY + 6f, 32f), (Vector3.left * 180f), new Vector3(1.5f, 0.5f, 1.5f)));
-            yield return StartCoroutine(SpawnPrefab(light_small, new Vector3(0f, secondFloorY + 4.5f, 32f), Vector3.left * 180f));
 
             //Left lower room
             yield return StartCoroutine(SpawnPrefab(Mod.cachePingsTerminal.ClassID, new Vector3(22, 0f, 28), new Vector3(0f, -45f, 0f)));
