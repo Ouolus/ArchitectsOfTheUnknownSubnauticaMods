7bb88e7e075db26a91073b25adc7eb9c290a0dca
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-23 09:12:32 -0400

    dont despawn garg while in voidbase (epic roars)

--- ProjectAncients/Mono/Singletons/VoidGargSingleton.cs
+++ ProjectAncients/Mono/Singletons/VoidGargSingleton.cs
@@ -33,7 +33,7 @@ namespace ProjectAncients.Mono
 		void CheckDistance()
 		{
 			string playerBiome = Player.main.GetBiomeString();
-			if (!VoidGargSpawner.IsVoidBiome(playerBiome) && !playerBiome.StartsWith("precursor", System.StringComparison.OrdinalIgnoreCase) && !playerBiome.StartsWith("observatory", System.StringComparison.OrdinalIgnoreCase))
+			if (!VoidGargSpawner.IsVoidBiome(playerBiome) && !playerBiome.StartsWith("precursor", System.StringComparison.OrdinalIgnoreCase) && !playerBiome.StartsWith("prison", System.StringComparison.OrdinalIgnoreCase) && !playerBiome.StartsWith("observatory", System.StringComparison.OrdinalIgnoreCase))
 			{
 				float distance = Vector3.Distance(MainCameraControl.main.transform.position, transform.position);
 				if (distance > 250f)
