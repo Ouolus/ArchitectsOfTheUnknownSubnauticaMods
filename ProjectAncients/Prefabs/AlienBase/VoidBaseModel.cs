d1b5cfad4e8245469d154d8dfc215850eb6c348d
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-24 18:12:30 -0400

    code that doesnt work

--- ProjectAncients/Prefabs/AlienBase/VoidBaseModel.cs
+++ ProjectAncients/Prefabs/AlienBase/VoidBaseModel.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using ProjectAncients.Mono.AlienTech;
+using Story;
 
 namespace ProjectAncients.Prefabs.AlienBase
 {
@@ -15,9 +16,22 @@ namespace ProjectAncients.Prefabs.AlienBase
         {
         }
 
+        private StoryGoal approachBaseGoal = new StoryGoal("ApproachVoidBase", Story.GoalType.Story, 0f);
+
         public override void CustomizePrefab()
         {
             prefab.EnsureComponent<VoidBaseReveal>();
+            var voTrigger1 = prefab.AddComponent<AlienBasePlayerTrigger>();
+            voTrigger1.onTrigger = new AlienBasePlayerTrigger.OnTriggered(OnTrigger1);
+            voTrigger1.triggerObject = prefab.SearchChild("VOTrigger1");
+        }
+
+        public void OnTrigger1(GameObject obj)
+        {
+            if(!StoryGoalManager.main.OnGoalComplete(approachBaseGoal.key))
+            {
+                CustomPDALinesManager.PlayPDAVoiceLine(Mod.assetBundle.LoadAsset<AudioClip>("VoidBaseEncounter"), "VoidBaseEncounter", "Detecting leviathan-class lifeforms beyond this doorway. Approach with caution.");
+            }
         }
     }
 }
