c45c81a29028eb87a67eed4946c0a43fa96a3901
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-27 15:43:31 -0400

    decrease adult garg player aggression

--- ProjectAncients/Prefabs/Creatures/AdultGargantuan.cs
+++ ProjectAncients/Prefabs/Creatures/AdultGargantuan.cs
@@ -87,7 +87,7 @@ namespace ProjectAncients.Prefabs
 
         public override void ApplyAggression()
         {
-            MakeAggressiveTo(120f, 6, EcoTargetType.Shark, 0.2f, 3f);
+            MakeAggressiveTo(120f, 6, EcoTargetType.Shark, 0.2f, 1f);
             MakeAggressiveTo(60f, 2, EcoTargetType.Whale, 0.23f, 2.3f);
             MakeAggressiveTo(200f, 7, EcoTargetType.Leviathan, 0.3f, 3f);
             MakeAggressiveTo(200f, 7, Mod.superDecoyTargetType, 0f, 5f);
