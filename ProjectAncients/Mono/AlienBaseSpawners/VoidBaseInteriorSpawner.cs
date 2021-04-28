af708b95f5645638411e662595c0408d78064a8a
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-28 13:38:17 -0400

    removing ugliness in void base

--- ProjectAncients/Mono/AlienBaseSpawners/VoidBaseInteriorSpawner.cs
+++ ProjectAncients/Mono/AlienBaseSpawners/VoidBaseInteriorSpawner.cs
@@ -74,8 +74,14 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
             yield return StartCoroutine(SpawnPrefab(pedestal_empty2, new Vector3(0f, secondFloorY + 8f, 32f), (Vector3.left * 180f), new Vector3(2f, 1f, 2f)));
 
             //Egg room
-            yield return StartCoroutine(SpawnPrefab(prop_claw, new Vector3(20.64f, secondCeilingY - 2.5f, 31.32f), Vector3.up * 45f));
-            yield return StartCoroutine(SpawnPrefab(prop_claw, new Vector3(-27.05f, secondCeilingY - 2.5f, 15.4f), Vector3.up * 135f));
+            yield return StartCoroutine(SpawnPrefab(prop_claw, new Vector3(20.64f, secondCeilingY - 2.5f - 4.72f - 4.72f, 31.32f), Vector3.up * 45f)); //columns must have a scale of 0.59f to fit with the claws nicely, which puts the columns at 4.72m tall
+            yield return StartCoroutine(SpawnPrefab(structure_column, new Vector3(20.64f, secondCeilingY - 4.72f - 4.72f, 31.32f), Vector3.up * 45f, Vector3.one * 0.59f));
+            yield return StartCoroutine(SpawnPrefab(structure_column, new Vector3(20.64f, secondCeilingY - 4.72f, 31.32f), Vector3.up * 45f, Vector3.one * 0.59f));
+
+            yield return StartCoroutine(SpawnPrefab(prop_claw, new Vector3(-27.05f, secondCeilingY - 2.5f - 4.72f - 4.72f, 15.4f), Vector3.up * 135f));
+            yield return StartCoroutine(SpawnPrefab(structure_column, new Vector3(-27.05f, secondCeilingY - 4.72f - 4.72f, 15.4f), Vector3.up * 135f, Vector3.one * 0.59f));
+            yield return StartCoroutine(SpawnPrefab(structure_column, new Vector3(-27.05f, secondCeilingY - 4.72f, 15.4f), Vector3.up * 135f, Vector3.one * 0.59f));
+
             yield return StartCoroutine(SpawnPrefab(prop_tableRectangle, new Vector3(20.64f, secondFloorY, 31.32f), Vector3.zero, Vector3.one * 0.67f));
             yield return StartCoroutine(SpawnPrefab(prop_tableRectangle, new Vector3(20.64f + 1.4f, secondFloorY, 31.32f), Vector3.zero, Vector3.one * 0.67f));
             yield return StartCoroutine(SpawnPrefab(prop_tableRectangle, new Vector3(20.64f + 2.8f, secondFloorY, 31.32f), Vector3.zero, Vector3.one * 0.67f));
