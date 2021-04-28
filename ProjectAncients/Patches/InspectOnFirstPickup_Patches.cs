b2e92609cf77cd00003839575e60b9b5b22c26b6
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-04-28 13:27:53 -0400

    i think the egg should be a little bigger

--- ProjectAncients/Patches/InspectOnFirstPickup_Patches.cs
+++ ProjectAncients/Patches/InspectOnFirstPickup_Patches.cs
@@ -39,7 +39,7 @@ namespace ProjectAncients.Patches
             }
             if (techTag.type == Mod.gargEgg.TechType)
             {
-                __instance.gameObject.transform.localScale = Vector3.one * 0.15f;
+                __instance.gameObject.transform.localScale = Vector3.one * 0.17f;
             }
         }
 
