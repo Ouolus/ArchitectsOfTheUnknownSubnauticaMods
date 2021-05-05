942ab9f080001441920acd01fb470a728771f4ca
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-05 08:57:44 -0400

    Update Main.cs

--- ArchitectsLibrary/Main.cs
+++ ArchitectsLibrary/Main.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 using System.Collections;
 using CreatorKit.Patches;
 using UWE;
+using System.IO;
+using ArchitectsLibrary.Items;
+using SMLHelper.V2.Handlers;
 
 namespace ArchitectsLibrary
 {
@@ -16,6 +19,11 @@ namespace ArchitectsLibrary
         private static Assembly myAssembly = Assembly.GetExecutingAssembly();
         public static Material ionCubeMaterial;
         public static Material precursorGlassMaterial;
+        public static AssetBundle assetBundle;
+        public const string assetBundleName = "architectslibrary";
+        public static CraftTree.Type precursorFabricatorTree;
+
+        public static PrecursorAlloyIngot precursorAlloy = new PrecursorAlloyIngot();
 
         [QModPatch]
         public static void Load()
@@ -26,14 +34,19 @@ namespace ArchitectsLibrary
 
             QModManager.Utility.Logger.Log(QModManager.Utility.Logger.Level.Info, "ArchitectsLibrary successfully finished Patching!");
 
-            UWE.CoroutineHost.StartCoroutine(LoadIonCubeMaterial());
-            UWE.CoroutineHost.StartCoroutine(LoadPrecursorGlassMaterial());
+            CoroutineHost.StartCoroutine(LoadIonCubeMaterial());
+            CoroutineHost.StartCoroutine(LoadPrecursorGlassMaterial());
 
-            //CreatorKit.SNCreatorKit.Entry();
+            assetBundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Assets", assetBundleName));
+
+            CraftTreeHandler.CreateCustomCraftTreeAndType("PrecursorFabricator", out precursorFabricatorTree);
+            PatchItems();
 
             Harmony harmony = new Harmony($"ArchitectsOfTheUnknown_{myAssembly.GetName().Name}");
 
-            VehiclePatches.Patch(harmony); 
+            VehiclePatches.Patch(harmony);
+
+            //CreatorKit.SNCreatorKit.Entry();
             //MainMenuMusicPatches.Patch(harmony);
         }
 
@@ -53,6 +66,11 @@ namespace ArchitectsLibrary
 
             request.TryGetPrefab(out GameObject glassPanel);
             precursorGlassMaterial = glassPanel.GetComponentInChildren<MeshRenderer>().material;
+        }
+
+        private static void PatchItems()
+        {
+            precursorAlloy.Patch();
         }
     }
 }
