83b01f9b55c3a8ab032a903df74791f6459a1192
Metious <71298690+Metious@users.noreply.github.com>
2021-05-07 02:33:49 +0430

    more xml docs

--- ArchitectsLibrary/Utility/GameObjectExtensions.cs
+++ ArchitectsLibrary/Utility/GameObjectExtensions.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 namespace ArchitectsLibrary.Utility
 {
+    /// <summary>
+    /// a Class that contains a collection of useful extensions for <see cref="GameObject"/>s.
+    /// </summary>
     public static class GameObjectExtensions
     {
+        /// <summary>
+        /// Disables the <see cref="Collider"/>s and <see cref="Renderer"/>s of a <see cref="GameObject"/> without making it completely In Active.
+        /// </summary>
+        /// <param name="gameObject"></param>
         public static void SemiInActive(this GameObject gameObject)
         {
             foreach (Collider collider in gameObject.GetComponentsInChildren<Collider>())
@@ -13,6 +20,12 @@ namespace ArchitectsLibrary.Utility
                 renderer.enabled = false;
         }
 
+        /// <summary>
+        /// searches for a Child Object of a <see cref="GameObject"/> by passing the Child's name.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <param name="byName">The Child to search for.</param>
+        /// <returns>Found Child from the passed string.</returns>
         public static GameObject SearchChild(this GameObject gameObject, string byName)
         {
             GameObject obj = SearchChildRecursive(gameObject, byName);
