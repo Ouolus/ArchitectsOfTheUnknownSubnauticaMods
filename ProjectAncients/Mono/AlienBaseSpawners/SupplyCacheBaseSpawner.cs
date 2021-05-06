c078426665280a6bbad84a586e88fcbcd6227366
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-06 08:07:45 -0400

    add precingot to supply cache

--- ProjectAncients/Mono/AlienBaseSpawners/SupplyCacheBaseSpawner.cs
+++ ProjectAncients/Mono/AlienBaseSpawners/SupplyCacheBaseSpawner.cs
@@ -30,6 +30,8 @@ namespace ProjectAncients.Mono.AlienBaseSpawners
             yield return StartCoroutine(SpawnRelicInCase(new Vector3(centerLocalX - 6f, floorLocalY, 15f), Mod.ingotRelic.ClassID, new Vector3(0f, 1.25f, 0f)));
             yield return StartCoroutine(SpawnRelicInCase(new Vector3(centerLocalX - 6f, floorLocalY, 17f), Mod.ingotRelic.ClassID, new Vector3(0f, 1.25f, 0f)));
             yield return StartCoroutine(SpawnRelicInCase(new Vector3(centerLocalX - 6f, floorLocalY, 19f), Mod.ingotRelic.ClassID, new Vector3(0f, 1.25f, 0f)));
+            yield return StartCoroutine(SpawnPrefab(ArchitectsLibrary.Main.precursorAlloy.ClassID, new Vector3(centerLocalX, floorLocalY, 17f), new Vector3(0f, 37f, 0f)));
+
         }
 
         protected override string MainTerminalClassId => Mod.supplyCacheTerminal.ClassID;
