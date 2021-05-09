4a6787c470b13a6235dc34d3e5cc9b4d2c1de1a9
Metious <71298690+Metious@users.noreply.github.com>
2021-05-09 06:24:25 +0430

    GetGameObjectAsync for alloy ingot

--- ArchitectsLibrary/Items/PrecursorAlloyIngot.cs
+++ ArchitectsLibrary/Items/PrecursorAlloyIngot.cs
@@ -9,7 +9,7 @@ namespace ArchitectsLibrary.Items
 {
     class PrecursorAlloyIngot : Craftable
     {
-        private GameObject prefab;
+        GameObject prefab;
 
         public PrecursorAlloyIngot() : base("PrecursorIngot", "Precursor Alloy Ingot", "An alien resource with mysterious properties and unprecedented integrity.")
         {
@@ -22,9 +22,10 @@ namespace ArchitectsLibrary.Items
 
         public override bool UnlockedAtStart => false;
 
+#if SN1
         public override GameObject GetGameObject()
         {
-            if(prefab == null)
+            if (prefab is null)
             {
                 prefab = GameObject.Instantiate(Main.assetBundle.LoadAsset<GameObject>("PrecursorIngot_Prefab"));
                 prefab.SetActive(false);
@@ -51,5 +52,38 @@ namespace ArchitectsLibrary.Items
             }
             return prefab;
         }
+#else
+        public override IEnumerator GetGameObjectAsync(IOut<GameObject> gameObject)
+        {
+            if (prefab is null)
+            {
+                prefab = GameObject.Instantiate(Main.assetBundle.LoadAsset<GameObject>("PrecursorIngot_Prefab"));
+                prefab.SetActive(false);
+
+                prefab.EnsureComponent<TechTag>().type = TechType;
+                prefab.EnsureComponent<PrefabIdentifier>().ClassId = ClassID;
+                prefab.EnsureComponent<Pickupable>();
+                prefab.EnsureComponent<VFXSurface>().surfaceType = VFXSurfaceTypes.metal;
+                var rb = prefab.EnsureComponent<Rigidbody>();
+                rb.mass = 15f;
+                rb.isKinematic = true;
+                prefab.EnsureComponent<WorldForces>();
+                prefab.EnsureComponent<LargeWorldEntity>().cellLevel = LargeWorldEntity.CellLevel.Near;
+
+                var inspect = prefab.EnsureComponent<InspectOnFirstPickup>();
+                inspect.pickupAble = prefab.GetComponent<Pickupable>();
+                inspect.collision = prefab.GetComponent<Collider>();
+                inspect.rigidBody = prefab.GetComponent<Rigidbody>();
+                inspect.animParam = "holding_precursorkey";
+                inspect.inspectDuration = 4.1f;
+
+                MaterialUtils.ApplySNShaders(prefab);
+                MaterialUtils.ApplyPrecursorMaterials(prefab, 12);
+            }
+
+            yield return null;
+            gameObject.Set(prefab);
+        }
+#endif
     }
 }
