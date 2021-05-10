f1a8f2d253dea0fd14aabbda95a34b58917d55b1
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-10 09:33:44 -0400

    reflection utils class just in case

---
+++ ArchitectsLibrary/Utility/ReflectionUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchitectsLibrary.Utility
+{
+    /// <summary>
+    /// Contains methods to help in reflection. You should use the assembly publicizer in most cases.
+    /// </summary>
+    public static class ReflectionUtils
+    {
+        public static void SetPrivateField<T>(Type type, T instance, string name, object value)
+        {
+            var prop = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+            prop.SetValue(instance, value);
+        }
+        public static OutputT GetPrivateField<OutputT>(Type type, object target, string name)
+        {
+            var prop = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+            return (OutputT)prop.GetValue(target);
+        }
+        public static void SetProtectedField<T>(Type type, T instance, string name, object value)
+        {
+            var prop = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+            prop.SetValue(instance, value);
+        }
+        public static OutputT GetProtectedField<OutputT>(Type type, object target, string name)
+        {
+            var prop = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+            return (OutputT)prop.GetValue(target);
+        }
+    }
+}
