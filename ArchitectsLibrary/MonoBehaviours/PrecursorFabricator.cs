61c8f28401681bc875680431b1243866974c2b38
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-09 21:57:29 -0400

    change logic for power consumption

--- ArchitectsLibrary/MonoBehaviours/PrecursorFabricator.cs
+++ ArchitectsLibrary/MonoBehaviours/PrecursorFabricator.cs
@@ -4,6 +4,31 @@ namespace ArchitectsLibrary.MonoBehaviours
 {
     class PrecursorFabricator : Fabricator
     {
-        // logic here
+        public override void Craft(TechType techType, float duration)
+        {
+			float powerToConsume = 50f;
+			if(techType == TechType.PrecursorIonCrystal)
+            {
+				powerToConsume = 1000f;
+            }
+			if (!CrafterLogic.ConsumeEnergy(powerRelay, powerToConsume))
+			{
+				ErrorMessage.AddMessage("Not enough power.");
+				return;
+			}
+			if (!CrafterLogic.ConsumeResources(techType))
+			{
+				return;
+			}
+			if (CraftData.GetCraftTime(techType, out duration))
+			{
+				duration = Mathf.Max(spawnAnimationDelay + spawnAnimationDuration, duration);
+			}
+			else
+			{
+				duration = spawnAnimationDelay + spawnAnimationDuration;
+			}
+			base.Craft(techType, duration);
+		}
     }
 }
