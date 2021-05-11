98cfcee7710ada4812f2de0d7776af4b190ac50d
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-11 14:00:11 -0400

    adjust warp cannon craft model

--- ProjectAncients/Prefabs/Equipment/WarpCannonPrefab.cs
+++ ProjectAncients/Prefabs/Equipment/WarpCannonPrefab.cs
@@ -55,7 +55,7 @@ namespace ProjectAncients.Prefabs.Equipment
 
             var vfxFabricating = prefab.SearchChild("CraftModel").AddComponent<VFXFabricating>();
             vfxFabricating.localMinY = -0.31f;
-            vfxFabricating.localMaxY = 0.2f;
+            vfxFabricating.localMaxY = 0.1f;
             vfxFabricating.scaleFactor = 0.1f;
             vfxFabricating.posOffset = new Vector3(-0.70f, 0.1f, -0.1f);
             vfxFabricating.eulerOffset = new Vector3(0f, 90f, 90f);
