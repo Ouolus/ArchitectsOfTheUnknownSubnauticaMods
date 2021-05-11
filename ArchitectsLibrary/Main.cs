c3796497a8f76f85fe54586b7ae8d8f98f143eda
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-11 09:23:19 -0400

    update ion cube crafting model

--- ArchitectsLibrary/Main.cs
+++ ArchitectsLibrary/Main.cs
@@ -78,6 +78,7 @@ namespace ArchitectsLibrary
             yield return task;
             var prefab = task.GetResult();
             var vfxFabricating = prefab.GetComponentInChildren<MeshRenderer>(true).gameObject.AddComponent<VFXFabricating>();
+            vfxFabricating.localMinY = -0.12f;
             vfxFabricating.localMaxY = 0.34f;
             vfxFabricating.posOffset = new Vector3(0f, -0.04f, 0.1f);
             vfxFabricating.eulerOffset = new Vector3(270f, 0f, 0f);
