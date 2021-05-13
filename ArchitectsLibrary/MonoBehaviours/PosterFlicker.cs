b89c253811178ee1c439eae377af500025563b65
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-13 09:46:46 -0400

    flickering was too harsh

--- ArchitectsLibrary/MonoBehaviours/PosterFlicker.cs
+++ ArchitectsLibrary/MonoBehaviours/PosterFlicker.cs
@@ -12,7 +12,7 @@ namespace ArchitectsLibrary.MonoBehaviours
             if(Time.time > timeFlickerAgain)
             {
                 timeFlickerAgain = Time.time + Random.Range(0.05f, 0.1f);
-                renderer.material.SetColor("_Color", new Color(1f, 1.5f, 1f, Random.Range(0.2f, 0.6f)));
+                renderer.material.SetColor("_Color", new Color(1f, 1.5f, 1f, Random.Range(0.4f, 0.55f)));
             }
         }
     }
