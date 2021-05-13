872185e7b3e9f57d0be257b0185e78df4885ff30
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-13 09:40:21 -0400

    poster flicker class

---
+++ ArchitectsLibrary/MonoBehaviours/PosterFlicker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ArchitectsLibrary.MonoBehaviours
+{
+    class PosterFlicker  : MonoBehaviour
+    {
+        public Renderer renderer;
+        float timeFlickerAgain = 0f;
+
+        void Update()
+        {
+            if(Time.time > timeFlickerAgain)
+            {
+                timeFlickerAgain = Time.time + Random.Range(0.05f, 0.1f);
+                renderer.material.SetColor("_Color", new Color(1f, 1.5f, 1f, Random.Range(0.2f, 0.6f)));
+            }
+        }
+    }
+}
