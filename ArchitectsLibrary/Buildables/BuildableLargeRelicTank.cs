2ab323ca3a830ca405108dde5111a8b8fe645a81
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-17 09:50:21 -0400

    update bounds

--- ArchitectsLibrary/Buildables/BuildableLargeRelicTank.cs
+++ ArchitectsLibrary/Buildables/BuildableLargeRelicTank.cs
@@ -10,7 +10,7 @@ namespace ArchitectsLibrary.Buildables
 
         protected override ConstructableSettings GetConstructableSettings => new ConstructableSettings(true, true, true, true, true, true, true, placeDefaultDistance: 2f, placeMinDistance: 2f, placeMaxDistance: 10f);
 
-        protected override OrientedBounds[] GetBounds => new OrientedBounds[0];
+        protected override OrientedBounds[] GetBounds => new OrientedBounds[] { new OrientedBounds(new Vector3(0f, 0.5f, 0f), Quaternion.identity, new Vector3(0.4f, 0.4f, 1.8f)) };
 
         protected override string GetOriginalClassId => "1b85a183-2084-42a6-8d85-7e58dd6864bd";
 
