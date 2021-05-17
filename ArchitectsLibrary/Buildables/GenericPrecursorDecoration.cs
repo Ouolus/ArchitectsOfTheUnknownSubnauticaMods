0b3c1559c956be613f5d3f5949168688465e2d07
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-16 20:04:35 -0400

    update generic precursor decoration class

--- ArchitectsLibrary/Buildables/GenericPrecursorDecoration.cs
+++ ArchitectsLibrary/Buildables/GenericPrecursorDecoration.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +6,7 @@ using SMLHelper.V2.Assets;
 using SMLHelper.V2.Crafting;
 using ArchitectsLibrary.Handlers;
 using UnityEngine;
+using UWE;
 
 namespace ArchitectsLibrary.Buildables
 {
@@ -16,23 +16,49 @@ namespace ArchitectsLibrary.Buildables
         {
         }
 
-        /// <summary>
-        /// The recipe for this buildable.
-        /// </summary>
-        /// <returns></returns>
         protected override TechData GetBlueprintRecipe()
         {
-            return new TechData(new List<Ingredient>() { new Ingredient(TechType.Titanium, 1), new Ingredient(AUHandler.EmeraldTechType, 2) });
+            return new TechData(new List<Ingredient>() { new Ingredient(TechType.Titanium, 1), new Ingredient(AUHandler.AlienCompositeGlassTechType, 1) });
         }
 
-        public override sealed TechCategory CategoryForPDA => TechCategory.ExteriorModule;
-        public override sealed TechGroup GroupForPDA => TechGroup.ExteriorModules;
+        public override TechCategory CategoryForPDA => TechCategory.ExteriorModule;
+        public override TechGroup GroupForPDA => TechGroup.ExteriorModules;
 
-        protected virtual string ClassIdReference { get; }
+        public override bool UnlockedAtStart => false;
+        public override TechType RequiredForUnlock => AUHandler.PrecursorAlloyIngotTechType;
+
+        protected virtual OrientedBounds GetBounds { get; }
+        protected virtual string GetOriginalClassId { get; }
 
         public override GameObject GetGameObject()
         {
-            return null;
+            GameObject buildablePrefab = new GameObject(ClassID);
+            buildablePrefab.SetActive(false);
+            PrefabDatabase.TryGetPrefab(GetOriginalClassId, out GameObject originalPrefab);
+            GameObject.Instantiate(originalPrefab).transform.SetParent(buildablePrefab.transform, false);
+            DeleteChildComponentIfExists<LargeWorldEntity>(buildablePrefab);
+            DeleteChildComponentIfExists<PrefabIdentifier>(buildablePrefab);
+            DeleteChildComponentIfExists<TechTag>(buildablePrefab);
+            DeleteChildComponentIfExists<SkyApplier>(buildablePrefab);
+            buildablePrefab.EnsureComponent<PrefabIdentifier>().ClassId = ClassID;
+            buildablePrefab.EnsureComponent<TechTag>().type = TechType;
+            buildablePrefab.EnsureComponent<LargeWorldEntity>().cellLevel = LargeWorldEntity.CellLevel.Global;
+            buildablePrefab.EnsureComponent<SkyApplier>();
+            var con = buildablePrefab.AddComponent<Constructable>();
+            SetConstructableSettings(con);
+
+            return buildablePrefab;
+        }
+
+        protected abstract void SetConstructableSettings(Constructable con);
+
+        private void DeleteChildComponentIfExists<T>(GameObject prefab) where T : Component
+        {
+            T component = prefab.GetComponentInChildren<T>();
+            if (component)
+            {
+                Object.DestroyImmediate(component);
+            }
         }
     }
 }
