fd8c80b525ce6feed52a3856ad22c7e94a22557c
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-17 09:54:50 -0400

    allow buildable platform inside

--- ArchitectsLibrary/Buildables/BuildablePlatform.cs
+++ ArchitectsLibrary/Buildables/BuildablePlatform.cs
@@ -4,16 +4,16 @@ namespace ArchitectsLibrary.Buildables
 {
     class BuildablePlatform : GenericPrecursorDecoration
     {
-        public BuildablePlatform() : base("BuildablePlatform", "Alien Platform", "A flat platform, for decoration.")
+        public BuildablePlatform() : base("BuildablePlatform", "Alien Platform", "A flat platform, for decoration. Placeable inside and outside.")
         {
         }
 
-        protected override ConstructableSettings GetConstructableSettings => new ConstructableSettings(false, false, true, true, true, true, true, placeDefaultDistance: 4f, placeMinDistance: 2f, placeMaxDistance: 10f);
+        protected override ConstructableSettings GetConstructableSettings => new ConstructableSettings(true, true, true, true, true, true, true, placeDefaultDistance: 2f, placeMinDistance: 2f, placeMaxDistance: 10f);
 
         protected override OrientedBounds[] GetBounds => new OrientedBounds[] { new OrientedBounds(new Vector3(0f, 0.5f, 0f), Quaternion.identity, new Vector3(2f, 0.4f, 2f)) };
 
         protected override string GetOriginalClassId => "738892ae-64b0-4240-953c-cea1d19ca111";
 
-        protected override bool ExteriorOnly => true;
+        protected override bool ExteriorOnly => false;
     }
 }
