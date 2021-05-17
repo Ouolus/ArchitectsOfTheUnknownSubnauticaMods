54e3e68395f4a4f69e21be6278866f4e02b04945
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-17 09:54:19 -0400

    fix dissection tank clipping issues

--- ArchitectsLibrary/Buildables/BuildableDissectionTank.cs
+++ ArchitectsLibrary/Buildables/BuildableDissectionTank.cs
@@ -10,10 +10,15 @@ namespace ArchitectsLibrary.Buildables
 
         protected override ConstructableSettings GetConstructableSettings => new ConstructableSettings(true, true, true, true, true, true, true, placeDefaultDistance: 2f, placeMinDistance: 2f, placeMaxDistance: 10f);
 
-        protected override OrientedBounds[] GetBounds => new OrientedBounds[0];
+        protected override OrientedBounds[] GetBounds => new OrientedBounds[] { new OrientedBounds(new Vector3(0f, 0.5f, 0f), Quaternion.identity, new Vector3(0.4f, 0.4f, 1.8f)) };
 
         protected override string GetOriginalClassId => "44974fcd-c47a-41aa-a279-43eaf234bfa6";
 
         protected override bool ExteriorOnly => false;
+
+        protected override void EditPrefab(GameObject prefab)
+        {
+            prefab.transform.GetChild(0).position = new Vector3(0f, 0.1f, 0f);
+        }
     }
 }
