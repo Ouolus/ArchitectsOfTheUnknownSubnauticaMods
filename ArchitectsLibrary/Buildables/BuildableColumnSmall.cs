9c71f11a7408a6fcb9df6dbce35adc1abbef7ebf
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-17 14:02:29 -0400

    forgot to actually shrink the small column

--- ArchitectsLibrary/Buildables/BuildableColumnSmall.cs
+++ ArchitectsLibrary/Buildables/BuildableColumnSmall.cs
@@ -10,10 +10,15 @@ namespace ArchitectsLibrary.Buildables
 
         protected override ConstructableSettings GetConstructableSettings => new ConstructableSettings(true, true, true, true, true, true, true, placeDefaultDistance: 2f, placeMinDistance: 2f, placeMaxDistance: 10f);
 
-        protected override OrientedBounds[] GetBounds => new OrientedBounds[] { new OrientedBounds(new Vector3(0f, 4f * 0.37f, 0f), Quaternion.identity, new Vector3(1f * 0.37f, 3.9f * 0.37f, 1f * 0.37f)) };
+        protected override OrientedBounds[] GetBounds => new OrientedBounds[] { new OrientedBounds(new Vector3(0f, 4f * 0.37f, 0f), Quaternion.identity, new Vector3(1f * 0.37f, 3.7f * 0.37f, 1f * 0.37f)) };
 
         protected override string GetOriginalClassId => "640f57a6-6436-4132-a9bb-d914f3e19ef5";
 
         protected override bool ExteriorOnly => false;
+
+        protected override void EditPrefab(GameObject prefab)
+        {
+            prefab.transform.GetChild(0).localScale = Vector3.one * 0.37f;
+        }
     }
 }
