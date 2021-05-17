efc2b5840c423cdaaa247c3fe934a6d2aad85429
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-17 14:11:18 -0400

    fix unplacable obj

--- ArchitectsLibrary/Buildables/BuildableColumnSmall.cs
+++ ArchitectsLibrary/Buildables/BuildableColumnSmall.cs
@@ -4,13 +4,13 @@ namespace ArchitectsLibrary.Buildables
 {
     class BuildableColumnSmall : GenericPrecursorDecoration
     {
-        public BuildableColumnSmall() : base("BuildableColumnSmall", "Small Alien Column Structure", "A tall, column-like object with a complex design. Placeable inside and outside. Just tall enough to fit in a Multipurpose Room.")
+        public BuildableColumnSmall() : base("BuildableColumnSmall", "Small Alien Column Structure", "A tall, column-like object with a complex design. Placeable inside and outside.")
         {
         }
 
         protected override ConstructableSettings GetConstructableSettings => new ConstructableSettings(true, true, true, true, true, true, true, placeDefaultDistance: 2f, placeMinDistance: 2f, placeMaxDistance: 10f);
 
-        protected override OrientedBounds[] GetBounds => new OrientedBounds[] { new OrientedBounds(new Vector3(0f, 4f * 0.37f, 0f), Quaternion.identity, new Vector3(1f * 0.37f, 3.7f * 0.37f, 1f * 0.37f)) };
+        protected override OrientedBounds[] GetBounds => new OrientedBounds[] { new OrientedBounds(new Vector3(0f, 4f * 0.37f, 0f), Quaternion.identity, new Vector3(1f * 0.37f, 3.5f * 0.37f, 1f * 0.37f)) };
 
         protected override string GetOriginalClassId => "640f57a6-6436-4132-a9bb-d914f3e19ef5";
 
