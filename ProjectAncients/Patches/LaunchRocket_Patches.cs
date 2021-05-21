1af2de152b44b8b43b9c875b00292ba73e9b8e81
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-21 18:59:36 -0400

    increase rocket roar delay

--- ProjectAncients/Patches/LaunchRocket_Patches.cs
+++ ProjectAncients/Patches/LaunchRocket_Patches.cs
@@ -19,13 +19,17 @@ namespace ProjectAncients.Patches
 
         static IEnumerator PlayGargRoarDelayed()
         {
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(29f);
             GameObject roarObj = new GameObject("RocketRoar");
             var src = roarObj.AddComponent<AudioSource>();
             src.volume = ECCLibrary.ECCHelpers.GetECCVolume();
             src.clip = ECCLibrary.ECCAudio.LoadAudioClip("garg_roar-006");
             src.loop = false;
             src.playOnAwake = false;
+            src.spatialBlend = 1f;
+            src.transform.position = Player.main.transform.position + new Vector3(-60f, -15f, -30f);
+            src.minDistance = 20f;
+            src.maxDistance = 150f;
             src.Play();
             GameObject.Destroy(roarObj, 19f);
         }
