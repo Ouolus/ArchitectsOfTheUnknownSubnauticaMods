0d2702acfd32770f7ec902b2ced3ca46f0a0e496
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-21 18:50:58 -0400

    garg roar during rocket cutscene

---
+++ ProjectAncients/Patches/LaunchRocket_Patches.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using ProjectAncients.Prefabs;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UWE;
+
+namespace ProjectAncients.Patches
+{
+    [HarmonyPatch(typeof(LaunchRocket))]
+    public static class LaunchRocket_Patches
+    {
+        [HarmonyPatch(nameof(LaunchRocket.HideCrashedShip))]
+        [HarmonyPostfix]
+        public static void HideCrashedShip_Patch()
+        {
+            CoroutineHost.StartCoroutine(PlayGargRoarDelayed());
+        }
+
+        static IEnumerator PlayGargRoarDelayed()
+        {
+            yield return new WaitForSeconds(5f);
+            GameObject roarObj = new GameObject("RocketRoar");
+            var src = roarObj.AddComponent<AudioSource>();
+            src.volume = ECCLibrary.ECCHelpers.GetECCVolume();
+            src.clip = ECCLibrary.ECCAudio.LoadAudioClip("garg_roar-006");
+            src.loop = false;
+            src.playOnAwake = false;
+            src.Play();
+            GameObject.Destroy(roarObj, 19f);
+        }
+    }
+}
