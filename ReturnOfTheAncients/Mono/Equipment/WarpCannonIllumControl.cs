8e1555784ac3fd140db737011990d5b17a850d78
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-05-27 23:51:50 -0400

    wip warpcannonillumcontrol script

---
+++ ReturnOfTheAncients/Mono/Equipment/WarpCannonIllumControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RotA.Mono.Equipment
+{
+    public class WarpCannonIllumControl : MonoBehaviour
+    {
+        private Color startingColor;
+        private Color currentColor;
+        private Color targetColor;
+        private float timeColorShifted;
+        private float shiftSpeed;
+
+        private void Start()
+        {
+            currentColor = Color.green;
+            SetColor(Color.green, 1f);
+        }
+
+        private void SetColor(Color color, float shiftSpeed)
+        {
+            targetColor = color;
+        }
+
+        void Update()
+        {
+            currentColor = Color.Lerp(startingColor, targetColor, (Time.time - timeColorShifted) * shiftSpeed)
+        }
+    }
+}
