77f4da3ba577789899ef2a894be264d90555d41d
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-06-01 13:56:11 -0400

    more omega cube fabricator basics

--- ReturnOfTheAncients/Prefabs/AlienBase/OmegaCubeFabricator.cs
+++ ReturnOfTheAncients/Prefabs/AlienBase/OmegaCubeFabricator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using SMLHelper.V2.Assets;
 using UnityEngine;
+using UWE;  
 
 namespace RotA.Prefabs.AlienBase
 {
@@ -13,6 +14,7 @@ namespace RotA.Prefabs.AlienBase
 
         public override GameObject GetGameObject()
         {
+            //prefab essentials
             GameObject model = Mod.assetBundle.LoadAsset<GameObject>("OmegaCubeFabricator");
             GameObject prefab = GameObject.Instantiate(model);
             prefab.SetActive(false);
@@ -20,6 +22,10 @@ namespace RotA.Prefabs.AlienBase
             prefab.EnsureComponent<PrefabIdentifier>().ClassId = ClassID;
             prefab.EnsureComponent<TechTag>().type = TechType;
 
+            //drillable base
+            PrefabDatabase.TryGetPrefab("2db600ca-25f7-4000-93a5-f8c2a4ec0387", out GameObject drillableIonCubePrefab);
+            var drillableBase = GameObject.Instantiate(drillableIonCubePrefab, prefab.transform);
+            drillableBase.transform.localPosition = Vector3.zero;
             return prefab;
         }
     }
