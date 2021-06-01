9f106f54fef39a6d69115e147f17c3edd2ce35b0
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-06-01 16:59:22 -0400

    fabrication animation test

--- ReturnOfTheAncients/Mono/AlienTech/OmegaFabricatorRoot.cs
+++ ReturnOfTheAncients/Mono/AlienTech/OmegaFabricatorRoot.cs
@@ -20,8 +20,20 @@ namespace RotA.Mono
         public FMOD_CustomLoopingEmitter constructSoundEmitter;
 
         private bool cubeBeingBuilt;
+        private const float animationLength = 20f;
+        private float timeFabricationStart;
 
         GameObject currentCube;
+
+        List<Material> cachedMaterials;
+
+        bool CubeBeingFabricated
+        {
+            get
+            {
+                return cubeBeingBuilt && currentCube != null;
+            }
+        }
 
         public bool FormulaUnlocked()
         {
@@ -74,15 +86,86 @@ namespace RotA.Mono
             }
         }
 
-        void Update()
+        void StartFabricationAnimation()
         {
-
-        }
-
-        public IEnumerator GenerateCubeCoroutine()
-        {
+            timeFabricationStart = Time.time;
             animator.SetBool("fabricating", true);
             constructSoundEmitter.Play();
+            CacheMaterials();
+        }
+
+        void StopFabricationAnimation()
+        {
+            animator.SetBool("fabricating", false);
+            constructSoundEmitter.Stop();
+            Collider[] componentsInChildren = currentCube.GetComponentsInChildren<Collider>();
+            for (int i = 0; i < componentsInChildren.Length; i++)
+            {
+                componentsInChildren[i].enabled = true;
+            }
+        }
+
+        void Update()
+        {
+            if (currentCube != null)
+            {
+                if (Time.time < (timeFabricationStart + animationLength))
+                {
+
+                }
+            }
+        }
+
+        void CacheMaterials()
+        {
+            cachedMaterials = new List<Material>();
+            Renderer[] componentsInChildren = currentCube.GetComponentsInChildren<Renderer>();
+            float value = currentCube.transform.position.y - 0.5f;
+            float value2 = currentCube.transform.position.y + 2.5f;
+            foreach (Renderer renderer in componentsInChildren)
+            {
+                for (int j = 0; j < renderer.materials.Length; j++)
+                {
+                    renderer.materials[j].EnableKeyword("FX_BUILDING");
+                    renderer.materials[j].SetFloat(ShaderPropertyID._Built, 1f);
+                    renderer.materials[j].SetFloat(ShaderPropertyID._minYpos, value);
+                    renderer.materials[j].SetFloat(ShaderPropertyID._maxYpos, value2);
+                    //renderer.materials[j].SetTexture(ShaderPropertyID._EmissiveTex, this._EmissiveTex);
+                    renderer.materials[j].SetColor(ShaderPropertyID._BorderColor, new Color(0.75f, 1f, 0.9f, 1f));
+                    renderer.materials[j].SetFloat(ShaderPropertyID._Cutoff, 0.42f);
+                    renderer.materials[j].SetVector(ShaderPropertyID._BuildParams, new Vector4(2f, 0.7f, 3f, -0.25f));
+                    renderer.materials[j].SetFloat(ShaderPropertyID._NoiseStr, 0.25f);
+                    renderer.materials[j].SetFloat(ShaderPropertyID._NoiseThickness, 0.49f);
+                    renderer.materials[j].SetFloat(ShaderPropertyID._BuildLinear, 1f);
+                    renderer.materials[j].SetFloat(ShaderPropertyID._MyCullVariable, 0f);
+                    cachedMaterials.Add(renderer.materials[j]);
+                }
+            }
+        }
+
+        void UpdateScanAmountMaterials()
+        {
+            float value = currentCube.transform.position.y - 0.5f;
+            float value2 = currentCube.transform.position.y + 2.5f;
+            float num = 1f;
+            if (CubeBeingFabricated)
+            {
+                num = Mathf.Clamp((Time.time - timeFabricationStart) / animationLength, 0f, animationLength);
+            }
+            for (int i = 0; i < cachedMaterials.Count; i++)
+            {
+                Material material = cachedMaterials[i];
+                if (material != null)
+                {
+                    material.SetFloat(ShaderPropertyID._Built, num);
+                    material.SetFloat(ShaderPropertyID._minYpos, value);
+                    material.SetFloat(ShaderPropertyID._maxYpos, value2);
+                }
+            }
+        }
+
+        IEnumerator GenerateCubeCoroutine()
+        {
             cubeBeingBuilt = true;
             var task = PrefabDatabase.GetPrefabAsync("41406e76-4b4c-4072-86f8-f5b8e6523b73");
             yield return task;
@@ -105,12 +188,16 @@ namespace RotA.Mono
                 renderer.material.SetColor("_SquaresColor", new(0.5f, 0.5f, 0.5f));
                 renderer.material.SetFloat("_SquaresSpeed", 4f);
             }
+            foreach(Collider col in cube.GetComponentsInChildren<Collider>())
+            {
+                col.enabled = false;
+            }
             cube.SetActive(true);
-            yield return new WaitForSeconds(20f);
             currentCube = cube;
+            StartFabricationAnimation();
+            yield return new WaitForSeconds(animationLength);
             cubeBeingBuilt = false;
-            animator.SetBool("fabricating", false);
-            constructSoundEmitter.Stop();
+            StopFabricationAnimation();
         }
     }
 }
