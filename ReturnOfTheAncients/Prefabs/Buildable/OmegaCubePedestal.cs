5136b693d4955553f6fbcd5acacb6bb2aab7ced1
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-06-03 13:59:44 -0400

    Update OmegaCubePedestal.cs

--- ReturnOfTheAncients/Prefabs/Buildable/OmegaCubePedestal.cs
+++ ReturnOfTheAncients/Prefabs/Buildable/OmegaCubePedestal.cs
@@ -4,10 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ArchitectsLibrary.API;
+using ArchitectsLibrary.Buildables;
+using ArchitectsLibrary.Handlers;
+using SMLHelper.V2.Crafting;
 
 namespace RotA.Prefabs.Buildable
 {
-    public class OmegaCubePedestal
+    public class OmegaCubePedestal : BuildableIonCubePedestal
     {
+        public OmegaCubePedestal() : base("BuildableOmegaCubePedestal", "Omega Cube Pedestal", "A platform containing an Omega Cube. Placeable inside and outside.")
+        {
+        }
+
+        protected override TechData GetBlueprintRecipe()
+        {
+            return new TechData(new List<Ingredient>() { new Ingredient(AUHandler.AlienCompositeGlassTechType, 1), new Ingredient(Mod.omegaCube.TechType, 1) });
+        }
+
+        protected override string IonCubeClassId => Mod.omegaCube.ClassID;
     }
 }
