c7646bc8d865483ce5d298d20b6ca225a142c850
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-06-10 18:22:14 -0400

    cobalt class

---
+++ ArchitectsLibrary/Items/Cobalt.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using SMLHelper.V2.Utility;
+using UnityEngine;
+using UWE;
+
+namespace ArchitectsLibrary.Items
+{
+    class Cobalt : ReskinSpawnable
+    {
+        Atlas.Sprite sprite;
+        protected override string ReferenceClassId => "7815b1b7-2830-418b-9b5d-19949b0ae9ec";
+
+        public Cobalt() : base("Cobalt", "Cobalt", "Cobalt that makes Metious go yes.")
+        {
+        }
+
+        protected override void ApplyChangesToPrefab(GameObject prefab)
+        {
+            prefab.EnsureComponent<ResourceTracker>().overrideTechType = TechType;
+            var renderer = prefab.GetComponentInChildren<Renderer>();
+            renderer.material.SetTexture("_MainTex", Main.assetBundle.LoadAsset<Texture2D>("CobaltOre_Diffuse"));
+            renderer.material.SetTexture("_SpecTex", Main.assetBundle.LoadAsset<Texture2D>("CobaltOre_Spec"));
+        }
+
+        protected override Atlas.Sprite GetItemSprite()
+        {
+            return sprite ??= new Atlas.Sprite(Main.assetBundle.LoadAsset<Sprite>("CobaltOre_Icon"));
+        }
+
+        public override WorldEntityInfo EntityInfo => new()
+        {
+            cellLevel = LargeWorldEntity.CellLevel.Near,
+            classId = ClassID,
+            localScale = Vector3.one,
+            slotType = EntitySlot.Type.Small,
+            techType = TechType
+        };
+
+        public override List<LootDistributionData.BiomeData> BiomesToSpawnIn => new()
+        {
+            
+        };
+    }
+}
