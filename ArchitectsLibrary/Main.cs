3d2ca6db6dad47076868a2e2c04d26ff31d7ecbc
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-06-10 15:13:16 -0400

    patch new reosurces

--- ArchitectsLibrary/Main.cs
+++ ArchitectsLibrary/Main.cs
@@ -48,6 +48,8 @@ namespace ArchitectsLibrary
         static DrillableSapphire drillableSapphire;
         static RedBeryl redBeryl;
         static DrillableRedBeryl drillableRedBeryl;
+        static Bismuth bismuth;
+        static DrillableBismuth drillableBismuth;
         static ReinforcedGlass reinforcedGlass;
         static AlienCompositeGlass alienCompositeGlass;
         static AotuPoster aotuPoster;
@@ -203,6 +205,15 @@ namespace ArchitectsLibrary
             drillableRedBeryl = new DrillableRedBeryl();
             drillableRedBeryl.Patch();
             AUHandler.DrillableRedBerylTechType = drillableRedBeryl.TechType;
+
+            bismuth = new Bismuth();
+            bismuth.Patch();
+            AUHandler.BismuthTechType = bismuth.TechType;
+            CraftData.pickupSoundList.Add(bismuth.TechType, "event:/loot/pickup_precursorioncrystal");
+
+            drillableBismuth = new DrillableBismuth();
+            drillableBismuth.Patch();
+            AUHandler.DrillableBismuthTechType = drillableRedBeryl.TechType;
 
             morganite = new Morganite();
             morganite.Patch();
