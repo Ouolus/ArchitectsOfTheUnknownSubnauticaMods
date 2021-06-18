819c7a96455d8c8210fce0a8129fca13f9ed996d
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-06-17 23:27:27 -0400

    allow different items & auto format

--- ReturnOfTheAncients/Mono/AlienTech/CustomTeleporterTerminal.cs
+++ ReturnOfTheAncients/Mono/AlienTech/CustomTeleporterTerminal.cs
@@ -9,73 +9,87 @@ namespace RotA.Mono.AlienTech
         public string storyGoalName;
         public string interactText;
 
-		public PlayerCinematicController cinematicController;
+        public PlayerCinematicController cinematicController;
 
-		public Animator animator;
+        public Animator animator;
 
-		public FMODAsset useSound;
+        public FMODAsset useSound;
 
-		public FMODAsset openSound;
+        public FMODAsset openSound;
 
-		public FMODAsset closeSound;
+        public FMODAsset closeSound;
 
-		public GameObject root;
+        public GameObject root;
 
-		private GameObject itemObject;
+        private GameObject itemObject;
 
-		private int restoreQuickSlot = -1;
+        private int restoreQuickSlot = -1;
 
-		public bool Unlocked { get { return StoryGoalManager.main.IsGoalComplete(storyGoalName); } }
+        public bool Unlocked { get { return StoryGoalManager.main.IsGoalComplete(storyGoalName); } }
 
-		private void Start()
-		{
-			isValidHandTarget = false;
-		}
-
-		public void OpenDeck()
-		{
-			if (Unlocked)
-			{
-				return;
-			}
-			animator.SetBool("Open", true);
-			Utils.PlayFMODAsset(openSound, transform, 20f);
-		}
-
-		public void CloseDeck()
-		{
-			if (animator.GetBool("Open"))
-			{
-				animator.SetBool("Open", false);
-				Utils.PlayFMODAsset(closeSound, transform, 20f);
-			}
-		}
-
-		public void OnHandClick(GUIHand hand)
+        private void Start()
         {
-			if (!Unlocked)
-			{
-				Pickupable pickupable = Inventory.main.container.RemoveItem(TechType.PrecursorIonCrystal);
-				if (pickupable != null)
-				{
-					restoreQuickSlot = Inventory.main.quickSlots.activeSlot;
-					Inventory.main.ReturnHeld(true);
-					itemObject = pickupable.gameObject;
-					itemObject.transform.SetParent(Inventory.main.toolSocket);
-					itemObject.transform.localPosition = Vector3.zero;
-					itemObject.transform.localRotation = Quaternion.identity;
-					itemObject.SetActive(true);
-					Rigidbody component = itemObject.GetComponent<Rigidbody>();
-					if (component != null)
-					{
-						component.isKinematic = true;
-					}
-					cinematicController.StartCinematicMode(Player.main);
-					Utils.PlayFMODAsset(useSound, transform, 20f);
-					StoryGoalManager.main.OnGoalComplete(storyGoalName);
-				}
-			}
-		}
+            isValidHandTarget = false;
+        }
+
+        public void OpenDeck()
+        {
+            if (Unlocked)
+            {
+                return;
+            }
+            animator.SetBool("Open", true);
+            Utils.PlayFMODAsset(openSound, transform, 20f);
+        }
+
+        public void CloseDeck()
+        {
+            if (animator.GetBool("Open"))
+            {
+                animator.SetBool("Open", false);
+                Utils.PlayFMODAsset(closeSound, transform, 20f);
+            }
+        }
+
+        public void OnHandClick(GUIHand hand)
+        {
+            if (!Unlocked)
+            {
+                if (TryEnterItem(out GameObject pickupable))
+                {
+                    restoreQuickSlot = Inventory.main.quickSlots.activeSlot;
+                    Inventory.main.ReturnHeld(true);
+                    itemObject = pickupable.gameObject;
+                    itemObject.transform.SetParent(Inventory.main.toolSocket);
+                    itemObject.transform.localPosition = Vector3.zero;
+                    itemObject.transform.localRotation = Quaternion.identity;
+                    itemObject.SetActive(true);
+                    Rigidbody component = itemObject.GetComponent<Rigidbody>();
+                    if (component != null)
+                    {
+                        component.isKinematic = true;
+                    }
+                    cinematicController.StartCinematicMode(Player.main);
+                    Utils.PlayFMODAsset(useSound, transform, 20f);
+                    StoryGoalManager.main.OnGoalComplete(storyGoalName);
+                }
+            }
+        }
+
+        private bool TryEnterItem(out GameObject pickupableObject)
+        {
+            foreach (TechType tt in acceptedTechTypes)
+            {
+                Pickupable pickupable = Inventory.main.container.RemoveItem(tt);
+                if (pickupable != null)
+                {
+                    pickupableObject = pickupable.gameObject;
+                    return true;
+                }
+            }
+            pickupableObject = null;
+            return false;
+        }
 
         public void OnHandHover(GUIHand hand)
         {
@@ -86,25 +100,25 @@ namespace RotA.Mono.AlienTech
             }
         }
 
-		public void OnPlayerCinematicModeEnd(PlayerCinematicController controller)
-		{
-			if (itemObject)
-			{
-				Destroy(itemObject);
-			}
-			if (root)
-			{
-				root.BroadcastMessage("ToggleDoor", true, SendMessageOptions.RequireReceiver);
-			}
-			else
-			{
-				BroadcastMessage("ToggleDoor", true, SendMessageOptions.RequireReceiver);
-			}
-			CloseDeck();
-			if (restoreQuickSlot != -1)
-			{
-				Inventory.main.quickSlots.Select(restoreQuickSlot);
-			}
-		}
-	}
+        public void OnPlayerCinematicModeEnd(PlayerCinematicController controller)
+        {
+            if (itemObject)
+            {
+                Destroy(itemObject);
+            }
+            if (root)
+            {
+                root.BroadcastMessage("ToggleDoor", true, SendMessageOptions.RequireReceiver);
+            }
+            else
+            {
+                BroadcastMessage("ToggleDoor", true, SendMessageOptions.RequireReceiver);
+            }
+            CloseDeck();
+            if (restoreQuickSlot != -1)
+            {
+                Inventory.main.quickSlots.Select(restoreQuickSlot);
+            }
+        }
+    }
 }
