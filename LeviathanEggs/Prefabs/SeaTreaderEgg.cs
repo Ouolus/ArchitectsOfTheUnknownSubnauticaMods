5e46c2dc8136525a81169fc8b02c2396b176212c
Metious <71298690+Metious@users.noreply.github.com>
2021-06-21 01:16:45 +0430

    reduced SeaTreaderEgg glow

--- LeviathanEggs/Prefabs/SeaTreaderEgg.cs
+++ LeviathanEggs/Prefabs/SeaTreaderEgg.cs
@@ -10,14 +10,27 @@ namespace LeviathanEggs.Prefabs
         public SeaTreaderEgg()
             : base("SeaTreaderEgg", "Sea Treader Egg", "Sea Treaders hatch from these.")
         {
-            LateEnhancements += prefab => prefab.AddComponent<SpawnLocations>();
+            LateEnhancements += LateEnhance;
         }
 
         public override GameObject Model => LoadGameObject("SeaTreaderEgg.prefab");
+        
         public override TechType HatchingCreature => TechType.SeaTreader;
+        
         public override float HatchingTime => 5f;
+        
         public override Sprite ItemSprite => LoadSprite("SeaTreaderEgg");
         
         public override Vector2int SizeInInventory { get; } = new(3, 3);
+
+        void LateEnhance(GameObject prefab)
+        {
+            var renderer = prefab.GetComponentInChildren<Renderer>();
+
+            renderer.material.SetFloat(ShaderPropertyID._GlowStrength, .2f);
+            renderer.material.SetFloat(ShaderPropertyID._GlowStrengthNight, .2f);
+
+            prefab.AddComponent<SpawnLocations>();
+        }
     }
 }
