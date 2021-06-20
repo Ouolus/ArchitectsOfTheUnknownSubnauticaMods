bc2caaebcbbbacf3d7952cee3f705c3052547f6c
Metious <71298690+Metious@users.noreply.github.com>
2021-06-21 02:06:02 +0430

    reduced LavaLarvaEgg glow

--- LeviathanEggs/Prefabs/LavaLarvaEgg.cs
+++ LeviathanEggs/Prefabs/LavaLarvaEgg.cs
@@ -10,12 +10,27 @@ namespace LeviathanEggs.Prefabs
         public LavaLarvaEgg()
             : base("LavaLarvaEgg", "LavaLarva Egg", "LavaLarva hatch from these.")
         {
-            LateEnhancements += prefab => prefab.AddComponent<SpawnLocations>();
+            LateEnhancements += LateEnhance;
         }
+        
         public override GameObject Model => LoadGameObject("LavaLarvaEgg.prefab");
+        
         public override TechType HatchingCreature => TechType.LavaLarva;
+        
         public override float HatchingTime => 5f;
+        
         public override Sprite ItemSprite => LoadSprite("LavaLarvaEgg");
+        
         public override Vector2int SizeInInventory { get; } = new(1, 1);
+
+        void LateEnhance(GameObject prefab)
+        {
+            var renderer = prefab.GetComponentInChildren<Renderer>();
+            
+            renderer.material.SetFloat(ShaderPropertyID._GlowStrength, 0.3f);
+            renderer.material.SetFloat(ShaderPropertyID._GlowStrengthNight, 0.3f);
+            
+            prefab.AddComponent<SpawnLocations>();
+        }
     }
 }
