f18f8d87bf83b5a04b89bf5191354e879ce10ef9
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-06-20 23:51:22 -0400

    remove drillables from koosh zone base

--- ReturnOfTheAncients/Mono/AlienBaseSpawners/KooshBaseSpawner.cs
+++ ReturnOfTheAncients/Mono/AlienBaseSpawners/KooshBaseSpawner.cs
@@ -17,11 +17,6 @@ namespace RotA.Mono.AlienBaseSpawners
             yield return StartCoroutine(SpawnPrefab(structure_column, new Vector3(centerLocalX + 14f, floorLocalY, 0f), Vector3.zero, columnScale));
             yield return StartCoroutine(SpawnPrefab(structure_column, new Vector3(centerLocalX + 14f, floorLocalY, -6f), Vector3.zero, columnScale));
             yield return StartCoroutine(SpawnPrefab(structure_column, new Vector3(centerLocalX + 14f, floorLocalY, 6f), Vector3.zero, columnScale));
-            yield return StartCoroutine(SpawnPrefab(CraftData.GetClassIdForTechType(AUHandler.DrillableCobaltTechType), new Vector3(centerLocalX - 5f, floorLocalY, 13f)));
-            yield return StartCoroutine(SpawnPrefab(CraftData.GetClassIdForTechType(AUHandler.DrillableSapphireTechType), new Vector3(centerLocalX + 5f, floorLocalY, 13f)));
-            yield return StartCoroutine(SpawnPrefab(CraftData.GetClassIdForTechType(AUHandler.DrillableEmeraldTechType), new Vector3(centerLocalX - 5f, floorLocalY, 16f)));
-            yield return StartCoroutine(SpawnPrefab(CraftData.GetClassIdForTechType(AUHandler.DrillableMorganiteTechType), new Vector3(centerLocalX + 5f, floorLocalY, 16f)));
-            yield return StartCoroutine(SpawnPrefab(CraftData.GetClassIdForTechType(AUHandler.PrecursorAlloyIngotTechType), new Vector3(centerLocalX, floorLocalY + 0.1f, 19f), new Vector3(0f, 240f, 0f)));
             //yield return StartCoroutine(SpawnPrefab(Mod.precursorMasterTechTerminal.ClassID, new Vector3(centerLocalX, floorLocalY, -3f), new Vector3(0f, 0f, 0f)));
 
         }
