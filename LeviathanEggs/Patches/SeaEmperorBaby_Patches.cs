e7efccba423f4eb1f18b520ae97ccaa6a1aa90d5
Metious <71298690+Metious@users.noreply.github.com>
2021-06-21 22:20:12 +0430

    cleanup

--- LeviathanEggs/Patches/SeaEmperorBaby_Patches.cs
+++ LeviathanEggs/Patches/SeaEmperorBaby_Patches.cs
@@ -2,39 +2,15 @@
 using ArchitectsLibrary.MonoBehaviours;
 namespace LeviathanEggs.Patches
 {
-    [HarmonyPatch(typeof(SeaEmperorBaby))]
+    [HarmonyPatch]
     class SeaEmperorBaby_Patches
     {
         [HarmonyPrefix]
-        [HarmonyPatch(nameof(SeaEmperorBaby.Teleport))]
+        [HarmonyPatch(typeof(SeaEmperorBaby), nameof(SeaEmperorBaby.Teleport))]
+        [HarmonyPatch(typeof(SeaEmperorBaby), nameof(SeaEmperorBaby.SetTeleporterTarget))]
+        [HarmonyPatch(typeof(SeaEmperorBaby), nameof(SeaEmperorBaby.SwimToTeleporter))]
+        [HarmonyPatch(typeof(SeaEmperorBaby), nameof(SeaEmperorBaby.SwimToMother))]
         static bool Teleport_Patch(SeaEmperorBaby __instance)
-        {
-            if (__instance.gameObject.GetComponent<StagedGrowing>() != null)
-                return false;
-
-            return true;
-        }
-        [HarmonyPrefix]
-        [HarmonyPatch(nameof(SeaEmperorBaby.SetTeleporterTarget))]
-        static bool SetTeleporterTarget_Patch(SeaEmperorBaby __instance)
-        {
-            if (__instance.gameObject.GetComponent<StagedGrowing>() != null)
-                return false;
-
-            return true;
-        }
-        [HarmonyPrefix]
-        [HarmonyPatch(nameof(SeaEmperorBaby.SwimToTeleporter))]
-        static bool SwimToTeleporter_Patch(SeaEmperorBaby __instance)
-        {
-            if (__instance.gameObject.GetComponent<StagedGrowing>() != null)
-                return false;
-
-            return true;
-        }
-        [HarmonyPrefix]
-        [HarmonyPatch(nameof(SeaEmperorBaby.SwimToMother))]
-        static bool SwimToMother_Patch(SeaEmperorBaby __instance)
         {
             if (__instance.gameObject.GetComponent<StagedGrowing>() != null)
                 return false;
