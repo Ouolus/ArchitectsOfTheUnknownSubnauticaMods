755e4543c798e662242a0fa5553f2cd418c39331
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-06-20 23:51:56 -0400

    re-add precursor alloy ingot

--- ReturnOfTheAncients/Mono/AlienBaseSpawners/KooshBaseSpawner.cs
+++ ReturnOfTheAncients/Mono/AlienBaseSpawners/KooshBaseSpawner.cs
@@ -17,8 +17,8 @@ namespace RotA.Mono.AlienBaseSpawners
             yield return StartCoroutine(SpawnPrefab(structure_column, new Vector3(centerLocalX + 14f, floorLocalY, 0f), Vector3.zero, columnScale));
             yield return StartCoroutine(SpawnPrefab(structure_column, new Vector3(centerLocalX + 14f, floorLocalY, -6f), Vector3.zero, columnScale));
             yield return StartCoroutine(SpawnPrefab(structure_column, new Vector3(centerLocalX + 14f, floorLocalY, 6f), Vector3.zero, columnScale));
+            yield return StartCoroutine(SpawnPrefab(CraftData.GetClassIdForTechType(AUHandler.PrecursorAlloyIngotTechType), new Vector3(centerLocalX, floorLocalY + 0.1f, 19f), new Vector3(0f, 240f, 0f)));
             //yield return StartCoroutine(SpawnPrefab(Mod.precursorMasterTechTerminal.ClassID, new Vector3(centerLocalX, floorLocalY, -3f), new Vector3(0f, 0f, 0f)));
-
         }
 
         protected override string MainTerminalClassId => Mod.kooshBaseTerminal.ClassID;
