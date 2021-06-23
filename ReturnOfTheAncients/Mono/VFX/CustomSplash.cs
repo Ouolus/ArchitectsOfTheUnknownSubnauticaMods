e24883149d33fcd377e0951707572e1bdf012ccb
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-06-23 01:04:32 -0400

    customsplash stuff

--- ReturnOfTheAncients/Mono/VFX/CustomSplash.cs
+++ ReturnOfTheAncients/Mono/VFX/CustomSplash.cs
@@ -9,5 +9,36 @@ namespace RotA.Mono.VFX
 {
     public class CustomSplash : MonoBehaviour
     {
-    }
+		private float animTime = 0f;
+		private bool playing;
+		private Vector3 newScale;
+		public Vector3 startScale;
+
+		public float duration = 3f;
+		public AnimationCurve surfScaleX;
+		public AnimationCurve surfScaleY;
+		public AnimationCurve surfScaleZ;
+		public AnimationCurve surfMaskCurve;
+
+		private void Update()
+		{
+			if (playing)
+			{
+				animTime += Time.deltaTime / duration;
+				if (animTime > 0.99f)
+				{
+					playing = false;
+					Destroy(gameObject);
+				}
+				if (gameObject != null)
+				{
+					gameObject.GetComponentInChildren<Renderer>().material.SetTextureOffset(ShaderPropertyID._MaskTex, new Vector2(surfMaskCurve.Evaluate(this.animTime), 0.5f));
+					newScale.x = startScale.x * surfScaleX.Evaluate(animTime);
+					newScale.y = startScale.y * surfScaleY.Evaluate(animTime);
+					newScale.z = startScale.z * surfScaleZ.Evaluate(animTime);
+					transform.localScale = newScale;
+				}
+			}
+		}
+	}
 }
