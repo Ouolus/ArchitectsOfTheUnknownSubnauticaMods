1636fda7d67c67a288e1aaa8c48a3381a2afc3a2
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-06-24 01:10:36 -0400

    fix red tablet position

--- ReturnOfTheAncients/Mono/AlienBaseSpawners/ResearchBaseSpawner.cs
+++ ReturnOfTheAncients/Mono/AlienBaseSpawners/ResearchBaseSpawner.cs
@@ -28,8 +28,8 @@ namespace RotA.Mono.AlienBaseSpawners
 
             yield return StartCoroutine(SpawnPrefab(CraftData.GetClassIdForTechType(AUHandler.PrecursorAlloyIngotTechType), new Vector3(centerLocalX, floorLocalY + 0.1f, 19f), new Vector3(0f, 37f, 0f)));
 
-            yield return StartCoroutine(SpawnPrefab(Mod.redTabletHolder.ClassID, new Vector3(centerLocalX, floorLocalY, 17), Vector3.up * 180f));
-            yield return StartCoroutine(SpawnPrefab(supplies_redTablet, new Vector3(centerLocalX, floorLocalY + 2.5f, 17.5f - 0.125f), new Vector3(90f, 0f, 0f), Vector3.one * 1.5f));
+            yield return StartCoroutine(SpawnPrefab(Mod.redTabletHolder.ClassID, new Vector3(centerLocalX, floorLocalY, 17 + 1f), Vector3.up * 180f));
+            yield return StartCoroutine(SpawnPrefab(supplies_redTablet, new Vector3(centerLocalX, floorLocalY + 2.5f, 17.5f - 0.825f + 1f), new Vector3(90f, 0f, 0f), Vector3.one * 1.5f));
 
             //Outside
             yield return StartCoroutine(SpawnPrefabGlobally(bone_curly, new Vector3(-891.40f, -197.51f, -597.30f), Vector3.up, false));
