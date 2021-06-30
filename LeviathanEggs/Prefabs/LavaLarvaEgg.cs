15bd2d08440fc774ce34a143b01b195ab2197d8f
Metious <71298690+Metious@users.noreply.github.com>
2021-06-30 16:27:45 +0430

    LavaLarvaEgg spawns

--- LeviathanEggs/Prefabs/LavaLarvaEgg.cs
+++ LeviathanEggs/Prefabs/LavaLarvaEgg.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using LeviathanEggs.MonoBehaviours;
 using ArchitectsLibrary.API;
 using static LeviathanEggs.Helpers.AssetsBundleHelper;
@@ -16,6 +17,34 @@ namespace LeviathanEggs.Prefabs
         public override GameObject Model => LoadGameObject("LavaLarvaEgg.prefab");
         
         public override TechType HatchingCreature => TechType.LavaLarva;
+        
+        public override List<LootDistributionData.BiomeData> BiomesToSpawnIn { get; } = new()
+        {
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.InactiveLavaZone_Chamber_Floor,
+                probability = 0.01f
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.InactiveLavaZone_Corridor_Floor,
+                probability = 0.01f
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.InactiveLavaZone_CastleChamber_Floor,
+                probability = 0.01f
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.ActiveLavaZone_Chamber_Floor,
+                probability = 0.01f
+            }
+        };
         
         public override float HatchingTime => 5f;
         
