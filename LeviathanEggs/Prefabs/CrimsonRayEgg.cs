9764aae81c8217a7c7dc9a05c0cec5dc528a34aa
Metious <71298690+Metious@users.noreply.github.com>
2021-06-30 16:27:19 +0430

    CrimsonRayEgg spawns

--- LeviathanEggs/Prefabs/CrimsonRayEgg.cs
+++ LeviathanEggs/Prefabs/CrimsonRayEgg.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using LeviathanEggs.MonoBehaviours;
 using ArchitectsLibrary.API;
 using static LeviathanEggs.Helpers.AssetsBundleHelper;
@@ -13,9 +14,41 @@ namespace LeviathanEggs.Prefabs
             LateEnhancements += LateEnhance;
         }
         public override GameObject Model => LoadGameObject("CrimsonRayEgg.prefab");
+        
         public override TechType HatchingCreature => TechType.GhostRayRed;
+
+        public override List<LootDistributionData.BiomeData> BiomesToSpawnIn { get; } = new()
+        {
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.InactiveLavaZone_Chamber_Floor,
+                probability = 0.01f
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.InactiveLavaZone_Corridor_Floor,
+                probability = 0.01f
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.InactiveLavaZone_CastleChamber_Floor,
+                probability = 0.01f
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.ActiveLavaZone_Chamber_Floor,
+                probability = 0.01f
+            }
+        };
+
         public override float HatchingTime => 5f;
+        
         public override Sprite ItemSprite => LoadSprite("CrimsonRayEgg");
+        
         public override Vector2int SizeInInventory { get; } = new(2, 2);
 
         void LateEnhance(GameObject prefab)
