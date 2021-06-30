8b34872241985d1bb8b4eae1d442cb3cac68e379
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-06-29 20:26:22 -0400

    weird exceptions for eyes

--- ReturnOfTheAncients/Mono/Creatures/GargEssentials/GargEyeTracker.cs
+++ ReturnOfTheAncients/Mono/Creatures/GargEssentials/GargEyeTracker.cs
@@ -5,13 +5,20 @@ namespace RotA.Mono.Creatures.GargEssentials
     public class GargEyeTracker : MonoBehaviour
     {
         Quaternion defaultLocalRotation;
+        Vector3 eyeOverrideScale = new Vector3(0.95f, 0.95f, 0.95f);
+        bool isWeirdBackEye;
+        bool isWeirdFrontEye;
+        bool isWeirdMiddleEye;
 
         void Start()
         {
             defaultLocalRotation = transform.localRotation;
+            isWeirdBackEye = gameObject.name == "BRE";
+            isWeirdFrontEye = gameObject.name == "FRE";
+            isWeirdMiddleEye = gameObject.name == "MRE";
         }
 
-        void Update()
+        void LateUpdate()
         {
             Transform target = GetTarget();
             if (target)
@@ -23,6 +30,24 @@ namespace RotA.Mono.Creatures.GargEssentials
             {
                 transform.localRotation = Quaternion.RotateTowards(transform.localRotation, defaultLocalRotation, Time.deltaTime * 150f);
             }
+
+            if (isWeirdBackEye)
+            {
+                transform.localEulerAngles = new Vector3(145f, transform.localEulerAngles.y, transform.localEulerAngles.z);
+            }
+            else if (isWeirdFrontEye)
+            {
+                transform.localEulerAngles = new Vector3(50f, transform.localEulerAngles.y, transform.localEulerAngles.z);
+            }
+            else if (isWeirdMiddleEye)
+            {
+                transform.localEulerAngles = new Vector3(70f, transform.localEulerAngles.y, transform.localEulerAngles.z);
+            }
+            else
+            {
+                transform.localEulerAngles = new Vector3(0f, transform.localEulerAngles.y, transform.localEulerAngles.z);
+            }
+            transform.localScale = eyeOverrideScale;
         }
 
         Transform GetTarget()
