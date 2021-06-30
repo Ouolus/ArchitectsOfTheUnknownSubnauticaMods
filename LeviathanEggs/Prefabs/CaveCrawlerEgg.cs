8376be4c47af1a870f5bbb657837d354af71bad8
Metious <71298690+Metious@users.noreply.github.com>
2021-06-30 16:27:02 +0430

    CaveCrawlerEgg spawns

--- LeviathanEggs/Prefabs/CaveCrawlerEgg.cs
+++ LeviathanEggs/Prefabs/CaveCrawlerEgg.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using LeviathanEggs.MonoBehaviours;
 using ArchitectsLibrary.API;
 using static LeviathanEggs.Helpers.AssetsBundleHelper;
@@ -13,8 +14,51 @@ namespace LeviathanEggs.Prefabs
             LateEnhancements += prefab => prefab.AddComponent<SpawnLocations>();
         }
         public override GameObject Model => LoadGameObject("CaveCrawlerEgg.prefab");
+        
         public override TechType HatchingCreature => TechType.CaveCrawler;
+
+        public override List<LootDistributionData.BiomeData> BiomesToSpawnIn { get; } = new()
+        {
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.FloatingIslands_Shallows,
+                probability = 0.02f
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.FloatingIslands_AbandonedBase_Outside,
+                probability = 0.01f
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.Mountains_IslandGrass,
+                probability = 0.01f,
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.Mountains_IslandSand,
+                probability = 0.01f
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.Dunes_CaveFloor,
+                probability = 0.01f
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.UnderwaterIslands_IslandCaveFloor,
+                probability = 0.01f
+            }
+        };
+
         public override float HatchingTime => 2f;
+        
         public override Sprite ItemSprite => LoadSprite("CaveCrawlerEgg");
     }
 }
