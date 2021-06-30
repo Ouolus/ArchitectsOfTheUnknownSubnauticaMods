53f46a30685dba58df381b68ea69bc5c079b984c
Metious <71298690+Metious@users.noreply.github.com>
2021-06-30 16:28:30 +0430

    SkyRayEgg spawns

--- LeviathanEggs/Prefabs/SkyRayEgg.cs
+++ LeviathanEggs/Prefabs/SkyRayEgg.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using ArchitectsLibrary.API;
 using LeviathanEggs.MonoBehaviours;
 using static LeviathanEggs.Helpers.AssetsBundleHelper;
@@ -15,8 +16,30 @@ namespace LeviathanEggs.Prefabs
         
         public override GameObject Model => LoadGameObject("SkyRayEgg.prefab");
         
-        public override TechType HatchingCreature => TechType.SeaTreader;
-        
+        public override TechType HatchingCreature => TechType.Skyray;
+
+        public override List<LootDistributionData.BiomeData> BiomesToSpawnIn { get; } = new()
+        {
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.FloatingIslands_Shallows,
+                probability = 0.02f
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.Mountains_IslandGrass,
+                probability = 0.01f,
+            },
+            new LootDistributionData.BiomeData
+            {
+                count = 1,
+                biome = BiomeType.Mountains_IslandRock,
+                probability = 0.01f
+            }
+        };
+
         public override float HatchingTime => 5f;
         
         public override Sprite ItemSprite => LoadSprite("SkyRayEgg");
