6f11ec7cc9fcece9ed02e48bfdc926c207b5fd21
Metious <71298690+Metious@users.noreply.github.com>
2021-07-02 01:04:01 +0430

    (Big commit) alien robots now repair bases + vehicle repair tweaks

--- LeviathanEggs/MonoBehaviours/DroidWelder.cs
+++ LeviathanEggs/MonoBehaviours/DroidWelder.cs
@@ -4,19 +4,63 @@ namespace LeviathanEggs.MonoBehaviours
 {
     class DroidWelder :  CreatureAction
     {
+        Leakable[] _leakables;
+        BaseRoot _base;
         WalkBehaviour _walkBehaviour;
         MeleeAttack _meleeAttack;
         LiveMixin _weldableTarget;
         float _timeToPerform;
+        float _emergencyWeld = 3;
+        bool _weAreLeaking;
 
         void Start()
         {
             _walkBehaviour = GetComponent<WalkBehaviour>();
             _meleeAttack = GetComponent<MeleeAttack>();
         }
+
+        void Update()
+        {
+            if (_base is null)
+                return;
+
+            _weAreLeaking = _base.IsLeaking();
+
+            if (_weAreLeaking)
+            {
+                if (_leakables[0]?.leakingLeakPoints.Count <= 0)
+                    _leakables.RemoveFast(0);
+                
+                if (_leakables.Length > 0)
+                    return;
+                
+                _leakables = _base.flood.leakers.Clone() as Leakable[];
+            }
+        }
+
+        public void SetSubRoot(SubRoot subRoot)
+        {
+            if (subRoot is null)
+                return;
+
+            subRoot.gameObject.TryGetComponent(out _base);
+        }
+
+        // called when the object is picked up by the player
+        void OnExamine()
+        {
+            _base = null;
+            _leakables = null;
+            _weldableTarget = null;
+            _weAreLeaking = false;
+        }
         
         public override float Evaluate(Creature creature)
         {
+            if (_weAreLeaking && Time.time >= _timeToPerform)
+            {
+                return _emergencyWeld;
+            }
             if (_weldableTarget is not null && _weldableTarget.health < _weldableTarget.maxHealth && Time.time >= _timeToPerform)
             {
                 return evaluatePriority;
@@ -27,16 +71,18 @@ namespace LeviathanEggs.MonoBehaviours
 
         public override void StartPerform(Creature creature)
         {
-            _walkBehaviour.WalkTo(_weldableTarget.transform.position, 2f);
+            if (_weAreLeaking)
+            {
+                var pos = _leakables[0].leakingLeakPoints[0].transform.position;
+                _walkBehaviour.WalkTo(pos, 4f);
+            }
         }
 
         public override void Perform(Creature creature, float deltaTime)
         {
-            if (_weldableTarget.AddHealth(2f) > 0f)
+            if ((_weAreLeaking && Vector3.Distance(transform.position, _leakables[0].leakingLeakPoints[0].transform.position) < 2f && _leakables[0].live.AddHealth(2f) > 0f) || (_weldableTarget && Vector3.Distance(transform.position, _weldableTarget.transform.position) < 2f && _weldableTarget.AddHealth(2f) > 0f))
             {
-                var mouth = _meleeAttack.mouth.transform; 
-                Instantiate(_meleeAttack.damageFX, mouth.position, mouth.rotation);
-                Utils.PlayEnvSound(_meleeAttack.attackSound, mouth.position);
+                WeldingFx();
             }
         }
 
@@ -46,21 +92,22 @@ namespace LeviathanEggs.MonoBehaviours
             _timeToPerform = Time.time + 5f;
         }
 
-        void Update()
+        void OnCollisionEnter(Collision other)
         {
-            if (_weldableTarget is not null) // no need to consistently check if we already found a weldable target
+            if (_weldableTarget)
                 return;
+            
+            var obj = UWE.Utils.GetEntityRoot(other.collider.gameObject);
+            obj = obj ? obj : other.collider.gameObject;
+            if (obj.TryGetComponent(out LiveMixin lm) && lm.IsWeldable() && lm.health < lm.maxHealth)
+                _weldableTarget = lm;
+        }
 
-            var targets = UWE.Utils.OverlapSphereIntoSharedBuffer(transform.position, 0.5f);
-            for (int i = 0; i < targets; i++)
-            {
-                var currentTarget = UWE.Utils.sharedColliderBuffer[i];
-
-                var obj = UWE.Utils.GetEntityRoot(currentTarget.gameObject);
-                obj = obj ? obj : currentTarget.gameObject;
-                if (obj.TryGetComponent(out LiveMixin lm) && lm.IsWeldable() && lm.health < lm.maxHealth)
-                    _weldableTarget = lm;
-            }
+        void WeldingFx()
+        {
+            var mouth = _meleeAttack.mouth.transform; 
+            Instantiate(_meleeAttack.damageFX, mouth.position, mouth.rotation);
+            Utils.PlayEnvSound(_meleeAttack.attackSound, mouth.position);
         }
     }
 }
