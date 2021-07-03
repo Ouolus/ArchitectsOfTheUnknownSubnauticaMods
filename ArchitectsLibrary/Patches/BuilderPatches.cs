1e8f74cec8a9c5bd671a2b13484cc0fa5bb4d2ab
Metious <71298690+Metious@users.noreply.github.com>
2021-07-03 21:44:10 +0430

    commented the transpiler so u know wth it does

--- ArchitectsLibrary/Patches/BuilderPatches.cs
+++ ArchitectsLibrary/Patches/BuilderPatches.cs
@@ -45,17 +45,50 @@ namespace ArchitectsLibrary.Patches
             List<CodeInstruction> codes = new(instructions);
 
             var setScale = AccessTools.Method(typeof(BuilderPatches), nameof(SetScale));
-            var rotationSet = AccessTools.PropertySetter(typeof(Transform), nameof(Transform.rotation));
+            var rotationSet = AccessTools.PropertySetter(typeof(Transform), nameof(Transform.rotation)); // setter method of the rotation property
 
             bool found = false;
+            
+            /*
+             * our target is the following:
+             * Transform transform = gameObject.transform;
+			 * transform.position = Builder.placePosition;
+			 * transform.rotation = Builder.placeRotation;
+             * Constructable componentInParent3 = gameObject.GetComponentInParent<Constructable>();
+             *
+             * what we want to do is to add the SetScale() method at the end of our target so we end up having something like this:
+             * Transform transform = gameObject.transform;
+			 * transform.position = Builder.placePosition;
+			 * transform.rotation = Builder.placeRotation;
+             * SetScale(transform)
+             * Constructable componentInParent3 = gameObject.GetComponentInParent<Constructable>();
+             */
 
             for (int i = 0; i < codes.Count; i++)
             {
+                /*
+                 * this is our target's IL code:
+                 * ldloc.2
+                 * callvirt  instance class [UnityEngine.CoreModule]UnityEngine.Transform [UnityEngine.CoreModule]UnityEngine.GameObject::get_transform()
+                 * dup
+                 * ldsfld    valuetype [UnityEngine.CoreModule]UnityEngine.Vector3 Builder::placePosition
+                 * callvirt  instance void [UnityEngine.CoreModule]UnityEngine.Transform::set_position(valuetype [UnityEngine.CoreModule]UnityEngine.Vector3)
+                 * ldsfld    valuetype [UnityEngine.CoreModule]UnityEngine.Quaternion Builder::placeRotation
+                 * callvirt  instance void [UnityEngine.CoreModule]UnityEngine.Transform::set_rotation(valuetype [UnityEngine.CoreModule]UnityEngine.Quaternion)
+                 * ldloc.2
+                 * callvirt  instance !!0 [UnityEngine.CoreModule]UnityEngine.GameObject::GetComponentInParent<class Constructable>()
+                 * ...
+                 *
+                 * so since we want to insert our SetScale() method after the 'transform.rotation = Builder.placeRotation;' we're gonna target it.
+                 * 
+                 * the 'ldloc.2' is the 'Transform transform = gameObject.transform' local variable, so we need to pass it as an argument for the SetScale() so it can modify
+                 * the scale.
+                 */
                 if (codes[i].opcode == OpCodes.Callvirt && Equals(codes[i].operand, rotationSet) && codes[i + 1].opcode == OpCodes.Ldloc_2)
                 {
                     found = true;
-                    codes.Insert(i + 1, new CodeInstruction(OpCodes.Call, setScale));
-                    codes.Insert(i + 1, new CodeInstruction(OpCodes.Ldloc_2));
+                    codes.Insert(i + 1, new CodeInstruction(OpCodes.Call, setScale)); // first we call the setScale method
+                    codes.Insert(i + 1, new CodeInstruction(OpCodes.Ldloc_2)); // then we insert the transform local variable to the stack at the top of our call
                     break;
                 }
             }
