331130f01fbbb3c16c26b72f59c99440662c7b57
Metious <71298690+Metious@users.noreply.github.com>
2021-07-05 17:58:27 +0430

    virbatim identifier

--- ArchitectsLibrary/Utility/GlobalExtensions.cs
+++ ArchitectsLibrary/Utility/GlobalExtensions.cs
@@ -9,11 +9,11 @@ public static class GlobalExtensions
     /// <summary>
     /// Adds a message and increases the life of it, instead of spamming it.
     /// </summary>
-    /// <param name="errorMessage"></param>
+    /// <param name="this"></param>
     /// <param name="message">the message text</param>
-    public static void AddHint(this ErrorMessage errorMessage, string message)
+    public static void AddHint(this ErrorMessage @this, string message)
     {
-        var msg = errorMessage.GetExistingMessage(message);
+        var msg = @this.GetExistingMessage(message);
         if (msg is null)
             ErrorMessage.AddMessage(message);
         else if (msg.timeEnd < Time.time + 2)
