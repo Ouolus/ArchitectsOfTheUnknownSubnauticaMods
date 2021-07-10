d6b4ff38a12a9d29dd44146e29e01b115761bc51
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-07-10 16:58:41 -0400

    add all dev terminals to base

--- ReturnOfTheAncients/Mono/AlienBaseSpawners/SecretBaseSpawner.cs
+++ ReturnOfTheAncients/Mono/AlienBaseSpawners/SecretBaseSpawner.cs
@@ -12,7 +12,16 @@ namespace RotA.Mono.AlienBaseSpawners
 
             yield return SpawnPrefab(Mod.precursorAtmosphereVolume.ClassID, new Vector3(0f, 3f, -30f), Vector3.zero, new Vector3(50f, 50f, 70f));
 
-            yield return SpawnPrefab(Mod.devSecretTerminal.ClassID, new Vector3(0f, 0f, -49), Vector3.up * 180f);
+            yield return SpawnPrefab(Mod.devSecretTerminal.ClassID, new Vector3(0, 0, -49), Vector3.up * 180f);
+
+            yield return SpawnPrefab(Mod.devTerminalAlan.ClassID, new Vector3(13, 0, -11), Vector3.up * 90);
+            yield return SpawnPrefab(Mod.devTerminalHipnox.ClassID, new Vector3(13, 0, -25), Vector3.up * 90);
+            yield return SpawnPrefab(Mod.devTerminalLee23.ClassID, new Vector3(13, 0, -35), Vector3.up * 90);
+            yield return SpawnPrefab(Mod.devTerminalMetious.ClassID, new Vector3(-13, 0, -11), Vector3.up * -90);
+            yield return SpawnPrefab(Mod.devTerminalN8crafter.ClassID, new Vector3(-13, 0, -25), Vector3.up * -90);
+            yield return SpawnPrefab(Mod.devTerminalSlendyPlayz.ClassID, new Vector3(-13, 0, -35), Vector3.up * -90);
+
+            yield return SpawnPrefab(Mod.devTerminalTori.ClassID, new Vector3(0f, 0, -25), Vector3.up * 180f);
         }
     }
 }
