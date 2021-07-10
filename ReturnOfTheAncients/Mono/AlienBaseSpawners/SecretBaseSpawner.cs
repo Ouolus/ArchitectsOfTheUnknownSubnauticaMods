f19ebbb2de858e5091acf067a0afe9e15d29711b
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-07-10 16:54:32 -0400

    fix terminal scale being 0

--- ReturnOfTheAncients/Mono/AlienBaseSpawners/SecretBaseSpawner.cs
+++ ReturnOfTheAncients/Mono/AlienBaseSpawners/SecretBaseSpawner.cs
@@ -12,7 +12,7 @@ namespace RotA.Mono.AlienBaseSpawners
 
             yield return SpawnPrefab(Mod.precursorAtmosphereVolume.ClassID, new Vector3(0f, 3f, -30f), Vector3.zero, new Vector3(50f, 50f, 70f));
 
-            yield return SpawnPrefab(Mod.devSecretTerminal.ClassID, new Vector3(0f, 0f, -49), Vector3.zero, Vector3.up * 180f);
+            yield return SpawnPrefab(Mod.devSecretTerminal.ClassID, new Vector3(0f, 0f, -49), Vector3.up * 180f);
         }
     }
 }
