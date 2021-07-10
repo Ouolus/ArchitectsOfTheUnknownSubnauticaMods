459702f0db9ccabebc8fa5b67d6043275483cfcb
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-07-09 22:37:14 -0400

    atmosphere volume in secret base

--- ReturnOfTheAncients/Mono/AlienBaseSpawners/SecretBaseSpawner.cs
+++ ReturnOfTheAncients/Mono/AlienBaseSpawners/SecretBaseSpawner.cs
@@ -9,6 +9,7 @@ namespace RotA.Mono.AlienBaseSpawners
         public override IEnumerator ConstructBase()
         {
             yield return SpawnPrefab(Mod.secretBaseModel.ClassID, new Vector3(0f, 0f, 0f));
+            StartCoroutine(SpawnPrefab(atmosphereVolume_cache, new Vector3(0f, 3f, -30f), Vector3.zero, new Vector3(50f, 50f, 70f)));
         }
     }
 }
