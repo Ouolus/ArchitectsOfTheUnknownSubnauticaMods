e3cf155cde46c3a8a705c9a15a91b6d8b610c3ee
Metious <71298690+Metious@users.noreply.github.com>
2021-07-18 13:24:20 +0430

    Power Cube material for knife

--- ReturnOfTheAncients/Mono/Equipment/IonKnifeActions/RedIonCubeAction.cs
+++ ReturnOfTheAncients/Mono/Equipment/IonKnifeActions/RedIonCubeAction.cs
@@ -13,6 +13,9 @@ namespace RotA.Mono.Equipment.IonKnifeActions
             ionKnife.PlaySwitchSound("event:/env/pink_artifact_loop");
             ionKnife.VfxEventType = VFXEventTypes.heatBlade;
             ionKnife.ResourceBonus = 1;
+
+            ionKnife.SetMaterialColors(new Color(1f, 0.2f, 0f), new Color(1f, 0.2f, 0f), 
+                Color.red, new Color(0.5f, 0.5f, 0.5f));
         }
 
         public void OnHit(IonKnife ionKnife, LiveMixin hitLiveMixin)
