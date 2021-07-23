933302cd4c7a3e1e5ae613a224ff65dd35b387e6
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-07-23 13:41:22 -0400

    secretbasecontroller WIP monobehavior

---
+++ ReturnOfTheAncients/Mono/Cinematics/SecretBaseGargController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using RotA.Prefabs.Creatures;
+using ECCLibrary;
+using ArchitectsLibrary.Utility;
+    
+namespace RotA.Mono.Cinematics
+{
+    public class SecretBaseGargController : MonoBehaviour
+    {
+        public GameObject GetGargPrefab()
+        {
+            GameObject prefab = GameObject.Instantiate(Mod.gargAssetBundle.LoadAsset<GameObject>("SecretBaseGarg_Prefab"));
+            prefab.SetActive(false);
+            prefab.transform.forward = Vector3.up;
+            prefab.transform.localScale = Vector3.one * 5.5f;
+            MaterialUtils.ApplySNShaders(prefab);
+            BehaviourLOD lod = prefab.EnsureComponent<BehaviourLOD>();
+            lod.veryCloseThreshold = 5000f;
+            lod.closeThreshold = 7500f;
+            lod.farThreshold = 10000f;
+            List<Transform> spines = new List<Transform>();
+            GameObject currentSpine = prefab.SearchChild("Spine");
+            while (currentSpine != null)
+            {
+                currentSpine = currentSpine.SearchChild("Spine", ECCStringComparison.StartsWith);
+                if (currentSpine)
+                {
+                    if (currentSpine.name.Contains("59"))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        spines.Add(currentSpine.transform);
+                    }
+                }
+            }
+            spines.Add(prefab.SearchChild("Tail", ECCStringComparison.Equals).transform);
+            spines.Add(prefab.SearchChild("Tail1", ECCStringComparison.Equals).transform);
+            spines.Add(prefab.SearchChild("Tail2", ECCStringComparison.Equals).transform);
+            spines.Add(prefab.SearchChild("Tail3", ECCStringComparison.Equals).transform);
+            spines.Add(prefab.SearchChild("Tail4", ECCStringComparison.Equals).transform);
+            spines.Add(prefab.SearchChild("Tail5", ECCStringComparison.Equals).transform);
+            spines.Add(prefab.SearchChild("Tail6", ECCStringComparison.Equals).transform);
+            FixRotationMultipliers(CreateTrail(prefab, prefab.SearchChild("Spine"), spines.ToArray(), lod), 0.26f, 0.26f);
+            return prefab;
+        }
+
+        TrailManager CreateTrail(GameObject prefab, GameObject trailRoot, Transform[] trails, BehaviourLOD lod)
+        {
+            trailRoot.gameObject.SetActive(false);
+            TrailManager trail = trailRoot.AddComponent<TrailManager>();
+            trail.trails = trails;
+            trail.rootTransform = prefab.transform;
+            trail.rootSegment = trail.transform;
+            trail.levelOfDetail = lod;
+            trail.segmentSnapSpeed = 0.075f / 4.5f;
+            trail.maxSegmentOffset = 700f;
+            trail.allowDisableOnScreen = false;
+            AnimationCurve decreasing = new AnimationCurve(new Keyframe[] { new Keyframe(0f, 0.25f), new Keyframe(1f, 0.75f) });
+            trail.pitchMultiplier = decreasing;
+            trail.rollMultiplier = decreasing;
+            trail.yawMultiplier = decreasing;
+            trail.Initialize();
+            trailRoot.gameObject.SetActive(true);
+            return trail;
+        }
+
+        void FixRotationMultipliers(TrailManager tm, float min, float max)
+        {
+            AnimationCurve curve = new AnimationCurve(new Keyframe[] { new Keyframe(0f, min), new Keyframe(1f, max) });
+            tm.pitchMultiplier = curve;
+            tm.rollMultiplier = curve;
+            tm.yawMultiplier = curve;
+        }
+    }
+}
