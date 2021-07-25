2bf636691e807653c4da10817a4affa2cf88d6ca
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-07-24 22:34:31 -0400

    oops too soon

--- ReturnOfTheAncients/Mono/Cinematics/SecretBaseGargController.cs
+++ ReturnOfTheAncients/Mono/Cinematics/SecretBaseGargController.cs
@@ -37,7 +37,7 @@ namespace RotA.Mono.Cinematics
             yield return new WaitForSeconds(11f);
             PlayCreakSFX("Creaking5");
             Floodlights(2f);
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(2f);
             PlayCloseRoarSFX();
             yield return new WaitForSeconds(2f);
             StartBlackOutEffect();
