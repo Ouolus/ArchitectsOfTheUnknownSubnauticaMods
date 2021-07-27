6a3aa9ec5884785bf75b2f4d7158f8f7c1c517fc
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-07-27 16:58:50 -0400

    hopefully this will work????

--- ArchitectsLibrary/Patches/CraftDataPatches.cs
+++ ArchitectsLibrary/Patches/CraftDataPatches.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using ArchitectsLibrary.MonoBehaviours;
 using HarmonyLib;
 using UnityEngine;
+using UWE;
 
 namespace ArchitectsLibrary.Patches
 {
@@ -9,18 +10,19 @@ namespace ArchitectsLibrary.Patches
     {
         internal static void Patch(Harmony harmony)
         {
-            var orig = AccessTools.Method(typeof(CraftData), nameof(CraftData.PreparePrefabIDCache));
+            var orig = AccessTools.Method(typeof(PrefabDatabase), nameof(PrefabDatabase.LoadPrefabDatabase));
             var postfix = new HarmonyMethod(AccessTools.Method(typeof(CraftDataPatches), nameof(PreparePrefabIDCachePostfix)));
             harmony.Patch(orig, postfix: postfix);
         }
 
         static void PreparePrefabIDCachePostfix()
         {
-            UWE.CoroutineHost.StartCoroutine(FixIonCubeCraftingCoroutine());
+            CoroutineHost.StartCoroutine(FixIonCubeCraftingCoroutine());
         }
-        
+
         static IEnumerator FixIonCubeCraftingCoroutine()
         {
+            yield return null;
             var task = CraftData.GetPrefabForTechTypeAsync(TechType.PrecursorIonCrystal);
             yield return task;
             var prefab = task.GetResult();
