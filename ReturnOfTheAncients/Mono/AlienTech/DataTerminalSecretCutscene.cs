904d133d21d375f4497be850a1ea446c533d236b
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-07-29 17:34:13 -0400

    secret cutscene terminal mono

---
+++ ReturnOfTheAncients/Mono/AlienTech/DataTerminalSecretCutscene.cs
@@ -0,0 +1,25 @@
+﻿using RotA.Mono.Cinematics;
+using UnityEngine;
+
+namespace RotA.Mono.AlienTech
+{
+    public class DataTerminalSecretCutscene : MonoBehaviour
+    {
+        private bool interactedThisSession;
+
+        public void OnStoryHandTarget()
+        {
+            if (interactedThisSession == true)
+            {
+                return;
+            }
+            interactedThisSession = true;
+            Invoke(nameof(StartCinematic), 3f);
+        }
+
+        private void StartCinematic()
+        {
+            SecretBaseGargController.PlayCinematic();
+        }
+    }
+}
