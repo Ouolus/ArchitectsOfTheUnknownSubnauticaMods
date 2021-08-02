b494674a7aad4ad234f9ece7cdc1d224aac0664c
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-08-01 21:30:18 -0400

    attempt to fix custom teleporter color with a patch

---
+++ ReturnOfTheAncients/Patches/PrecursorTeleporter_Patches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RotA.Mono.AlienTech;
+
+namespace RotA.Patches
+{
+    [HarmonyPatch(typeof(PrecursorTeleporter))]
+    public class PrecursorTeleporter_Patches
+    {
+        [HarmonyPatch(nameof(PrecursorTeleporter.Start))]
+        [HarmonyPostfix]
+        public static void Start_Postfix(PrecursorTeleporter __instance)
+        {
+            if (__instance.gameObject.GetComponent<RotaTeleporter>() != null)
+            {
+                if (__instance.portalFxControl != null)
+                {
+                    __instance.portalFxControl.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
