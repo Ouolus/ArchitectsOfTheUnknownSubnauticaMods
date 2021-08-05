747a98a9ce26a765b5dfd349e6012ac86df30d10
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-08-04 22:08:21 -0400

    remove POINTless POINT light

--- ReturnOfTheAncients/Mono/AlienBaseSpawners/CablesNearGuardian.cs
+++ ReturnOfTheAncients/Mono/AlienBaseSpawners/CablesNearGuardian.cs
@@ -8,7 +8,6 @@ namespace RotA.Mono.AlienBaseSpawners
         public override IEnumerator ConstructBase()
         {
             yield return StartCoroutine(GenerateCable(new Vector3(354, -430, -1780), Vector3.up, new Vector3(356, -348, -1756), new Vector3(-0.2f, 0, 1), Vector3.back, 10f, 3f, false)); //when you're facing the guardian from the void, this is the one on the left
-            yield return StartCoroutine(SpawnPrefabGlobally(ambience_greenLight, new Vector3(378, -352, -1750)));
             yield return StartCoroutine(SpawnPrefabGlobally(ambience_greenLight, new Vector3(370, -338, -1746)));
             yield return StartCoroutine(SpawnPrefabGlobally(ambience_greenLight, new Vector3(384, -332, -1736.5f)));
             yield return StartCoroutine(SpawnPrefabGlobally(ambience_greenLight, new Vector3(366, -327, -1731)));
