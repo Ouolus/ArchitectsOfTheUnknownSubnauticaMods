42b03b3de177009451194cf84f8c7d517925f0f0
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-08-09 15:38:47 -0400

    leave shader stuff up to the GetModel implementation

--- ArchitectsLibrary/API/ALPlaceable.cs
+++ ArchitectsLibrary/API/ALPlaceable.cs
@@ -106,8 +106,6 @@ namespace ArchitectsLibrary.API
                 placeTool.placementSound = SNAudioEvents.GetFmodAsset(SNAudioEvents.Paths.LightStickPlace);
                 placeTool.alignWithSurface = GetPlacementMode.HasFlag(PlacementFlags.AlignWithSurface);
                 placeTool.allowedOnRigidBody = GetPlacementMode.HasFlag(PlacementFlags.AllowedOnRigidbody);
-
-                MaterialUtils.ApplySNShaders(cachedPrefab);
             }
             return cachedPrefab;
         }
