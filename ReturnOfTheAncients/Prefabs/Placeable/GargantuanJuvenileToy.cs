c66034f722f806138b06dfa6502a07ca8ea9a97d
Metious <71298690+Metious@users.noreply.github.com>
2021-08-09 11:16:52 +0430

    Added garg juvenile toy

---
+++ ReturnOfTheAncients/Prefabs/Placeable/GargantuanJuvenileToy.cs
@@ -0,0 +1,31 @@
+using ArchitectsLibrary.API;
+using SMLHelper.V2.Crafting;
+using UnityEngine;
+
+namespace RotA.Prefabs.Placeable
+{
+    public class GargantuanJuvenileToy : ALPlaceable
+    {
+        public GargantuanJuvenileToy()
+            : base("GargantuanJuvenileToy", "Gargantuan Juvenile Toy", "Gargantuan Juvenile Toy that makes me go yes.")
+        {}
+
+
+        public override PlacementFlags GetPlacementMode => PlacementFlags.Inside | PlacementFlags.AllowedOnRigidbody;
+
+        protected override TechData GetBlueprintRecipe() => new()
+        {
+            craftAmount = 1,
+            Ingredients =
+            {
+                new(TechType.Quartz, 2),
+                new(TechType.Titanium, 1),
+                new(TechType.CreepvineSeedCluster, 1)
+            }
+        };
+
+        protected override Atlas.Sprite GetItemSprite() => SpriteManager.defaultSprite;
+
+        public override GameObject GetModel() => Mod.gargAssetBundle.LoadAsset<GameObject>("GargantuanAdultToy");
+    }
+}
