c2c6be8d21541180fe32e8d576307c64b629604c
Metious <71298690+Metious@users.noreply.github.com>
2021-08-09 11:14:39 +0430

    Added garg adult toy

---
+++ ReturnOfTheAncients/Prefabs/Placeable/GargantuanAdultToy.cs
@@ -0,0 +1,32 @@
+using ArchitectsLibrary.API;
+using SMLHelper.V2.Crafting;
+using UnityEngine;
+
+namespace RotA.Prefabs.Placeable
+{
+    class GargantuanAdultToy : ALPlaceable
+    {
+        public GargantuanAdultToy()
+            : base("GargantuanAdultToy", "Gargantuan Adult Toy", "Gargantuan Adult Toy that makes me go yes.")
+        {}
+
+
+        public override PlacementFlags GetPlacementMode => PlacementFlags.Inside | PlacementFlags.AllowedOnRigidbody;
+
+        protected override TechData GetBlueprintRecipe() => new()
+        {
+            craftAmount = 1,
+            Ingredients =
+            {
+                new(TechType.Quartz, 2),
+                new(TechType.Titanium, 1),
+                new(TechType.CreepvineSeedCluster, 1)
+            }
+        };
+
+        protected override Atlas.Sprite GetItemSprite() => SpriteManager.defaultSprite;
+
+        public override GameObject GetModel() => Mod.gargAssetBundle.LoadAsset<GameObject>("GargantuanAdultToy");
+
+    }
+}
