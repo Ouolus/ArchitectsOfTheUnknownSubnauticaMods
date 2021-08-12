9e6a76f019f515df641d4b6d85d9c4d66cbdf507
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-08-11 21:27:01 -0400

    update trailmanager more

--- ReturnOfTheAncients/Prefabs/Creatures/Guardian.cs
+++ ReturnOfTheAncients/Prefabs/Creatures/Guardian.cs
@@ -22,10 +22,10 @@ namespace RotA.Prefabs.Creatures
 
         public override void AddCustomBehaviour(CreatureComponents components)
         {
-            TrailManager trail = CreateTrail(prefab.SearchChild("Neck1"), components, 7f, maxSegmentOffset: 0.8f);
+            TrailManager trail = CreateTrail(prefab.SearchChild("Neck1"), components, 7f, maxSegmentOffset: 2f);
             trail.pitchMultiplier = new AnimationCurve(new[] { new Keyframe(0f, 0f), new Keyframe(1f, 0.8f) });
             trail.yawMultiplier = new AnimationCurve(new[] { new Keyframe(0f, 0f), new Keyframe(1f, 0.8f) });
-            trail.rollMultiplier = new AnimationCurve(new[] { new Keyframe(0f, 0.25f), new Keyframe(1f, 1f) });
+            trail.rollMultiplier = new AnimationCurve(new[] { new Keyframe(0f, 0f), new Keyframe(1f, 0.8f) });
         }
 
         public override void SetLiveMixinData(ref LiveMixinData liveMixinData)
