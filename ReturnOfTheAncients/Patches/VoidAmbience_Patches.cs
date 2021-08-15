285a5928973be35857eb847b1fe7c351a158f790
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-08-15 15:38:10 -0400

    white void reflections again

--- ReturnOfTheAncients/Patches/VoidAmbience_Patches.cs
+++ ReturnOfTheAncients/Patches/VoidAmbience_Patches.cs
@@ -38,7 +38,7 @@ namespace RotA.Patches
                 temperature = 5f,
                 scattering = 0.25f
             };
-            PatchBiomeFog(__instance, voidBiomeName, voidWaterscapeSettings, __instance.biomeSkies[22]);
+            PatchBiomeFog(__instance, voidBiomeName, voidWaterscapeSettings, __instance.biomeSkies[0]);
         }
 
         [HarmonyPostfix]
