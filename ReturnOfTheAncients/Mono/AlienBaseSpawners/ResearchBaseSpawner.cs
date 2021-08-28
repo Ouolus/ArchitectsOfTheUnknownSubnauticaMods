bafbb286db5cb345a163a099d43b631b6048d4ba
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-08-27 21:27:41 -0400

    change ghost levi lighting

--- ReturnOfTheAncients/Mono/AlienBaseSpawners/ResearchBaseSpawner.cs
+++ ReturnOfTheAncients/Mono/AlienBaseSpawners/ResearchBaseSpawner.cs
@@ -57,13 +57,25 @@ namespace RotA.Mono.AlienBaseSpawners
             yield return StartCoroutine(SpawnPrefabGlobally(natural_grass_green, new Vector3(-886.3569f, -200.9488f, -572.1064f), new Vector3(15.88102f, 3.901271f, 27.44349f), Vector3.one * 3f));
             yield return StartCoroutine(SpawnPrefabGlobally(natural_grass_green, new Vector3(-899.7899f, -205.5513f, -576.6423f), new Vector3(23.60654f, 4.788297f, 22.62831f), Vector3.one * 3f));
             
-            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-894.6136f, -201.1311f, -564.0109f), new Vector3(316.3175f, 346.2894f, 33.39309f), Vector3.one * 1f));
-            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-880.7109f, -192.7862f, -571.0246f), new Vector3(6.688025f, 1.020864f, 17.33839f), Vector3.one * 1f));
-            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-855.3006f, -190.3299f, -583.9695f), new Vector3(356.1317f, 0.1070935f, 356.8296f), Vector3.one * 1f));
-            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-864.6073f, -187.7225f, -583.7346f), new Vector3(270f, 0f, 0f), Vector3.one * 1f));
-            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-840.0343f, -191.2774f, -585.4749f), new Vector3(356.1317f, 0.1070936f, 356.8296f), Vector3.one * 1f));
-            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-829.215f, -188.6837f, -583.7346f), new Vector3(270f, 0f, 0f), Vector3.one * 1f));
-            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-868.9732f, -195.1799f, -576.115f), new Vector3(14.9749f, 2.321379f, 17.52697f), Vector3.one * 1f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-886.3828f, -202.0417f, -569.2833f), new Vector3(17.53328f, 5.449856f, 34.30387f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-881.7793f, -201.4561f, -556.8263f), new Vector3(358.7383f, 359.5172f, 41.88034f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-899.9372f, -212.8077f, -554.4672f), new Vector3(355.8499f, 0.2901385f, 352.0053f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-895.7101f, -207.3998f, -569.8304f), new Vector3(22.46861f, 6.085391f, 29.9639f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-872.4451f, -197.3629f, -559.0836f), new Vector3(358.9864f, 359.946f, 6.092449f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-881.7217f, -197.2678f, -581.2143f), new Vector3(7.454872f, 0.8958662f, 13.68609f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-868.9159f, -196.9121f, -566.4913f), new Vector3(2.146841f, 0.1172011f, 6.24889f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-870.4085f, -193.5893f, -584.7972f), new Vector3(10.8496f, 0.7330649f, 7.707815f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-859.1404f, -193.3646f, -575.5086f), new Vector3(4.823329f, 0.3031821f, 7.189247f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-849.6245f, -192.2948f, -580.6376f), new Vector3(1.352244f, 0.1537388f, 12.97182f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-844.4916f, -193.1719f, -590.3148f), new Vector3(351.4302f, 0.1242448f, 358.3419f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-839.6558f, -192.5452f, -580.223f), new Vector3(358.8148f, 0.006393833f, 359.3818f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-827.1651f, -194.2734f, -590.5538f), new Vector3(352.5652f, 0.1378824f, 357.8781f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-820.8176f, -193.6505f, -590.1338f), new Vector3(350.9109f, 359.3884f, 7.68268f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-829.6092f, -191.7808f, -578.1346f), new Vector3(336.974f, 357.5009f, 12.22479f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-860.9232f, -193.1857f, -587.629f), new Vector3(11.78806f, 359.9927f, 359.9293f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-889.2529f, -200.5325f, -576.9055f), new Vector3(21.1193f, 4.758039f, 25.12779f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-820.6724f, -190.5986f, -581.2459f), new Vector3(333.3828f, 356.8796f, 13.13683f), Vector3.one * 2f));
+            yield return StartCoroutine(SpawnPrefabGlobally(ambience_blueLight, new Vector3(-834.8636f, -193.3038f, -590.0609f), new Vector3(0.6323666f, 359.982f, 356.737f), Vector3.one * 2f));
         }
 
         protected override float MainTerminalZOffset => 12f;
