3c371e5439ef41110c92c40b1e977c7dbb9c5dfc
Metious <71298690+Metious@users.noreply.github.com>
2021-09-01 19:27:35 +0430

    Added StartsOrEndsWith extension method

--- ArchitectsLibrary/Utility/GlobalExtensions.cs
+++ ArchitectsLibrary/Utility/GlobalExtensions.cs
@@ -1,4 +1,6 @@
 // ReSharper disable CheckNamespace
+
+using System;
 using UnityEngine;
 
 /// <summary>
@@ -18,5 +20,17 @@ public static class GlobalExtensions
             ErrorMessage.AddMessage(message);
         else if (msg.timeEnd < Time.time + 2)
             msg.timeEnd += Time.deltaTime;
+    }
+
+    /// <summary>
+    /// Checks whether or not the value starts or ends with in the current string.
+    /// </summary>
+    /// <param name="this"></param>
+    /// <param name="value">The value to check.</param>
+    /// <param name="comparisonType">One of the enumeration values that determines how this string and value are compared.</param>
+    /// <returns><see langword="true"/> if it does, otherwise <see langword="false"/>.</returns>
+    public static bool StartsOrEndsWith(this string @this, string value, StringComparison comparisonType = StringComparison.OrdinalIgnoreCase)
+    {
+        return @this.StartsWith(value, comparisonType) || @this.EndsWith(value, comparisonType);
     }
 }
