81a60ce081fe2e4a3c69caf868f743611aa48a38
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2021-09-10 23:09:44 -0400

    UpgradeConsolePatches

--- ArchitectsLibrary/Interfaces/ICyclopsOnEquip.cs
+++ ArchitectsLibrary/Interfaces/ICyclopsOnEquip.cs
@@ -8,10 +8,10 @@ namespace ArchitectsLibrary.Interfaces
         /// <summary>
         /// your logic for your module when it is equipped into a cyclops.
         /// </summary>
-        /// <param name="slotID">which slot in the upgrade console your upgrade is in</param>
+        /// <param name="slotID">name of the slot in the upgrade console that your upgrade is in</param>
         /// <param name="equipped">whether the module has been equipped or unequipped</param>
         /// <param name="sub">an instance of the <see cref="SubRoot"/> so you can use the class' stuff to benefit from a more
         /// complex logic for your upgrade.</param>
-        void OnEquip(int slotID, bool equipped, SubRoot sub);
+        void OnEquip(string slotID, bool equipped, SubRoot sub);
     }
 }
---
+++ ArchitectsLibrary/Patches/UpgradeConsolePatches.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using ArchitectsLibrary.Interfaces;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ArchitectsLibrary.Patches
+{
+    internal class UpgradeConsolePatches
+    {
+        internal static readonly Dictionary<TechType, ICyclopsOnEquip> CyclopsOnEquips = new();
+
+        internal static void Patch(Harmony harmony)
+        {            
+            harmony.Patch(AccessTools.Method(typeof(SubRoot), nameof(UpgradeConsole.OnEquip)),
+                postfix: new HarmonyMethod(AccessTools.Method(typeof(UpgradeConsolePatches), nameof(OnEquip_Postfix))));
+
+            harmony.Patch(AccessTools.Method(typeof(SubRoot), nameof(UpgradeConsole.OnUnequip)),
+                postfix: new HarmonyMethod(AccessTools.Method(typeof(UpgradeConsolePatches), nameof(OnUnequip_Postfix))));
+        }
+
+        static void OnEquip_Postfix(UpgradeConsole __instance, string slot, InventoryItem item)
+        {
+            if (item == null || item.item == null) return;
+            if (CyclopsOnEquips.TryGetValue(item.item.GetTechType(), out ICyclopsOnEquip cyclopsOnEquip))
+            {
+                SubRoot subRoot = __instance.GetComponentInChildren<SubRoot>();
+                if (subRoot)
+                {
+                    cyclopsOnEquip.OnEquip(slot, true, subRoot);
+                }
+            }
+        }
+
+        static void OnUnequip_Postfix(UpgradeConsole __instance, string slot, InventoryItem item)
+        {
+            if (item == null || item.item == null) return;
+            if (CyclopsOnEquips.TryGetValue(item.item.GetTechType(), out ICyclopsOnEquip cyclopsOnEquip))
+            {
+                SubRoot subRoot = __instance.GetComponentInChildren<SubRoot>();
+                if (subRoot)
+                {
+                    cyclopsOnEquip.OnEquip(slot, false, subRoot);
+                }
+            }
+        }
+    }
+}
